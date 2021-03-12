bd8bdc38d498e42e018d6f2de8bf15b12a7781df
Simon Strandgaard <neoneye@gmail.com>
2021-03-12 01:57:03 +0100

    Extracted script that is responsible only for gathering program ids.

--- script/generate_csv_with_dependencies.rb
+++ script/generate_csv_with_dependencies.rb
@@ -1,11 +1,15 @@
 #!/usr/bin/env ruby
 
 =begin
-Prerequisits:
-The 'dotenv' gem. Install it using `$ gem install dotenv`
-https://github.com/bkeepers/dotenv
 
+This script takes input from a `program_ids.csv` file, with this format:
 
+    program id
+    4
+    5
+    7
+    8
+    10
 
 This script traverses all the programs inside the LODA program rootdir.
 Each program is a line in the CSV file, with it's dependencies listed.
@@ -22,88 +26,55 @@ This script outputs a `dependenies.csv` file, with this format:
 =end
 
 require 'csv'
-require 'dotenv'
-Dotenv.load('../.env')
 
-LODA_PROGRAM_ROOTDIR = ENV['LODA_PROGRAM_ROOTDIR']
-
-def obtain_files(path)
-    Dir.chdir(path) do
-        names = Dir.glob('*').select { |f| File.file? f }
-        return names.sort
-    end
-end
-
-def obtain_dirs(path)
-    Dir.chdir(path) do
-        names = Dir.glob('*').select { |f| File.directory? f }
-        return names.sort
-    end
-end
-
-def obtain_nested_names(rootdir)
-    names = []
-    root_dirs = obtain_dirs(rootdir)
-    root_dirs.each do |dirname|
-        path = File.join(rootdir, dirname)
-        names += obtain_files(path)
-    end
-    names
-end
-
-def sequenceids_from_filenames(filenames)
-    sequence_ids = []
-    filenames.map do |filename|
-        if filename =~ /0*(\d+)/
-            sequence_id = $1.to_i
-            sequence_ids << sequence_id
-        else
-            puts "Mismatch for #{filename}"
-        end
-    end
-    sequence_ids
-end
-
+input_filename = 'program_ids.csv'
+output_filename = 'dependencies.csv'
 
 # Build the newest version
 `cargo build --release`
 
 time_start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 
-filenames = obtain_nested_names(LODA_PROGRAM_ROOTDIR)
-sequence_ids = sequenceids_from_filenames(filenames)
-# p sequence_ids
-
-count_success = 0
-count_failure = 0
-sequence_ids_count_minus1 = sequence_ids.count - 1
-if sequence_ids_count_minus1 == 0
-    sequence_ids_count_minus1 = 1
+# Obtain all the program_ids to be processed
+program_ids = []
+CSV.foreach(input_filename, {:col_sep => ";"}) do |row|
+    col0 = row[0]
+    program_id = col0.to_i
+    next if program_id == 0
+    program_ids << program_id
 end
 
-CSV.open("dependencies.csv", "wb", {:col_sep => ";"}) do |csv|
+# Status 
+count_success = 0
+count_failure = 0
+program_ids_count_minus1 = program_ids.count - 1
+if program_ids_count_minus1 == 0
+    program_ids_count_minus1 = 1
+end
+
+# Generate output file
+CSV.open(output_filename, "wb", {:col_sep => ";"}) do |csv|
     csv << ["program id", "dependency count", "program ids"]
-    sequence_ids.each_with_index do |sequence_id, index|
-        output = `../target/release/loda_lab dependencies #{sequence_id}`
+    program_ids.each_with_index do |program_id, index|
+        output = `../target/release/loda_lab dependencies #{program_id}`
         output = output.strip
         dependency_count = output.split(',').count
         success = $?.success?
         if success
             count_success += 1
-            csv << [sequence_id.to_s, dependency_count.to_s, output]
+            csv << [program_id.to_s, dependency_count.to_s, output]
         else
             count_failure += 1
         end
         if (index % 1000) == 0
-            percent = (100 * index) / sequence_ids_count_minus1
-            puts "PROGRESS: #{index} / #{sequence_ids.count}  percent: #{percent}"
+            percent = (100 * index) / program_ids_count_minus1
+            puts "PROGRESS: #{index} / #{program_ids.count}  percent: #{percent}"
         end
-        # if index == 10
-        #     break
-        # end
+        break if index == 10
     end
 end
 
+# Show stats
 time_end = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 time_elapsed = time_end - time_start
 time_elapsed_s = "%.3f" % time_elapsed
---
+++ script/generate_csv_with_program_ids.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/env ruby
+
+=begin
+Prerequisits:
+The 'dotenv' gem. Install it using `$ gem install dotenv`
+https://github.com/bkeepers/dotenv
+
+This script traverses all the programs inside the LODA program rootdir.
+It looks for all the LODA assembly programs there are.
+Each program_id is appended to the CSV file.
+
+This script outputs a `program_ids.csv` file, with this format:
+
+    program id
+    4
+    5
+    7
+    8
+    10
+
+=end
+
+require 'csv'
+require 'dotenv'
+Dotenv.load('../.env')
+
+LODA_PROGRAM_ROOTDIR = ENV['LODA_PROGRAM_ROOTDIR']
+
+output_filename = 'program_ids.csv'
+
+def obtain_files(path)
+    Dir.chdir(path) do
+        names = Dir.glob('*').select { |f| File.file? f }
+        return names.sort
+    end
+end
+
+def obtain_dirs(path)
+    Dir.chdir(path) do
+        names = Dir.glob('*').select { |f| File.directory? f }
+        return names.sort
+    end
+end
+
+def obtain_nested_names(rootdir)
+    names = []
+    root_dirs = obtain_dirs(rootdir)
+    root_dirs.each do |dirname|
+        path = File.join(rootdir, dirname)
+        names += obtain_files(path)
+    end
+    names
+end
+
+def integers_from_filenames(filenames)
+    integers = []
+    filenames.map do |filename|
+        if filename =~ /0*(\d+)/
+            integers << $1.to_i
+        else
+            puts "Mismatch for #{filename}"
+        end
+    end
+    integers
+end
+
+
+filenames = obtain_nested_names(LODA_PROGRAM_ROOTDIR)
+program_ids = integers_from_filenames(filenames)
+
+CSV.open(output_filename, "wb", {:col_sep => ";"}) do |csv|
+    csv << ["program id"]
+    program_ids.each_with_index do |program_id, index|
+        csv << [program_id.to_s]
+        # break if index == 10
+    end
+end
