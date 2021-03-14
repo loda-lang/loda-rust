2c9a711c864a4ba95677dd83cfdc322a424bd536
Simon Strandgaard <neoneye@gmail.com>
2021-03-14 10:31:26 +0100

    NodeLoopRegister added.

--- src/execute/mod.rs
+++ src/execute/mod.rs
@@ -26,6 +26,7 @@ pub mod node_divideif;
 pub mod node_gcd;
 pub mod node_logarithm;
 pub mod node_loop_constant;
+pub mod node_loop_register;
 pub mod node_loop_simple;
 pub mod node_move;
 pub mod node_modulo;
---
+++ src/execute/node_loop_register.rs
@@ -0,0 +1,103 @@
+use super::{Node, Program, ProgramState, ProgramRunnerManager, RegisterIndex, RegisterValue, RunMode};
+use num_bigint::{BigInt, ToBigInt};
+use num_traits::{ToPrimitive, Signed};
+
+pub struct NodeLoopRegister {
+    register_start: RegisterIndex,
+    register_with_range_length: RegisterIndex,
+    program: Program,
+}
+
+impl NodeLoopRegister {
+    pub fn new(register_start: RegisterIndex, register_with_range_length: RegisterIndex, program: Program) -> Self {
+        Self {
+            register_start: register_start,
+            register_with_range_length: register_with_range_length,
+            program: program,
+        }
+    }
+}
+
+impl Node for NodeLoopRegister {
+    fn shorthand(&self) -> &str {
+        "loop register"
+    }
+
+    fn formatted_instruction(&self) -> String {
+        String::from("")
+    }
+
+    fn eval(&self, state: &mut ProgramState) {
+        if state.run_mode() == RunMode::Verbose {
+            let snapshot = state.register_vec_to_string();
+            let instruction = format!("lpb {},{}", self.register_start, self.register_with_range_length);
+            println!("{:12} {} => {}", instruction, snapshot, snapshot);
+        }
+
+        let max_range_length_bigint: BigInt = 255.to_bigint().unwrap();
+
+        let mut cycles = 0;
+        loop {
+            let old_state: ProgramState = state.clone();
+
+            self.program.run(state);
+
+            let value: RegisterValue = old_state.get_register_value(self.register_with_range_length.clone());
+            let value_inner: &BigInt = &value.0;
+            let range_length: u8;
+            if value_inner.is_positive() {
+                if value_inner > &max_range_length_bigint {
+                    error!("Range length is beyond the ProgramState max length. Clamping range to 255.");
+                    range_length = 255;
+                } else {
+                    // Value is between 0 and 255, so it can be casted to an unsigned byte.
+                    range_length = value_inner.to_u8().unwrap();
+                }
+            } else {
+                // Value is negative. Clamp to 0 length.
+                range_length = 0;
+            }
+
+            let is_less: bool = state.is_less(
+                &old_state, 
+                self.register_start.clone(), 
+                range_length
+            );
+
+            if !is_less {
+
+                if state.run_mode() == RunMode::Verbose {
+                    println!("LOOP CYCLE EXIT");
+                }
+
+                // When the loop reaches its end, the previous state is restored.
+                *state = old_state.clone();
+                break;
+            }
+
+
+            cycles += 1;
+            if cycles > 1000 {
+                panic!("looped too many times");
+                // TODO: propagate info about problematic loops all the way
+                // to caller and their caller, and let them decide what to do about it.
+            }
+            if state.run_mode() == RunMode::Verbose {
+                println!("lpe");
+            }
+        }
+    }
+
+    fn accumulate_register_indexes(&self, register_vec: &mut Vec<RegisterIndex>) {
+        // Loop doesn't modify any registers
+        self.program.accumulate_register_indexes(register_vec);
+    }
+
+    fn update_call(&mut self, program_manager: &mut ProgramRunnerManager) {
+        self.program.update_call(program_manager);
+    }
+
+    fn accumulate_call_dependencies(&self, program_id_vec: &mut Vec<u64>) {
+        self.program.accumulate_call_dependencies(program_id_vec);
+    }
+}
