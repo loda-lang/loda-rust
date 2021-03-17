cedc3d25326df0e39fbfee624702571cd4cae8f4
Simon Strandgaard <neoneye@gmail.com>
2021-03-17 22:39:51 +0100

    Documentation

--- src/execute/node.rs
+++ src/execute/node.rs
@@ -1,5 +1,4 @@
 use super::{ProgramState, ProgramRunnerManager, RegisterIndex};
-// use std::result;
 
 pub struct ValidateCallError {}
 
@@ -13,11 +12,16 @@ pub trait Node {
     // Determine the number of registers required by this program.
     fn accumulate_register_indexes(&self, _register_vec: &mut Vec<RegisterIndex>) {}
     
-    // Establish links to other programs.
-    fn update_call(&mut self, _program_manager: &mut ProgramRunnerManager) {} // does nothing by default
-    
     // Gather a list of dependencies on other programs.
-    fn accumulate_call_dependencies(&self, _program_id_vec: &mut Vec<u64>) {} // does nothing by default
+    // Every CallNode depends on another program_id. These program_id's gets appended to the result.
+    // For most nodes, this is irrelevant, so this does nothing by default.
+    fn accumulate_call_dependencies(&self, _program_id_vec: &mut Vec<u64>) {}
+    
+    // Establish links to other programs.
+    // A CallNode looks up its program_id in the `ProgramRunnerManager`.
+    // If found, then establishes a link from the CallNode to the program.
+    // For most nodes, this is irrelevant, so this does nothing by default.
+    fn update_call(&mut self, _program_manager: &mut ProgramRunnerManager) {}
     
     // Check CallNode's have been linked with the program they depend on.
     // If there is a node with a missing link, then an error is returned.
