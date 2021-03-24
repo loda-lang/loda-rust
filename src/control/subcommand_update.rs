9c3bfdbc33b9c9f08e0b77acba805769c842c869
Simon Strandgaard <neoneye@gmail.com>
2021-03-24 03:46:14 +0100

    Generating a bloom filter with 10 terms.

--- src/control/subcommand_update.rs
+++ src/control/subcommand_update.rs
@@ -5,6 +5,12 @@ use std::path::Path;
 pub fn subcommand_update(_settings: &Settings) {
     println!("updating cache");
     let oeis_stripped_file = Path::new("/Users/neoneye/.loda/oeis/stripped");
-    let destination_file = Path::new("cache/fixed_length_sequence_5terms.json");
-    create_cache_file(oeis_stripped_file, destination_file, 5);
+    {
+        let destination_file = Path::new("cache/fixed_length_sequence_5terms.json");
+        create_cache_file(oeis_stripped_file, destination_file, 5);
+    }
+    {
+        let destination_file = Path::new("cache/fixed_length_sequence_10terms.json");
+        create_cache_file(oeis_stripped_file, destination_file, 10);
+    }
 }
