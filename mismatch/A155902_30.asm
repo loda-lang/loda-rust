7b46217dd863d82bc5c13b7f3664e0bfdb13526e
Simon Strandgaard <neoneye@gmail.com>
2021-03-26 19:28:26 +0100

    Keeping track of mismatches

---
+++ mismatch/A155902_30.asm
@@ -0,0 +1,9 @@
+; A155902 Arises in Connell's game, a variation of Wythoff's Nim game.
+; 0,1,2,4,5,7,8,10,11,12,14,15,17,18,20,21,22,24,25,27,28,30,31,32,34,35,37,38,40,41,MISMATCH
+; 30 terms correct.
+
+mov $4,10
+add $1,$0
+trn $0,$0
+mul $1,10
+div $1,7
