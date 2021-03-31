ebdfaa2d52df63e054552f961d2f15d90675a5ec
Simon Strandgaard <neoneye@gmail.com>
2021-03-31 19:15:13 +0200

    Made ParsedProgram struct available.

--- rust_project/src/parser/mod.rs
+++ rust_project/src/parser/mod.rs
@@ -1,16 +1,18 @@
 mod instruction;
 mod instruction_id;
 mod parameter_type;
+mod parse_parameters;
+mod parse_program;
 
-pub use instruction::{Instruction,InstructionParameter};
+pub use instruction::{Instruction, InstructionParameter};
 pub use instruction_id::{InstructionId, ParseInstructionIdError};
 pub use parameter_type::ParameterType;
+pub use parse_parameters::{ParseParametersError, parse_parameters};
+pub use parse_program::{ParsedProgram, ParseProgramError, parse_program};
 
 pub mod create_program;
 pub mod extract_parameter_re;
 pub mod extract_row_re;
-pub mod parse_parameters;
-pub mod parse_program;
 pub mod parse;
 pub mod remove_comment;
 pub mod validate_loops;
