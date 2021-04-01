d0a98d0b3e19ce8ef48e116ae5346e9107ba51a1
Simon Strandgaard <neoneye@gmail.com>
2021-04-01 17:18:47 +0200

    Correct program that unfortunately overflows using i64.

---
+++ resources/programs/match_overflow/readme.md
@@ -0,0 +1,2 @@
+This is for programs that computes the correct sequence, but takes overflows when using 64bit signed integer.
+
--- resources/programs/match_too_slow/A007887.asm
+++ resources/programs/match_too_slow/A007887.asm
@@ -1,6 +1,5 @@
 ; A007887: a(n) = Fibonacci(n) mod 9.
 ; 0,1,1,2,3,5,8,4,3,7,1,8,0,8,8,7,6,4,1,5,6,2,8,1,0,1,1,2,3,5,8,4,3,7,1,8,0,8,8,7,6,4,1,5,6,2,8,1,0,1,1,2,3,5,8,4,3,7,1,8,0,8,8,7,6,4,1,5,6,2,8,1,0,1,1,2,3,5,8,4,3
-; This program doesn't satisfy the requirement that the first 250 terms can be computed in 10 mio cycles.
 
 mov $1,$0
 cal $1,45
