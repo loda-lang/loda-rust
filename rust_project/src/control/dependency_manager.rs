a5e01b7c2e3fae2fea35c8fdddbc31d28d432736
Simon Strandgaard <neoneye@gmail.com>
2021-04-02 18:18:22 +0200

    test_20002_call_with_negative_parameter2 added

--- rust_project/src/control/dependency_manager.rs
+++ rust_project/src/control/dependency_manager.rs
@@ -252,8 +252,15 @@ mod tests {
     }
 
     #[test]
-    fn test_20001_call_with_negative_parameter() {
-        let mut dm: DependencyManager = dependency_manager_mock("tests/dependency_manager_call_with_negative_parameter");
+    fn test_20001_call_with_negative_parameter1() {
+        let mut dm: DependencyManager = dependency_manager_mock("tests/dependency_manager_call_with_negative_parameter1");
+        let runner: Rc::<ProgramRunner> = dm.load(666).unwrap();
+        assert_eq!(runner.inspect(10), "BOOM");
+    }
+
+    #[test]
+    fn test_20002_call_with_negative_parameter2() {
+        let mut dm: DependencyManager = dependency_manager_mock("tests/dependency_manager_call_with_negative_parameter2");
         let runner: Rc::<ProgramRunner> = dm.load(666).unwrap();
         assert_eq!(runner.inspect(10), "BOOM");
     }
