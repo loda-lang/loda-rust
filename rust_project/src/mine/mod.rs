42ae35ecaf6e9c1ed4fd2b4af606fdf3bc171467
Simon Strandgaard <neoneye@gmail.com>
2021-04-03 10:07:54 +0200

    Renamed from load_dontmine_file() to load_program_ids_csv_file().

--- rust_project/src/mine/mod.rs
+++ rust_project/src/mine/mod.rs
@@ -1,3 +1,6 @@
+mod load_program_ids_csv_file;
+
+pub use load_program_ids_csv_file::load_program_ids_csv_file;
+
 pub mod check_fixed_length_sequence;
-pub mod dont_mine;
 pub mod mine_experiments;
