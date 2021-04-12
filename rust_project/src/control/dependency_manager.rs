638c0a1e7be4f2c838287fce39edfde3d040cb5d
Simon Strandgaard <neoneye@gmail.com>
2021-04-12 19:50:36 +0200

    Disabled code for rejecting negative input for call instruction.

--- rust_project/src/control/dependency_manager.rs
+++ rust_project/src/control/dependency_manager.rs
@@ -247,19 +247,19 @@ mod tests {
         assert_eq!(error, DependencyManagerError::CannotLoadFile);
     }
 
-    #[test]
-    fn test_20001_call_with_negative_parameter1() {
-        let mut dm: DependencyManager = dependency_manager_mock("tests/call_with_negative_parameter1");
-        let runner: Rc::<ProgramRunner> = dm.load(666).unwrap();
-        assert_eq!(runner.inspect(10), "BOOM");
-    }
+    // #[test]
+    // fn test_20001_call_with_negative_parameter1() {
+    //     let mut dm: DependencyManager = dependency_manager_mock("tests/call_with_negative_parameter1");
+    //     let runner: Rc::<ProgramRunner> = dm.load(666).unwrap();
+    //     assert_eq!(runner.inspect(10), "BOOM");
+    // }
 
-    #[test]
-    fn test_20002_call_with_negative_parameter2() {
-        let mut dm: DependencyManager = dependency_manager_mock("tests/call_with_negative_parameter2");
-        let runner: Rc::<ProgramRunner> = dm.load(666).unwrap();
-        assert_eq!(runner.inspect(10), "BOOM");
-    }
+    // #[test]
+    // fn test_20002_call_with_negative_parameter2() {
+    //     let mut dm: DependencyManager = dependency_manager_mock("tests/call_with_negative_parameter2");
+    //     let runner: Rc::<ProgramRunner> = dm.load(666).unwrap();
+    //     assert_eq!(runner.inspect(10), "BOOM");
+    // }
 
     #[test]
     fn test_30001_live_register1() {
