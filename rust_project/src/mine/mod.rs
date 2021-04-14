0659c059221c119b2ff2c323792c3690ecb4f78f
Simon Strandgaard <neoneye@gmail.com>
2021-04-14 16:48:50 +0200

    renamed file

--- rust_project/src/mine/mod.rs
+++ rust_project/src/mine/mod.rs
@@ -4,7 +4,7 @@ mod genome;
 mod genome_item;
 mod genome_mutate_context;
 mod load_program_ids_csv_file;
-mod load_program_popularity_csv_file;
+mod popular_program_container;
 mod run_miner_loop;
 mod save_candidate_program;
 
@@ -14,6 +14,6 @@ pub use genome_mutate_context::GenomeMutateContext;
 pub use genome::{Genome, MutateGenome};
 pub use genome_item::{GenomeItem, MutateValue};
 pub use load_program_ids_csv_file::load_program_ids_csv_file;
-pub use load_program_popularity_csv_file::PopularProgramContainer;
+pub use popular_program_container::PopularProgramContainer;
 pub use run_miner_loop::run_miner_loop;
 pub use save_candidate_program::save_candidate_program;
