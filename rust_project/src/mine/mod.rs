8beedb4c3bd5500e758b686005b7976d6fb38137
Simon Strandgaard <neoneye@gmail.com>
2021-04-19 09:37:24 +0200

    Loading the "program_creation_dates.csv".

--- rust_project/src/mine/mod.rs
+++ rust_project/src/mine/mod.rs
@@ -5,6 +5,7 @@ mod genome_item;
 mod genome_mutate_context;
 mod load_program_ids_csv_file;
 mod popular_program_container;
+mod recent_program_container;
 mod run_miner_loop;
 mod save_candidate_program;
 
@@ -15,5 +16,6 @@ pub use genome::{Genome, MutateGenome};
 pub use genome_item::{GenomeItem, MutateValue};
 pub use load_program_ids_csv_file::load_program_ids_csv_file;
 pub use popular_program_container::PopularProgramContainer;
+pub use recent_program_container::RecentProgramContainer;
 pub use run_miner_loop::run_miner_loop;
 pub use save_candidate_program::save_candidate_program;
