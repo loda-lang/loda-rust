2647e0bfedcec0a72aed0eb8f9df93c728eb4919
Simon Strandgaard <neoneye@gmail.com>
2021-04-22 19:00:36 +0200

    Keeping track of mismatches

---
+++ resources/programs/mismatch/A182777_55.asm
@@ -0,0 +1,8 @@
+; A182777: Beatty sequence for 3-sqrt(3).
+; 1,2,3,5,6,7,8,10,11,12,13,15,16,17,19,20,21,22,24,25,26,27,29,30,31,32,34,35,36,38,39,40,41,43,44,45,46,48,49,50,snip,67,68,69,MISMATCH
+; 55 terms correct.
+
+mul $0,2
+add $0,1
+cal $0,74065 ; Numerators a(n) of fractions slowly converging to sqrt(3)
+mov $1,$0
