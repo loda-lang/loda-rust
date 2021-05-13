55d3373bda31c70cd25632daa64b4861fcf871a9
Simon Strandgaard <neoneye@gmail.com>
2021-05-13 18:11:33 +0200

    Keeping track of mismatches

---
+++ resources/programs/mismatch/A249243_41.asm
@@ -0,0 +1,7 @@
+; A249243: Floor(1/((1 + 1/n)^(n + 1)) - e).
+; 0,1,2,2,3,4,5,5,6,7,8,8,9,10,11,11,12,13,14,14,15,16,16,17,18,19,19,20,21,22,22,23,24,25,25,26,27,28,28,29,30,MISMATCH
+; 41 correct terms.
+
+cal $0,186291 ; Adjusted joint rank sequence of (f(i)) and (g(j)) with f(i) after g(j) when f(i)=g(j), where f and g are the squares and pentagonal numbers. Complement of A186290.
+div $0,3
+mov $1,$0
