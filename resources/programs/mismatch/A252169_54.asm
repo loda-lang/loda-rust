1b098766bcc740f252897380d0ff7e0eb2e8621b
Simon Strandgaard <neoneye@gmail.com>
2021-05-13 18:08:09 +0200

    Keeping track of mismatches

---
+++ resources/programs/mismatch/A252169_54.asm
@@ -0,0 +1,8 @@
+; A252169: Beatty sequence for sqrt(Pi*phi) where phi is the golden ratio A001622.
+; 2,4,6,9,11,13,15,18,20,22,24,27,29,31,33,36,38,40,42,45,47,49,51,54,56,58,60,63,65,67,69,72,74,76,78,81,83,85,87,90,snip,117,119,121,MISMATCH
+; 54 correct terms.
+
+mov $1,$0
+add $1,1
+mul $1,9
+div $1,4
