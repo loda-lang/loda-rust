2f9c26f393581c3b63a352abdeb48d76d61470f8
Simon Strandgaard <neoneye@gmail.com>
2021-05-13 17:46:16 +0200

    Keeping track of mismatches

---
+++ resources/programs/mismatch/A172278_162.asm
@@ -0,0 +1,11 @@
+; A172278: a(n) = floor(n*(sqrt(13)-sqrt(2))).
+; 0,2,4,6,8,10,13,15,17,19,21,24,26,28,30,32,35,37,39,41,43,46,48,50,52,54,56,59,61,63,65,67,70,72,74,76,78,81,83,85,snip,350,352,355,MISMATCH
+; 162 terms correct.
+
+mov $1,$0
+cmp $1,0
+cmp $1,0
+sub $0,1
+max $0,0
+cal $0,276383 ; Complement of A158919: complementary Beatty sequence to the Beatty sequence defined by the tribonacci constant tau = A058265.
+mul $1,$0
