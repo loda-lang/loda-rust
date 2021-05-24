8047d041d2d6a9ee68a002ff2cdc60cceb056dfd
Simon Strandgaard <neoneye@gmail.com>
2021-05-24 17:20:23 +0200

    Slightly shorter program

--- resources/programs/mismatch/A286989_65.asm
+++ resources/programs/mismatch/A286989_65.asm
@@ -2,7 +2,6 @@
 ; 1,2,4,5,7,9,10,12,14,15,17,18,20,22,23,25,27,28,30,31,33,35,36,38,40,41,43,45,46,48,49,51,53,54,56,58,59,61,62,64,snip,102,103,105,MISMATCH
 ; 65 correct terms.
 
-cal $0,186322 ; Adjusted joint rank sequence of (f(i)) and (g(j)) with f(i) after g(j) when f(i)=g(j), where f and g are the squares and heptagonal numbers. Complement of A186323.
-sub $0,1
-dif $4,$0
+cal $0,186322 ; Adjusted joint rank sequence of (f(i)) and (g(j)) with f(i) after g(j) when f(i)=g(j), where f and g are the squares and heptagonal numbers.  Complement of A186323.
+trn $0,1
 mov $1,$0
