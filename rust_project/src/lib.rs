77343f7fdd24d0f1859520430ab6b88b749542de
Simon Strandgaard <neoneye@gmail.com>
2021-08-04 00:00:19 +0200

    Yay, just executed the first LODA program in Firefox!

--- rust_project/src/lib.rs
+++ rust_project/src/lib.rs
@@ -5,14 +5,20 @@ use log::{Log,Metadata,Record,LevelFilter};
 #[macro_use]
 extern crate log;
 
-// mod config;
-// mod control;
-// mod execute;
-// mod mine;
-// mod parser;
-// mod oeis;
-// mod util;
-// use control::*;
+mod config;
+mod control;
+mod execute;
+mod parser;
+mod oeis;
+mod util;
+
+use std::path::PathBuf;
+use control::DependencyManager;
+use execute::{NodeLoopLimit, ProgramCache, ProgramId, ProgramRunner, RegisterValue, RunMode};
+use execute::NodeRegisterLimit;
+use execute::node_binomial::NodeBinomialLimit;
+use execute::node_power::NodePowerLimit;
+
 
 #[derive(Clone)]
 struct MyCustomLog {
@@ -87,8 +93,59 @@ fn eval_loda_program() {
     info!("I'm info");
     warn!("I'm warn");
 
-    // let program_id: u64 = 40;
-    // let number_of_terms: u64 = 20;
-    // let mode = SubcommandEvaluateMode::PrintTerms;
-    // subcommand_evaluate(program_id, number_of_terms, mode);
+    const PROGRAM: &str = r#"        
+    mov $1,2
+    pow $1,$0
+    "#;
+    let mut dm = DependencyManager::new(
+        PathBuf::from("non-existing-dir"),
+    );
+    let source_code: String = PROGRAM.to_string();
+    let runner: ProgramRunner = dm.parse(ProgramId::ProgramWithoutId, &source_code).unwrap();
+    runner.my_print_terms(10);
+}
+
+impl ProgramRunner {
+    fn my_print_terms(&self, count: u64) {
+        if count >= 0x7fff_ffff_ffff_ffff {
+            error!("Value is too high. Cannot be converted to 64bit signed integer.");
+            return;
+        }
+        if count < 1 {
+            error!("Expected number of terms to be 1 or greater.");
+            return;
+        }
+        let mut cache = ProgramCache::new();
+        let step_count_limit: u64 = 10000000;
+        let mut step_count: u64 = 0;
+        for index in 0..(count as i64) {
+            let input = RegisterValue::from_i64(index);
+            let result_run = self.run(
+                &input, 
+                RunMode::Verbose, 
+                &mut step_count, 
+                step_count_limit,
+                NodeRegisterLimit::Unlimited,
+                NodeBinomialLimit::Unlimited,
+                NodeLoopLimit::Unlimited,
+                NodePowerLimit::Unlimited,
+                &mut cache
+            );
+            let output: RegisterValue = match result_run {
+                Ok(value) => value,
+                Err(error) => {
+                    error!("Failure while computing term {}, error: {:?}", index, error);
+                    return;
+                }
+            };
+            if index == 0 {
+                info!("{}", output.0);
+                continue;
+            }
+            info!(",{}", output.0);
+        }
+        info!("\n");
+        debug!("steps: {}", step_count);
+        debug!("cache: {}", cache.hit_miss_info());
+    }
 }
