e9f2b1c568e95b84bbd938cbf5a284534f34c6f2
Simon Strandgaard <neoneye@gmail.com>
2021-08-03 23:40:41 +0200

    Bridging Rust's debug!() and error!() to the JS console.log().

--- rust_project/src/lib.rs
+++ rust_project/src/lib.rs
@@ -1,5 +1,10 @@
 use wasm_bindgen::prelude::*;
 
+use log::{Log,Metadata,Record,LevelFilter};
+
+#[macro_use]
+extern crate log;
+
 // mod config;
 // mod control;
 // mod execute;
@@ -9,6 +14,42 @@ use wasm_bindgen::prelude::*;
 // mod util;
 // use control::*;
 
+#[derive(Clone)]
+struct MyCustomLog {
+}
+
+impl Log for MyCustomLog {
+    fn enabled(&self, _metadata: &Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &Record) {
+        let level_string = record.level().to_string();
+
+        let target = if !record.target().is_empty() {
+            record.target()
+        } else {
+            record.module_path().unwrap_or_default()
+        };
+
+        let message = format!("{:<5} [{}] {}", level_string, target, record.args());
+        console::log(&message);
+    }
+
+    fn flush(&self) {
+    }
+}
+
+impl MyCustomLog {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn init(&mut self) -> Result<(), log::SetLoggerError> {
+        log::set_max_level(LevelFilter::Trace);
+        log::set_boxed_logger(Box::new(self.clone()))
+    }
+}
 
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
@@ -38,6 +79,14 @@ pub fn myjsfunc_from_wasm() {
 }
 
 fn eval_loda_program() {
+    MyCustomLog::new().init().unwrap();
+
+    trace!("I'm trace");
+    debug!("I'm debug");
+    error!("I'm error");
+    info!("I'm info");
+    warn!("I'm warn");
+
     // let program_id: u64 = 40;
     // let number_of_terms: u64 = 20;
     // let mode = SubcommandEvaluateMode::PrintTerms;
