61941d30ccfbacccde5f9ccf76d939d312d39441
Simon Strandgaard <neoneye@gmail.com>
2021-09-01 22:00:09 +0200

    Replaced "loda_program_rootdir" with "loda_programs_repository".

--- script/config.rb
+++ script/config.rb
@@ -4,13 +4,17 @@ require 'singleton'
 class Config
     include Singleton
     
-    attr_reader :loda_program_rootdir
+    attr_reader :loda_programs_repository
     attr_reader :loda_cpp_repository
     
     def initialize
         path = File.join(ENV['HOME'], '/.loda-rust/config.toml')
         dict = TomlRB.load_file(path)
-        @loda_program_rootdir = dict['loda_program_rootdir']
+        @loda_programs_repository = dict['loda_programs_repository']
         @loda_cpp_repository = dict['loda_cpp_repository']
+    end
+
+    def loda_program_rootdir
+        File.join(@loda_programs_repository, 'oeis')
     end
 end
