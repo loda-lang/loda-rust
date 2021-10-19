f5c330e1adee55b57c2373e76eeee565db704994
Simon Strandgaard <neoneye@gmail.com>
2021-10-19 19:00:17 +0200

    Looking up the names for each program

--- script/insert_oeis_names_into_program.rb
+++ script/insert_oeis_names_into_program.rb
@@ -4,12 +4,17 @@
 
 =end
 
-require 'csv'
+require 'set'
 require_relative 'config'
 
 LODA_PROGRAMS_OEIS = Config.instance.loda_programs_oeis
 unless File.exist?(LODA_PROGRAMS_OEIS)
     raise "No such dir #{LODA_PROGRAMS_OEIS}, cannot run script"
+end
+
+OEIS_NAMES_FILE = Config.instance.oeis_names_file
+unless File.exist?(OEIS_NAMES_FILE)
+    raise "No such file #{OEIS_NAMES_FILE}, cannot run script"
 end
 
 # git: list new files only
@@ -33,15 +38,25 @@ def absolute_paths_for_unstaged_programs
     paths3
 end
 
-def process_program_file(path)
+def extract_oeis_ids_from_program_file(path)
     path =~ /\bA0*(\d+)[.]asm$/
     program_id = $1.to_i
     if program_id == 0
-        puts "Mismatch for #{path}"
-        return
+        raise "Unable to process file at path: #{path}"
     end
-    puts "match: #{program_id}"
     content = IO.read(path)
+    sequence_instructions = content.scan(/^\s*seq .*,\s*(\d+)$/).flatten
+    sequence_program_ids = sequence_instructions.map { |seq_program_id| seq_program_id.to_i }
+    # puts "program: #{program_id} depends on: #{sequence_program_ids}"
+    return [program_id] + sequence_program_ids
+end
+
+def extract_oeis_ids_from_program_files(paths)
+    program_ids = []
+    paths.each do |path|
+        program_ids += extract_oeis_ids_from_program_file(path)
+    end
+    program_ids
 end
 
 paths = absolute_paths_for_unstaged_programs
@@ -53,7 +68,21 @@ end
 
 puts "Number of unstaged .asm files: #{paths.count}"
 
-paths.each do |path|
-    process_program_file(path)
-end
+# Harvest all the program ids that needs to have their name looked up
+program_ids = extract_oeis_ids_from_program_files(paths)
+program_ids_set = program_ids.to_set
+puts "Will lookup names for these program ids: #{program_ids_set.to_a.sort}" 
 
+approx_row_count = 350000
+File.new(OEIS_NAMES_FILE, "r").each_with_index do |line, index|
+    if (index % 30000) == 0
+        percentage = (100 * index) / approx_row_count
+        puts "progress %#{percentage}  #{index}/#{approx_row_count}"
+    end
+    next unless line =~ /^A0*(\d+) (.+)$/
+    program_id = $1.to_i
+    name = $2
+    if program_ids_set.include?(program_id)
+        puts "program_id: #{program_id} name: #{name}"
+    end
+end
