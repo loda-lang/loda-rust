bb73ce2e7c23f63527759cc689661551870af985
Simon Strandgaard <neoneye@gmail.com>
2021-11-17 20:31:08 +0100

    identify_all_valid_programs() added.

--- rust_project/loda-rust-cli/src/subcommand_update.rs
+++ rust_project/loda-rust-cli/src/subcommand_update.rs
@@ -1,10 +1,121 @@
 use loda_rust_core;
 use loda_rust_core::config::Config;
 use crate::mine::{create_cache_files, load_program_ids_csv_file};
+use crate::mine::find_asm_files_recursively;
+use crate::mine::program_id_from_path;
+use loda_rust_core::control::{DependencyManager,DependencyManagerFileSystemMode};
+use loda_rust_core::execute::{NodeLoopLimit, ProgramCache, ProgramRunner, RegisterValue, RunMode};
+use loda_rust_core::execute::NodeRegisterLimit;
+use loda_rust_core::execute::node_binomial::NodeBinomialLimit;
+use loda_rust_core::execute::node_power::NodePowerLimit;
 use std::path::{Path, PathBuf};
 use std::collections::HashSet;
 use std::iter::FromIterator;
 use std::time::Instant;
+use std::rc::Rc;
+
+fn identify_all_valid_programs() {
+    let config = Config::load();
+    let loda_programs_oeis_dir: PathBuf = config.loda_programs_oeis_dir();
+
+    // Obtain paths to loda asm files
+    let paths: Vec<PathBuf> = find_asm_files_recursively(&loda_programs_oeis_dir);
+    debug!("number of paths: {:?}", paths.len());
+
+    // Extract program_ids from paths
+    let mut program_ids: Vec<u32> = vec!();
+    for path in paths {
+        let program_id: u32 = match program_id_from_path(&path) {
+            Some(program_id) => program_id,
+            None => {
+                warn!("Unable to extract program_id from {:?}", path);
+                continue;
+            }
+        };
+        program_ids.push(program_id);
+    }
+    program_ids.sort();
+    debug!("number of program_ids: {:?}", program_ids.len());
+    if program_ids.len() > 10 {
+        let first_items = &program_ids[0..10];
+        debug!("program_ids first: {:?}", first_items);
+        let last_items = &program_ids[program_ids.len()-10..];
+        debug!("program_ids last: {:?}", last_items);
+    }
+
+    // Run all the programs.
+    // Reject the programs that is having difficulties running.
+    let mut dm = DependencyManager::new(
+        DependencyManagerFileSystemMode::System,
+        loda_programs_oeis_dir,
+    );
+    let mut cache = ProgramCache::new();
+    let mut progress_time = Instant::now();
+    let program_ids_len: usize = program_ids.len();
+    for (index, program_id) in program_ids.iter().enumerate() {
+        let elapsed: u128 = progress_time.elapsed().as_millis();
+        if elapsed >= 1000 {
+            let percent: f32 = ((index * 100) as f32) / (program_ids_len as f32);
+            debug!("progress: {:.2}%  {:?} / {:?}", percent, index, program_ids_len);
+            progress_time = Instant::now();
+        }
+        let program_id64 = *program_id as u64;
+        let program_runner: Rc::<ProgramRunner> = match dm.load(program_id64) {
+            Ok(value) => value,
+            Err(error) => {
+                error!("Cannot load program {:?}: {:?}", program_id, error);
+                continue;
+            }
+        };
+        program_runner.compute_terms(10, &mut cache);
+    }
+
+}
+
+trait ComputeTerms {
+    fn compute_terms(&self, count: u64, cache: &mut ProgramCache);
+}
+
+impl ComputeTerms for ProgramRunner {
+    fn compute_terms(&self, count: u64, cache: &mut ProgramCache) {
+        if count >= 0x7fff_ffff_ffff_ffff {
+            panic!("Value is too high. Cannot be converted to 64bit signed integer.");
+        }
+        if count < 1 {
+            panic!("Expected number of terms to be 1 or greater.");
+        }
+        let step_count_limit: u64 = 1000000000;
+        let mut step_count: u64 = 0;
+        for index in 0..(count as i64) {
+            let input = RegisterValue::from_i64(index);
+            let result_run = self.run(
+                &input, 
+                RunMode::Silent, 
+                &mut step_count, 
+                step_count_limit,
+                NodeRegisterLimit::Unlimited,
+                NodeBinomialLimit::Unlimited,
+                NodeLoopLimit::Unlimited,
+                NodePowerLimit::Unlimited,
+                cache
+            );
+            let output: RegisterValue = match result_run {
+                Ok(value) => value,
+                Err(error) => {
+                    error!("Failure while computing term {}, error: {:?}", index, error);
+                    return;
+                }
+            };
+            // if index == 0 {
+            //     print!("{}", output.0);
+            //     continue;
+            // }
+            // print!(",{}", output.0);
+        }
+        // print!("\n");
+        // debug!("steps: {}", step_count);
+    }
+}
 
 fn obtain_dontmine_program_ids(loda_rust_repository: &Path) -> HashSet<u32> {
     let relative_path = Path::new("resources/dont_mine.csv");
