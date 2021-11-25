44ec68484f2bd6ba1debd7b2ae28084723af02ee
Simon Strandgaard <neoneye@gmail.com>
2021-11-25 22:06:02 +0100

    Proof of concept determining what NOT to mine.. being performed in Rust. Eliminating the some Ruby code and some manual steps.

--- rust_project/loda-rust-cli/src/subcommand_update.rs
+++ rust_project/loda-rust-cli/src/subcommand_update.rs
@@ -12,6 +12,87 @@ use std::io;
 use std::error::Error;
 use crate::mine::find_asm_files_recursively;
 use crate::mine::program_id_from_path;
+use crate::mine::program_ids_from_paths;
+
+// What NOT to be mined!
+//
+// If a sequence already has been mined, then it's no longer a top priority to mine it again.
+// This may change in the future.
+//
+// If a sequence is on the loda-programs deny.txt list, then ignore it.
+// This is for duplicates in oeis and things to be ignored.
+//
+// If the same program is frequently turning up a false positive, then ignore it.
+// This is done by adding the program to the "mismatches" directory.
+struct DontMine {
+    config: Config,
+    program_ids: Vec<u32>
+}
+
+impl DontMine {
+    fn create() -> Self {
+        let mut instance = Self {
+            config: Config::load(),
+            program_ids: vec!()
+        };
+        {
+            let program_ids: Vec<u32> = instance.process_existing_programs();
+            instance.program_ids.extend(program_ids);
+        }
+        {
+            let program_ids: Vec<u32> = instance.process_mismatches();
+            instance.program_ids.extend(program_ids);
+        }
+        {
+            let program_ids: Vec<u32> = instance.process_loda_programs_deny_file();
+            instance.program_ids.extend(program_ids);
+        }
+        instance
+    }
+
+    fn process_existing_programs(&self) -> Vec<u32> {
+        let dir_containing_programs: PathBuf = self.config.loda_programs_oeis_dir();
+        let paths: Vec<PathBuf> = find_asm_files_recursively(&dir_containing_programs);
+        let program_ids: Vec<u32> = program_ids_from_paths(paths);
+        println!("number of existing programs: {:?}", program_ids.len());
+        program_ids
+    }
+
+    fn process_mismatches(&self) -> Vec<u32> {
+        let dir_containing_mismatches: PathBuf = self.config.loda_rust_mismatches();
+        let paths: Vec<PathBuf> = find_asm_files_recursively(&dir_containing_mismatches);
+        let program_ids: Vec<u32> = program_ids_from_paths(paths);
+        println!("number of mismatches: {:?}", program_ids.len());
+        program_ids
+    }
+
+    fn process_loda_programs_deny_file(&self) -> Vec<u32> {
+        let path = self.config.loda_programs_oeis_deny_file();
+        let program_ids: Vec<u32> = match load_program_ids_from_deny_file(&path) {
+            Ok(value) => value,
+            Err(error) => {
+                error!("Unable to read the file: {:?} error: {:?}", path, error);
+                return vec!();
+            }
+        };
+        println!("number of programs in the 'deny.txt' file: {:?}", program_ids.len());
+        program_ids
+    }
+
+    fn save(&self) {
+        println!("saving, number of program_ids: {:?}", self.program_ids.len());
+        let output_path: PathBuf = self.config.cache_dir_dont_mine_file();
+        match create_csv_file(&self.program_ids, &output_path) {
+            Ok(_) => {
+                println!("save ok");
+            },
+            Err(error) => {
+                println!("save error: {:?}", error);
+            }
+        }
+    }
+}
+
 
 fn process_mismatches() {
     let config = Config::load();
@@ -33,7 +114,7 @@ fn process_loda_programs_deny_file() {
         }
     };
     println!("deny.txt program_ids.len(): {:?}", program_ids.len());
-    match create_csv_file(program_ids, &output_path) {
+    match create_csv_file(&program_ids, &output_path) {
         Ok(_) => {
             println!("ok");
         },
@@ -43,7 +124,7 @@ fn process_loda_programs_deny_file() {
     }
 }
 
-fn create_csv_file(program_ids: Vec<u32>, output_path: &Path) -> Result<(), Box<dyn Error>> {
+fn create_csv_file(program_ids: &Vec<u32>, output_path: &Path) -> Result<(), Box<dyn Error>> {
     let mut wtr = csv::Writer::from_path(output_path)?;
     wtr.write_record(&["program id"])?;
     for program_id in program_ids {
@@ -55,9 +136,10 @@ fn create_csv_file(program_ids: Vec<u32>, output_path: &Path) -> Result<(), Box<
 }
 
 fn obtain_dontmine_program_ids(loda_rust_repository: &Path) -> HashSet<u32> {
-    let relative_path = Path::new("resources/dont_mine.csv");
-    let path = loda_rust_repository.join(relative_path);
-
+    // let relative_path = Path::new("resources/dont_mine.csv");
+    // let path = loda_rust_repository.join(relative_path);
+    let config = Config::load();
+    let path = config.cache_dir_dont_mine_file();
     let program_ids: Vec<u32> = match load_program_ids_csv_file(&path) {
         Ok(value) => value,
         Err(error) => {
@@ -86,6 +168,8 @@ fn populate_bloomfilter() {
 pub fn subcommand_update() {
     let start_time = Instant::now();
     println!("update begin");
+    let dontmine = DontMine::create();
+    dontmine.save();
     // process_mismatches();
     // process_loda_programs_deny_file();
     let _ = validate_programs();
--- rust_project/loda-rust-core/src/config/config.rs
+++ rust_project/loda-rust-core/src/config/config.rs
@@ -60,6 +60,12 @@ impl Config {
         path
     }
 
+    pub fn cache_dir_dont_mine_file(&self) -> PathBuf {
+        let path = self.cache_dir().join("dont_mine.csv");
+        assert!(path.is_absolute());
+        PathBuf::from(path)
+    }
+
     pub fn mine_event_dir(&self) -> PathBuf {
         let name = Path::new("mine-event");
         let path = self.basedir.join(name);
