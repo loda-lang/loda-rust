3eca08be6afa68b011680511c5bfcaf25cdff390
Simon Strandgaard <neoneye@gmail.com>
2021-12-05 11:59:16 +0100

    Sending commands to the worker thread.

--- rust_project/loda-rust-web/worker.js
+++ rust_project/loda-rust-web/worker.js
@@ -1,6 +1,20 @@
+function commandRange(parameters) {
+    self.postMessage(`commandRange ${parameters}`);
+}
 
 self.onmessage = e => {
-    self.postMessage('onmessage');
+    // command = the first element of the input array
+    // parameters = except the first element of the input array
+    var parameters = e.data.slice();
+    const command = parameters.shift();
+    switch (command) {
+    case "range":
+        commandRange(parameters);
+        break;
+    default:
+        self.postMessage(`unknown: ${e.data}`);
+        break;
+    }
 };
 
 postMessage('started');
