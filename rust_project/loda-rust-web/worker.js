0b0db0bde90411f12c48968f1763ade50852827c
Simon Strandgaard <neoneye@gmail.com>
2021-12-07 20:38:54 +0100

    Mimic that it takes 100 ms for computing a term.

--- rust_project/loda-rust-web/worker.js
+++ rust_project/loda-rust-web/worker.js
@@ -1,20 +1,31 @@
-function commandRange(parameters) {
-    self.postMessage(`commandRange ${parameters}`);
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-self.onmessage = e => {
+async function commandRange(parameters) {
+    self.postMessage(`commandRange ${parameters}`);
+    const rangeStart = parameters[0];
+    const rangeLength = parameters[1];
+    for (var i = rangeStart; i < rangeLength; i++) {
+        console.log("step", i);
+        await sleep(100);
+        self.postMessage(['result', i]);
+    }
+}
+
+addEventListener('message', async (e) => {
     // command = the first element of the input array
     // parameters = except the first element of the input array
     var parameters = e.data.slice();
     const command = parameters.shift();
     switch (command) {
     case "range":
-        commandRange(parameters);
+        await commandRange(parameters);
         break;
     default:
         self.postMessage(`unknown: ${e.data}`);
         break;
     }
-};
+});
 
 postMessage('started');
