edf438a9540eb578c083a2b3e317592f452baad6
Simon Strandgaard <neoneye@gmail.com>
2021-12-11 20:53:08 +0100

    I'm experiencing that console.log() gets invoked twice when called inside a web worker.

--- rust_project/loda-rust-web/worker.js
+++ rust_project/loda-rust-web/worker.js
@@ -2,33 +2,42 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function commandRange(parameters) {
-    // self.postMessage([`commandRange ${parameters}`]);
-    console.log("commandRange");
-    const rangeStart = parameters.rangeStart;
-    const rangeLength = parameters.rangeLength;
-    for (var i = rangeStart; i < rangeLength; i++) {
-        console.log("step", i);
-        await sleep(100);
-        self.postMessage({
-            fn: 'result', 
-            value: i
+class MyWorker {
+    constructor(workerOwner) {
+        this.mWorkerOwner = workerOwner;
+    }
+
+    debug(message) {
+        this.mWorkerOwner.postMessage({
+            fn: 'debug',
+            message: message
         });
+    }
+
+    async commandRange(parameters) {
+        this.debug("commandRange");
+        const rangeStart = parameters.rangeStart;
+        const rangeLength = parameters.rangeLength;
+        for (var i = rangeStart; i < rangeLength; i++) {
+            // this.debug(`step ${i}`);
+            await sleep(100);
+            this.mWorkerOwner.postMessage({
+                fn: 'result', 
+                value: i
+            });
+        }
     }
 }
 
+const myWorker = new MyWorker(this);
+  
 addEventListener('message', async (e) => {
-    console.log("worker.js addEventListener message. before");
     switch (e.data.fn) {
     case "range":
-        await commandRange(e.data);
-        break;
-    case "setup":
-        console.log("setup:", e.data.instanceId);
+        await myWorker.commandRange(e.data);
         break;
     default:
-        console.error(`worker.message: unknown: ${e.data}`);
+        console.error(`worker.message: unknown: ${e.data.fn} ${e.data}`);
         break;
     }
-    console.log("worker.js addEventListener message. after");
-});
+}, false);
