5b5232fc79259b3396372612e05b8643d9d368bc
Simon Strandgaard <neoneye@gmail.com>
2021-12-15 21:48:22 +0100

    Don't send commands to the worker before it has fully initialized.

--- rust_project/loda-rust-web/worker.js
+++ rust_project/loda-rust-web/worker.js
@@ -28,27 +28,40 @@ class MyWorker {
     }
 
     async commandExecuteRange(parameters) {
-        this.debug("commandExecuteRange");
-        const rangeStart = this.mRangeStart;
-        const rangeLength = this.mRangeLength;
-        for (var i = rangeStart; i < rangeLength; i++) {
-            // this.debug(`step ${i}`);
+        console.log("commandExecuteRange before");
+        const index0 = this.mRangeStart;
+        const index1 = this.mRangeStart + this.mRangeLength;
+        for (var i = index0; i < index1; i++) {
+            await this.executeIndex(i);
+        }
+        console.log("commandExecuteRange after");
+    }
 
-            const valueString = await this.mDependencyManager.clone().execute_current_program(i);
+    async executeIndex(index) {
+        console.log(`executeIndex before step ${index}`);
+
+        await sleep(100);
+
+        try {
+            const valueString = await this.mDependencyManager.clone().execute_current_program(index);
             // console.log("computed value: ", valueString);
-        
-            // await sleep(100);
             this.mWorkerOwner.postMessage({
                 fn: 'result', 
                 valueString: valueString
             });
         }
+        catch(err) {
+            console.log("Exception inside execute_current_program: ", err);
+        }    
+        
+        console.log("executeNext after");
     }
 
     async commandCompile(parameters) {
-        this.debug("commandCompile");
+        console.log("commandCompile before");
         const sourceCode = parameters.sourceCode;
         await this.mDependencyManager.clone().run_source_code(sourceCode);
+        console.log("commandCompile after");
     }
 }
 
@@ -70,15 +83,15 @@ async function init_worker(owner) {
 
     const dm = new WebDependencyManager();
 
-    dm.increment();
+    // dm.increment();
     // await dm.clone().run_source_code("mov $1,2\npow $1,$0");
-    await dm.clone().run_source_code("seq $0,40\nmul $0,-1");
+    // await dm.clone().run_source_code("seq $0,40\nmul $0,-1");
 
-    const index = 2;
-    const value = await dm.clone().execute_current_program(index);
-    console.log("computed value: ", value);
+    // const index = 2;
+    // const value = await dm.clone().execute_current_program(index);
+    // console.log("computed value: ", value);
 
-    dm.clone().print_stats();
+    // dm.clone().print_stats();
 
     const myWorker = new MyWorker(owner, dm);
   
