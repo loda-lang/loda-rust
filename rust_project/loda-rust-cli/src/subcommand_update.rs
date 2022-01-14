bedb89dce37f13245e3c728d3213df6c3834bf04
Simon Strandgaard <neoneye@gmail.com>
2022-01-14 23:44:53 +0100

    Changed ordering of tasks.

--- rust_project/loda-rust-cli/src/subcommand_update.rs
+++ rust_project/loda-rust-cli/src/subcommand_update.rs
@@ -4,9 +4,9 @@ use crate::mine::{DontMine, HistogramInstructionConstantAnalyzer, NgramGenerator
 pub fn subcommand_update() {
     let start_time = Instant::now();
     println!("update begin");
-    NgramGenerator::run();
     DontMine::run();
     HistogramInstructionConstantAnalyzer::run();
+    NgramGenerator::run();
     ValidatePrograms::run();
     PopulateBloomfilter::run();
     println!("update end, elapsed: {:?} ms", start_time.elapsed().as_millis());
