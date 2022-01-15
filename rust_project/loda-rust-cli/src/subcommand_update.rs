516077b11d5307e8e980888cb68643bd172a060b
Simon Strandgaard <neoneye@gmail.com>
2022-01-15 11:21:37 +0100

    Renamed from NgramGenerator to HistogramInstructionNgramAnalyzer.

--- rust_project/loda-rust-cli/src/subcommand_update.rs
+++ rust_project/loda-rust-cli/src/subcommand_update.rs
@@ -1,12 +1,12 @@
 use std::time::Instant;
-use crate::mine::{DontMine, HistogramInstructionConstantAnalyzer, NgramGenerator, PopulateBloomfilter, ValidatePrograms};
+use crate::mine::{DontMine, HistogramInstructionConstantAnalyzer, HistogramInstructionNgramAnalyzer, PopulateBloomfilter, ValidatePrograms};
 
 pub fn subcommand_update() {
     let start_time = Instant::now();
     println!("update begin");
     DontMine::run();
     HistogramInstructionConstantAnalyzer::run();
-    NgramGenerator::run();
+    HistogramInstructionNgramAnalyzer::run();
     ValidatePrograms::run();
     PopulateBloomfilter::run();
     println!("update end, elapsed: {:?} ms", start_time.elapsed().as_millis());
