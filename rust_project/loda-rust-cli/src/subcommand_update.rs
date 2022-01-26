dabd8013a48ac8fe33a6b845611963dac3a50648
Simon Strandgaard <neoneye@gmail.com>
2022-01-26 20:42:43 +0100

    The subcommand "update" is now again doing all the steps.

--- rust_project/loda-rust-cli/src/subcommand_update.rs
+++ rust_project/loda-rust-cli/src/subcommand_update.rs
@@ -2,11 +2,13 @@ use std::time::Instant;
 use std::rc::Rc;
 use core::cell::RefCell;
 use crate::mine::{DontMine, HistogramInstructionConstantAnalyzer, HistogramInstructionNgramAnalyzer, PopulateBloomfilter, ValidatePrograms};
-use crate::mine::{ProgramIterator, ProgramIteratorPlugin};
+use crate::mine::ProgramIterator;
 
 pub fn subcommand_update() {
     let start_time = Instant::now();
     println!("update begin");
+
+    DontMine::run();
 
     let plugin_ngram = Rc::new(RefCell::new(HistogramInstructionNgramAnalyzer::new()));
     let plugin_constant = Rc::new(RefCell::new(HistogramInstructionConstantAnalyzer::new()));
@@ -23,11 +25,7 @@ pub fn subcommand_update() {
     plugin2.borrow_mut().save();
     plugin3.borrow_mut().save();
 
-    /*
-    DontMine::run();
-    HistogramInstructionConstantAnalyzer::run();
-    // HistogramInstructionNgramAnalyzer::run();
     ValidatePrograms::run();
-    PopulateBloomfilter::run(); */
+    PopulateBloomfilter::run();
     println!("update end, elapsed: {:?} ms", start_time.elapsed().as_millis());
 }
