ad36576f30498605a4abce7904daf30add758532
Simon Strandgaard <neoneye@gmail.com>
2022-03-30 22:00:12 +0200

    dealing with edge cases.

--- rust_project/loda-rust-cli/src/common/find_files_recursively.rs
+++ rust_project/loda-rust-cli/src/common/find_files_recursively.rs
@@ -11,7 +11,25 @@ pub fn find_csv_files_recursively(rootdir: &Path) -> Vec<PathBuf> {
 
 fn find_files_recursively(rootdir: &Path, find_extension: &str) -> Vec<PathBuf> {
 
-    fn is_hidden(entry: &DirEntry) -> bool {
+    fn is_hidden(entry: &DirEntry, find_extension_inner: &str) -> bool {
+        if entry.file_type().is_file() {
+            // If this is a file and it has the wrong extension, then ignore it
+            if let Some(extension) = entry.path().extension() {
+                if extension != find_extension_inner {
+                    // debug!("ignore file {:?}", entry);
+                    return true
+                }
+            }
+        } else {
+            // If this is directory with same extension, then ignore traversing this directory
+            if let Some(extension) = entry.path().extension() {
+                if extension == find_extension_inner {
+                    // debug!("ignore dir {:?}", entry);
+                    return true
+                }
+            }
+        }
+        // If the name starts with a dot character, then ignore the file/dir
         entry.file_name()
              .to_str()
              .map(|s| s.starts_with("."))
@@ -20,7 +38,7 @@ fn find_files_recursively(rootdir: &Path, find_extension: &str) -> Vec<PathBuf> 
     
     let walker = WalkDir::new(rootdir).into_iter();
     let mut paths: Vec<PathBuf> = vec!();
-    for entry in walker.filter_entry(|e| !is_hidden(e)) {
+    for entry in walker.filter_entry(|e| !is_hidden(e, find_extension)) {
         let entry = match entry {
             Ok(value) => value,
             Err(_) => {
@@ -28,19 +46,11 @@ fn find_files_recursively(rootdir: &Path, find_extension: &str) -> Vec<PathBuf> 
                 continue;
             }
         };
-        let path: PathBuf = entry.into_path();
-        let extension = match path.extension() {
-            Some(value) => value,
-            None => {
-                // debug!("path has no extension. Ignoring: {:}", path.display());
-                continue;
-            }
-        };
-        if extension != find_extension {
-            // debug!("path does not match the extension. Ignoring: {:}", path.display());
+        if !entry.file_type().is_file() {
+            // debug!("Ignoring a path that isn't a file");
             continue;
         }
-        // debug!("path: {:?}", path.display());
+        let path: PathBuf = entry.into_path();
         paths.push(path);
     }
     // debug!("number of paths: {} in dir: {}", paths.len(), rootdir.display());
@@ -70,6 +80,36 @@ mod tests {
     fn test_10001_non_existing_dir() -> Result<(), Box<dyn Error>> {
         let tempdir = tempfile::tempdir().unwrap();
         let basedir = PathBuf::from(&tempdir.path()).join("non-existing-dir");
+        let paths = find_asm_files_recursively(&basedir);
+        assert_eq!(paths.len(), 0);
+        Ok(())
+    }
+
+    #[test]
+    fn test_10002_ignore_dotgit_dir() -> Result<(), Box<dyn Error>> {
+        let tempdir = tempfile::tempdir().unwrap();
+        let basedir = PathBuf::from(&tempdir.path()).join("find_files_recursively_test_10002_ignore_dotgit_dir");
+        fs::create_dir(&basedir)?;
+        let dir0 = PathBuf::from(&basedir).join(".git");
+        fs::create_dir(&dir0)?;
+        let mut file0 = File::create(dir0.join("file0.asm"))?;
+        file0.write_all(b"I'm an asm file inside .git, so I am to be ignored")?;
+        let mut file1 = File::create(dir0.join("ignore.md"))?;
+        file1.write_all(b"Ignore this file. It doesn't have the asm file extension")?;
+        let paths = find_asm_files_recursively(&basedir);
+        assert_eq!(paths.len(), 0);
+        Ok(())
+    }
+
+    #[test]
+    fn test_10003_ignore_dir_with_asm_extension() -> Result<(), Box<dyn Error>> {
+        let tempdir = tempfile::tempdir().unwrap();
+        let basedir = PathBuf::from(&tempdir.path()).join("find_files_recursively_test_10003_ignore_dir_with_asm_extension");
+        fs::create_dir(&basedir)?;
+        let dir0 = PathBuf::from(&basedir).join("ignore-this-dir.asm");
+        fs::create_dir(&dir0)?;
+        let mut file0 = File::create(dir0.join("file0.asm"))?;
+        file0.write_all(b"I'm an asm file inside a dir named 'ignore-this-dir.asm', so I am to be ignored")?;
         let paths = find_asm_files_recursively(&basedir);
         assert_eq!(paths.len(), 0);
         Ok(())
