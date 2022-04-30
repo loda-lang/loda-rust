2210f4b6abb6e9b10552be3946344592e8449db9
Simon Strandgaard <neoneye@gmail.com>
2022-04-30 13:16:15 +0200

    Testing the pagerank code.

--- rust_project/loda-rust-cli/src/analytics/program_rank.rs
+++ rust_project/loda-rust-cli/src/analytics/program_rank.rs
@@ -3,7 +3,6 @@ use simple_pagerank::Pagerank;
 use crate::common::parse_csv_file;
 use std::path::{Path, PathBuf};
 use std::error::Error;
-use std::collections::HashMap;
 use serde::{Serialize, Deserialize};
 use csv::WriterBuilder;
 
@@ -15,12 +14,6 @@ struct RecordDependency {
     source: u32,
     #[serde(rename = "callee program id")]
     target: u32,
-}
-
-impl RecordDependency {
-    pub fn parse_csv(path: &Path) -> Result<Vec<RecordDependency>, Box<dyn Error>> {
-        parse_csv_file(path)
-    }
 }
 
 fn create_csv_file<S: Serialize>(records: &Vec<S>, output_path: &Path) -> Result<(), Box<dyn Error>> {
@@ -41,36 +34,39 @@ pub fn compute_program_rank() {
     let input_path: PathBuf = config.analytics_dir_dependencies_file();
     let output_rank_path: PathBuf = config.analytics_dir_program_rank_file();
     let output_popularity_path: PathBuf = config.analytics_dir_program_popularity_file();
-    let dependency_vec: Vec<RecordDependency> = RecordDependency::parse_csv(&input_path).expect("Unable to load input file");
+    let pr: Pagerank<u32> = calculate_pagerank(&input_path);
+    create_dependencies_csv_file(&pr, &output_rank_path);
+    create_popularity_csv_file(&pr, &output_popularity_path, HIGHEST_POPULARITY_VALUE);
+}
 
+fn calculate_pagerank(input_path: &Path) -> Pagerank::<u32> {
+    let dependency_vec: Vec<RecordDependency> = parse_csv_file(input_path).expect("Unable to load input file");
     let mut pr = Pagerank::<u32>::new();
     for dependency in &dependency_vec {
         pr.add_edge(dependency.source, dependency.target);
     }
     pr.calculate();
-
-    create_dependencies_csv_file(&pr, &output_rank_path);
-    create_popularity_csv_file(&pr, &output_popularity_path);
+    pr
 }
 
-#[derive(Serialize)]
-struct ProgramRankResult {
+#[derive(Debug, Deserialize, Serialize)]
+struct ProgramRankItem {
     #[serde(rename = "program id")]
     program_id: u32,
     score: String,
 }
 
 fn create_dependencies_csv_file(pagerank: &Pagerank::<u32>, output_path: &Path) {
-    let mut rows = Vec::<ProgramRankResult>::new();
+    let mut items = Vec::<ProgramRankItem>::new();
     for node in pagerank.nodes() {
-        let row = ProgramRankResult {
+        let item = ProgramRankItem {
             program_id: *node.0,
             score: format!("{:.4}", node.1),
         };
-        rows.push(row);
+        items.push(item);
     }
 
-    match create_csv_file(&rows, &output_path) {
+    match create_csv_file(&items, &output_path) {
         Ok(()) => {},
         Err(error) => {
             error!("Unable to save csv file: {:?}", error);
@@ -84,14 +80,34 @@ struct ProgramScoreItem {
     score: i64,
 }
 
-#[derive(Clone, Copy, Serialize)]
+#[derive(Clone, Copy, Deserialize, Serialize)]
 struct ProgramPopularityItem {
     #[serde(rename = "program id")]
     program_id: u32,
     popularity: usize,
 }
 
-fn create_popularity_csv_file(pagerank: &Pagerank::<u32>, output_path: &Path) {
+/// Determine the popularity group which each program_id belong to.
+/// 
+/// The miner is more likely to call a popular program_id, eg. the Fibonacci program, or the sqrt2 function.
+/// 
+/// The miner is less likely to call an unpopular program, eg. an unreferenced program, or few insignificant references. 
+/// 
+/// This saves a `program_popularity.csv` file, with this format:
+/// 
+/// ```
+/// program id;popularity
+/// 5;9
+/// 6;9
+/// 8;8
+/// 10;9
+/// 15;9
+/// 30;4
+/// 31;8
+/// 32;9
+/// 33;6
+/// ```
+fn create_popularity_csv_file(pagerank: &Pagerank::<u32>, output_path: &Path, highest_popularity_value: usize) {
     let mut program_score_items = Vec::<ProgramScoreItem>::new();
     for node in pagerank.nodes() {
         let score_float: f64 = node.1;
@@ -104,20 +120,15 @@ fn create_popularity_csv_file(pagerank: &Pagerank::<u32>, output_path: &Path) {
     }
 
     // Find lowest score
-    let mut histogram = HashMap::<i64,u32>::new();
-    for item in &program_score_items {
-        let counter = histogram.entry(item.score).or_insert(0);
-        *counter += 1;
+    let mut lowest_score: i64 = 0;
+    if let Some(item0) = program_score_items.first() {
+        lowest_score = item0.score;
     }
-    let mut found_key: i64 = -666;
-    let mut found_value: u32 = 0;
-    for (key, value) in histogram.iter() {
-        if *value > found_value {
-            found_value = *value;
-            found_key = *key;
+    for item in &program_score_items {
+        if lowest_score > item.score {
+            lowest_score = item.score;
         }
     }
-    let lowest_score: i64 = found_key;
 
     // Separate the unused programs, from the used programs
     let (items_with_lowest_score, items_remaining): (Vec<ProgramScoreItem>, Vec<ProgramScoreItem>) = 
@@ -139,7 +150,7 @@ fn create_popularity_csv_file(pagerank: &Pagerank::<u32>, output_path: &Path) {
     // bin 0 is for the unused and unpopular programs. This cluster is huge.
     let count: usize = items_remaining.len();
     for (index, item) in items_remaining.iter().enumerate() {
-        let popularity = HIGHEST_POPULARITY_VALUE - (index * HIGHEST_POPULARITY_VALUE / count);
+        let popularity = highest_popularity_value - (index * highest_popularity_value / count);
         let result_item = ProgramPopularityItem { program_id: item.program_id, popularity: popularity };
         result.push(result_item);
     }
@@ -152,5 +163,94 @@ fn create_popularity_csv_file(pagerank: &Pagerank::<u32>, output_path: &Path) {
         Err(error) => {
             error!("Unable to save csv file: {:?}", error);
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::PathBuf;
+    use std::fs;
+    use std::error::Error;
+    use std::fs::File;
+    use std::io::prelude::*;
+
+    #[test]
+    fn test_10000_create_dependencies_csv_file() -> Result<(), Box<dyn Error>> {
+        // Arrange
+        let tempdir = tempfile::tempdir().unwrap();
+        let basedir = PathBuf::from(&tempdir.path()).join("test_10000_create_dependencies_csv_file");
+        fs::create_dir(&basedir)?;
+        let input_path: PathBuf = basedir.join("input.csv");
+        let output_path: PathBuf = basedir.join("output.csv");
+
+        let input_content = 
+r#"caller program id;callee program id
+6;4
+4;5
+3;6
+2;5
+1;2
+1;5
+"#;
+        let mut input_file = File::create(&input_path)?;
+        input_file.write_all(input_content.as_bytes())?;
+
+        // Act
+        let pr = calculate_pagerank(&input_path);
+        create_dependencies_csv_file(&pr, &output_path);
+
+        // Assert
+        let result_records: Vec<ProgramRankItem> = parse_csv_file(&output_path)?;
+        let mut result_items = Vec::<String>::new();
+        for record in result_records {
+            result_items.push(format!("{}", record.program_id));
+        }
+        let result = result_items.join(",");
+        assert_eq!(result, "5,4,6,2,3,1");
+        Ok(())
+    }
+
+    #[test]
+    fn test_20000_create_popularity_csv_file() -> Result<(), Box<dyn Error>> {
+        // Arrange
+        let tempdir = tempfile::tempdir().unwrap();
+        let basedir = PathBuf::from(&tempdir.path()).join("test_20000_create_popularity_csv_file");
+        fs::create_dir(&basedir)?;
+        let input_path: PathBuf = basedir.join("input.csv");
+        let output_path: PathBuf = basedir.join("output.csv");
+
+        let input_content = 
+r#"caller program id;callee program id
+8;9
+7;8
+6;7
+5;6
+4;5
+3;4
+2;3
+1;2
+104;1
+100;1
+103;1
+101;1
+102;1
+"#;
+        let mut input_file = File::create(&input_path)?;
+        input_file.write_all(input_content.as_bytes())?;
+
+        // Act
+        let pr = calculate_pagerank(&input_path);
+        create_popularity_csv_file(&pr, &output_path, 3);
+
+        // Assert
+        let result_records: Vec<ProgramPopularityItem> = parse_csv_file(&output_path)?;
+        let mut result_items = Vec::<String>::new();
+        for record in result_records {
+            result_items.push(format!("{} {}", record.program_id, record.popularity));
+        }
+        let result = result_items.join(",");
+        assert_eq!(result, "1 1,2 1,3 1,4 2,5 2,6 2,7 3,8 3,9 3,100 0,101 0,102 0,103 0,104 0");
+        Ok(())
     }
 }
