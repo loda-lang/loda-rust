d1e4b109bb3c722becd74098a366de96424ec1d2
Simon Strandgaard <neoneye@gmail.com>
2022-05-16 17:23:25 +0200

    Wrapped in a timeout command, so it doesn't hang for ages.

--- script/task_analyze_mined_programs.rb
+++ script/task_analyze_mined_programs.rb
@@ -247,8 +247,16 @@ def analyze_candidate(candidate_program, program_id)
 
     a_name = "A%06i" % program_id
 
-    loda_check_output = `#{LODA_CPP_EXECUTABLE} check #{a_name} -b 0 > #{path_check_output}`
+    loda_check_output = `timeout --verbose 60s #{LODA_CPP_EXECUTABLE} check #{a_name} -b 0 > #{path_check_output}`
     loda_check_output.strip!
+    if $?.exitstatus == 124  # when the `timeout` command is triggered it returns status 124
+        puts "Rejecting. It takes too long to check the terms of the new program."
+        File.rename(path, path_reject)
+        if has_original_file
+            File.rename(path_original, path)
+        end
+        return false
+    end
     unless $?.success?
         puts "loda check, expected exit code 0, but got exit code: #{$?}, see loda check output: #{path_check_output}"
         puts loda_check_output
