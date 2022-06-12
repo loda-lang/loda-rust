7570fa770618550aceacc7904a4fbda965228a59
Simon Strandgaard <neoneye@gmail.com>
2022-06-12 13:04:28 +0200

    Renamed file

--- rust_project/loda-rust-cli/src/common/mod.rs
+++ rust_project/loda-rust-cli/src/common/mod.rs
@@ -1,19 +1,19 @@
 mod create_csv_file;
 mod find_files_recursively;
 mod load_program_ids_csv_file;
-mod parse_csv_bigram;
 mod parse_csv_data;
 mod parse_csv_file;
 mod parse_csv_skipgram;
 mod program_id_from_path;
+mod record_bigram;
 mod record_trigram;
 
 pub use create_csv_file::create_csv_file;
 pub use find_files_recursively::{find_asm_files_recursively, find_csv_files_recursively};
 pub use load_program_ids_csv_file::load_program_ids_csv_file;
-pub use parse_csv_bigram::RecordBigram;
 pub use parse_csv_data::parse_csv_data;
 pub use parse_csv_file::parse_csv_file;
 pub use parse_csv_skipgram::RecordSkipgram;
 pub use program_id_from_path::{program_id_from_path, program_ids_from_paths};
+pub use record_bigram::RecordBigram;
 pub use record_trigram::RecordTrigram;
