e74748f500faa62a23f6d8c4d70c4d8f44ef73bb
Simon Strandgaard <neoneye@gmail.com>
2022-06-15 18:27:29 +0200

    SimpleLog::sink() added

--- rust_project/loda-rust-cli/src/common/simple_log.rs
+++ rust_project/loda-rust-cli/src/common/simple_log.rs
@@ -5,26 +5,57 @@ use std::io::LineWriter;
 use std::path::Path;
 use std::sync::{Arc, Mutex};
 
-struct State {
+
+trait SimpleLogState {
+    fn print(&mut self, content: &String) -> Result<(), Box<dyn Error>>;
+}
+
+struct StateLineWriter {
     line_writer: LineWriter<File>
+}
+
+impl SimpleLogState for StateLineWriter {
+    fn print(&mut self, content: &String) -> Result<(), Box<dyn Error>> {
+        // Append to a log file
+        self.line_writer.write_all(content.as_bytes())?;
+        self.line_writer.flush()?;
+        Ok(())
+    }
+}
+
+struct StateSink {}
+
+impl SimpleLogState for StateSink {
+    fn print(&mut self, _content: &String) -> Result<(), Box<dyn Error>> {
+        // Silently ignores printing
+        Ok(())
+    }
 }
 
 #[derive(Clone)]
 pub struct SimpleLog {
-    state: Arc<Mutex<State>>,
+    state: Arc<Mutex<dyn SimpleLogState>>,
 }
 
 impl SimpleLog {
     pub fn new(path: &Path) -> Result<Self, Box<dyn Error>> {
         let file = File::create(path)?;
         let line_writer: LineWriter<File> = LineWriter::new(file);
-        let state = State {
+        let state = StateLineWriter {
             line_writer: line_writer
         };
         let instance = Self {
             state: Arc::new(Mutex::new(state))
         };
         Ok(instance)
+    }
+
+    #[allow(dead_code)]
+    pub fn sink() -> Self {
+        let state = StateSink {};
+        Self {
+            state: Arc::new(Mutex::new(state))
+        }
     }
 
     pub fn println<I: AsRef<str>>(&self, message: I) {
@@ -35,8 +66,7 @@ impl SimpleLog {
 
     pub fn print(&self, content: &String) -> Result<(), Box<dyn Error>> {
         let mut state = self.state.lock().unwrap();
-        state.line_writer.write_all(content.as_bytes())?;
-        state.line_writer.flush()?;
+        state.print(content)?;
         Ok(())
     }
 }
