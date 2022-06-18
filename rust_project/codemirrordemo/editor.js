40d3dc9de483a9197d3b49fedf257d5b1039246b
Simon Strandgaard <neoneye@gmail.com>
2022-06-19 01:23:40 +0200

    Editor is now showing an initial document

--- rust_project/codemirrordemo/editor.js
+++ rust_project/codemirrordemo/editor.js
@@ -1,14 +1,5 @@
 import { EditorView, basicSetup } from "codemirror";
-// import { EditorState } from '@codemirror/state'
-//import {basicSetup} from "codemirror"
-// import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup"
-// import {EditorView} from "@codemirror/view"
-// import {EditorView, keymap} from "@codemirror/view"
-// import {indentWithTab} from "@codemirror/commands"
 import {javascript} from "@codemirror/lang-javascript"
-
-// import {EditorView, basicSetup} from "@codemirror/basic-setup"
-// import {javascript} from "@codemirror/lang-javascript"
 
 const doc = `if (true) {
   console.log("okay")
@@ -17,37 +8,8 @@ const doc = `if (true) {
 }
 `;
 
-// alert('init');
-
-//let editor = 
-/*new EditorView({
-  doc: doc,
-  extensions: [
-    basicSetup,
-    // keymap.of([indentWithTab]),
-    javascript()
-  ],
-  // extensions: [
-  //   basicSetup, 
-  //   javascript()
-  // ],
-  // state: EditorState.create({
-  // }),
-  parent: document.querySelector("#editor")
-}); */
-
-// new EditorView({
-//   state: EditorState.create({
-//     doc: doc,
-//     extensions: [
-//       basicSetup,
-//       javascript()
-//     ]
-//   }),
-//   parent: document.querySelector("#editor")
-// });
-
 new EditorView({
+  doc: doc,
   parent: document.querySelector("#editor"),
   extensions: [
     basicSetup,
