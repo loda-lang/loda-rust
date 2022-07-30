dfe824150e50a8c3db70395294724ab761d9a2d9
Simon Strandgaard <neoneye@gmail.com>
2022-07-30 23:11:36 +0200

    Extracted LodaCppError enum

--- rust_project/loda-rust-cli/src/lodacpp/lodacpp.rs
+++ rust_project/loda-rust-cli/src/lodacpp/lodacpp.rs
@@ -1,4 +1,3 @@
-use std::fmt;
 use std::path::{Path, PathBuf};
 
 #[derive(Debug)]
@@ -17,26 +16,5 @@ impl LodaCpp {
 
     pub fn loda_cpp_executable(&self) -> &Path {
         &self.loda_cpp_executable
-    }
-}
-
-#[derive(Debug)]
-pub enum LodaCppError {
-    NonZeroExitCode,
-    NoOutput,
-    Parse,
-    Timeout,
-}
-
-impl std::error::Error for LodaCppError {}
-
-impl fmt::Display for LodaCppError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::NonZeroExitCode => write!(f, "Non-zero exit code"),
-            Self::NoOutput => write!(f, "No output from loda-cpp"),
-            Self::Parse => write!(f, "Cannot parse the output from loda-cpp"),
-            Self::Timeout => write!(f, "Timeout"),
-        }
     }
 }
--- rust_project/loda-rust-cli/src/lodacpp/mod.rs
+++ rust_project/loda-rust-cli/src/lodacpp/mod.rs
@@ -1,10 +1,12 @@
 //! Integration with the `loda-cpp` executable.
 mod lodacpp;
+mod lodacpp_error;
 mod lodacpp_eval;
 mod lodacpp_eval_ok;
 mod lodacpp_minimize;
 
-pub use lodacpp::{LodaCpp, LodaCppError};
+pub use lodacpp::LodaCpp;
+pub use lodacpp_error::LodaCppError;
 pub use lodacpp_eval::LodaCppEvalWithPath;
 pub use lodacpp_eval_ok::LodaCppEvalOk;
 pub use lodacpp_minimize::LodaCppMinimize;
