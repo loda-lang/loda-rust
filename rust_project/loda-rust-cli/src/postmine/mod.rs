f1bc1699d9c94dee2598fef43535ed0395ecba87
Simon Strandgaard <neoneye@gmail.com>
2022-07-31 09:35:23 +0200

    Moved SubcommandPostMine to postmine.rs

--- rust_project/loda-rust-cli/src/postmine/mod.rs
+++ rust_project/loda-rust-cli/src/postmine/mod.rs
@@ -9,4 +9,5 @@ pub use candidate_program::{CandidateProgram, State};
 pub use validate_single_program::{ValidateSingleProgram, ValidateSingleProgramError};
 pub use find_pending_programs::find_pending_programs;
 pub use path_util::PathUtil;
+pub use postmine::PostMine;
 pub use postmine_error::PostMineError;
