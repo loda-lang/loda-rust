f351536383391c085aea88229df581644f31e126
Simon Strandgaard <neoneye@gmail.com>
2022-07-31 12:05:13 +0200

    LodaCppEvalStepsExecute added

--- rust_project/loda-rust-cli/src/lodacpp/mod.rs
+++ rust_project/loda-rust-cli/src/lodacpp/mod.rs
@@ -2,6 +2,7 @@
 mod lodacpp;
 mod lodacpp_check;
 mod lodacpp_error;
+mod lodacpp_eval_steps_execute;
 mod lodacpp_eval_terms_execute;
 mod lodacpp_eval_terms;
 mod lodacpp_minimize;
@@ -9,6 +10,7 @@ mod lodacpp_minimize;
 pub use lodacpp::LodaCpp;
 pub use lodacpp_check::LodaCppCheck;
 pub use lodacpp_error::LodaCppError;
+pub use lodacpp_eval_steps_execute::LodaCppEvalStepsExecute;
 pub use lodacpp_eval_terms_execute::LodaCppEvalTermsExecute;
 pub use lodacpp_eval_terms::LodaCppEvalTerms;
 pub use lodacpp_minimize::LodaCppMinimize;
