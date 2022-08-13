5ea44f6736bdb72b35aa0834a77119a1979db2ac
Simon Strandgaard <neoneye@gmail.com>
2022-08-13 13:09:39 +0200

    Traverse all the rows of the OEIS `names` file.

--- rust_project/loda-rust-cli/src/oeis/mod.rs
+++ rust_project/loda-rust-cli/src/oeis/mod.rs
@@ -1,12 +1,14 @@
 //! OEIS specific code, such as A-numbers and loading of the 'stripped' file.
 mod name_row;
 mod oeis_id;
+mod process_names_file;
 mod process_stripped_sequence_file;
 mod stripped_sequence;
 mod terms_to_program_id;
 
 pub use name_row::NameRow;
 pub use oeis_id::OeisId;
+pub use process_names_file::ProcessNamesFile;
 pub use process_stripped_sequence_file::ProcessStrippedSequenceFile;
 pub use stripped_sequence::{parse_stripped_sequence_line, StrippedSequence};
 pub use terms_to_program_id::{TermsToProgramIdSet, load_terms_to_program_id_set};
