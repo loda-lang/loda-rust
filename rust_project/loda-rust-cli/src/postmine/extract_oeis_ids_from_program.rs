c9bc5f7b0c9787ef5612bbec7d1b488699f28707
Simon Strandgaard <neoneye@gmail.com>
2022-08-14 14:37:57 +0200

    extract_oeis_ids_from_program() implemented with tests.

--- rust_project/loda-rust-cli/src/postmine/extract_oeis_ids_from_program.rs
+++ rust_project/loda-rust-cli/src/postmine/extract_oeis_ids_from_program.rs
@@ -6,58 +6,135 @@ use loda_rust_core::execute::NodeRegisterLimit;
 use loda_rust_core::execute::node_binomial::NodeBinomialLimit;
 use loda_rust_core::execute::node_power::NodePowerLimit;
 use crate::common::oeis_id_from_path;
+use crate::oeis::OeisId;
+use std::collections::HashSet;
 use std::error::Error;
 use std::path::{Path, PathBuf};
 use std::fmt;
 use std::fs;
+use std::convert::TryFrom;
+use anyhow::Context;
 
-pub fn extract_oeis_ids_from_program(program_path: &Path) {
-/*    
-    if !program_path.is_file() {
-        debug!("Missing program {:?}.", program_path);
-        return Err(Box::new(ValidateSingleProgramError::MissingFile));
-    }
-    let program_contents: String = match fs::read_to_string(&program_path) {
-        Ok(value) => value,
-        Err(io_error) => {
-            debug!("Something went wrong reading the file. {:?}", io_error);
-            return Err(Box::new(ValidateSingleProgramError::Load));
-        }
-    };
+pub type OeisIdHashSet = HashSet<OeisId>;
 
+pub fn extract_oeis_ids_from_program(program_path: &Path) -> anyhow::Result<OeisIdHashSet> {
+    // Load asm file
+    let program_contents: String = fs::read_to_string(&program_path)
+        .with_context(|| format!("Read program from {:?}", &program_path))?;
+
+    // Convert file content to a program
     let parsed_program: ParsedProgram = match ParsedProgram::parse_program(&program_contents) {
         Ok(value) => value,
         Err(error) => {
-            return Err(DependencyManagerError::ParseProgram(error));
+            return Err(anyhow::anyhow!("Parse program from {:?} error: {:?}", &program_path, error));
         }
     };
 
+    // Loop over the `seq` instructions and gather their oeis ids
+    let mut oeis_ids = OeisIdHashSet::new();
     let direct_dependencies: Vec<u64> = parsed_program.direct_dependencies();
-
-    // let oeis_id: OeisId = oeis_id_from_path(program_path);
-*/
+    for direct_dependency in direct_dependencies {
+        match u32::try_from(direct_dependency) {
+            Ok(oeis_id_raw) => {
+                oeis_ids.insert(OeisId::from(oeis_id_raw));
+            },
+            Err(error) => {
+                return Err(anyhow::anyhow!("Value is outside than what OeisId can represent. program_path: {:?} error: {:?}", &program_path, error));
+            }
+        }
+    }
+    Ok(oeis_ids)
 }
-
-// def extract_oeis_ids_from_program_file(path)
-//     unless File.exist?(path)
-//         raise "file does not exist: #{path}"
-//     end
-//     path =~ /\bA0*(\d+)[.]asm$/
-//     program_id = $1.to_i
-//     if program_id == 0
-//         raise "Unable to process file at path: #{path}"
-//     end
-//     content = IO.read(path)
-//     sequence_instructions = content.scan(/^\s*seq .*,\s*(\d+)$/).flatten
-//     sequence_program_ids = sequence_instructions.map { |seq_program_id| seq_program_id.to_i }
-//     # puts "program: #{program_id} depends on: #{sequence_program_ids}"
-//     return [program_id] + sequence_program_ids
-// end
 
 // def extract_oeis_ids_from_program_files(paths)
 //     program_ids = []
 //     paths.each do |path|
+//         let oeis_id: OeisId = oeis_id_from_path(program_path);
 //         program_ids += extract_oeis_ids_from_program_file(path)
 //     end
 //     program_ids
 // end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::error::Error;
+    use std::fs::File;
+    use std::io::prelude::*;
+    
+    #[test]
+    fn test_10000_extract_oeis_ids_from_program_ok() -> Result<(), Box<dyn Error>> {
+        // Arrange
+        let tempdir = tempfile::tempdir().unwrap();
+        let basedir = PathBuf::from(&tempdir.path()).join("test_10000_extract_oeis_ids_from_program_ok");
+        fs::create_dir(&basedir)?;
+        let input_path: PathBuf = basedir.join("A026233.asm");
+
+        let input_content = 
+r#"
+; A026233: a(n) = j if n is the j-th prime, else a(n) = k if n is the k-th nonprime.
+mov $1,$0
+seq $0,66246 ; 0 unless n is a composite number A002808(k) when a(n) = k.
+add $0,1
+seq $1,159081 ; Let d be the largest element of A008578 which divides n, then a(n) is the position of d in A008578.
+sub $1,1
+max $1,$0
+mov $0,$1
+"#;
+        let mut input_file = File::create(&input_path)?;
+        input_file.write_all(input_content.as_bytes())?;
+
+        // Act
+        let oeis_ids: OeisIdHashSet = extract_oeis_ids_from_program(&input_path)?;
+
+        // Assert
+        let mut expected = OeisIdHashSet::new();
+        expected.insert(OeisId::from(66246));
+        expected.insert(OeisId::from(159081));
+        assert_eq!(oeis_ids, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_10001_extract_oeis_ids_from_program_missing_file() -> Result<(), Box<dyn Error>> {
+        // Arrange
+        let tempdir = tempfile::tempdir().unwrap();
+        let basedir = PathBuf::from(&tempdir.path()).join("test_10001_extract_oeis_ids_from_program_missing_file");
+        fs::create_dir(&basedir)?;
+        let input_path: PathBuf = basedir.join("non-existing.asm");
+
+        // Act
+        let result = extract_oeis_ids_from_program(&input_path);
+
+        // Assert
+        let error = result.expect_err("should fail");
+        let error_message: String = error.to_string();
+        assert_eq!(error_message.contains("Read program"), true);
+        Ok(())
+    }
+
+    #[test]
+    fn test_10002_extract_oeis_ids_from_program_parse_error() -> Result<(), Box<dyn Error>> {
+        // Arrange
+        let tempdir = tempfile::tempdir().unwrap();
+        let basedir = PathBuf::from(&tempdir.path()).join("test_10002_extract_oeis_ids_from_program_parse_error");
+        fs::create_dir(&basedir)?;
+        let input_path: PathBuf = basedir.join("A000040.asm");
+
+        let input_content = 
+r#"
+boom $0,3 ; non-existing instruction
+"#;
+        let mut input_file = File::create(&input_path)?;
+        input_file.write_all(input_content.as_bytes())?;
+
+        // Act
+        let result = extract_oeis_ids_from_program(&input_path);
+
+        // Assert
+        let error = result.expect_err("should fail");
+        let error_message: String = error.to_string();
+        assert_eq!(error_message.contains("Parse program"), true);
+        Ok(())
+    }
+}
