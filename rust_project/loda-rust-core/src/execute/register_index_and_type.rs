29de462e933df2617e40c3cba57b40c2d04ed0a7
Simon Strandgaard <neoneye@gmail.com>
2022-09-10 01:00:24 +0200

    RegisterIndexAndType formatting and cloning.

--- rust_project/loda-rust-core/src/execute/register_index_and_type.rs
+++ rust_project/loda-rust-core/src/execute/register_index_and_type.rs
@@ -1,7 +1,9 @@
 use crate::parser::{Instruction, InstructionParameter, ParameterType, CreateInstructionError, CreateInstructionErrorType};
 use super::{RegisterIndex, RegisterType};
+use std::fmt;
 
 /// Access to a register either direct or indirect.
+#[derive(Clone, Debug)]
 pub struct RegisterIndexAndType {
     pub register_index: RegisterIndex,
     pub register_type: RegisterType,
@@ -48,5 +50,11 @@ impl RegisterIndexAndType {
             register_index: register_index,
             register_type: register_type
         })
+    }
+}
+
+impl fmt::Display for RegisterIndexAndType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}{}", self.register_type.register_prefix(), self.register_index)
     }
 }
