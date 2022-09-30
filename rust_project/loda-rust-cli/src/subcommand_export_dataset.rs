f4a607553a4dc13c0ef35066f1eed21117871a24
Simon Strandgaard <neoneye@gmail.com>
2022-09-30 17:07:50 +0200

    Now saving a .csv file with programs,

--- rust_project/loda-rust-cli/src/subcommand_export_dataset.rs
+++ rust_project/loda-rust-cli/src/subcommand_export_dataset.rs
@@ -1,8 +1,10 @@
-//! The `loda-rust test-xyz` subcommands, runs automated tests.
+//! The `loda-rust export-dataset` subcommand, exports terms and programs to a CSV file.
 use crate::config::Config;
-use crate::common::{find_asm_files_recursively, load_program_ids_csv_file, oeis_id_from_path, SimpleLog};
+use crate::common::{find_asm_files_recursively, load_program_ids_csv_file, oeis_id_from_path};
+use crate::common::create_csv_file;
 use crate::postmine::{ParentDirAndChildFile, path_for_oeis_program};
 use loda_rust_core::oeis::OeisId;
+use loda_rust_core::parser::{ParsedProgram};
 use std::collections::HashSet;
 use std::error::Error;
 use std::fs;
@@ -12,10 +14,12 @@ use std::time::Duration;
 use anyhow::Context;
 use console::Style;
 use indicatif::{HumanDuration, ProgressBar};
+use serde::Serialize;
 
 pub struct SubcommandExportDataset {
     config: Config,
     number_of_program_files_ignored: usize,
+    records: Vec<Record>,
 }
 
 impl SubcommandExportDataset {
@@ -23,6 +27,7 @@ impl SubcommandExportDataset {
         let mut instance = Self {
             config: Config::load(),
             number_of_program_files_ignored: 0,
+            records: vec!(),
         };
         instance.run()?;
         Ok(())
@@ -40,7 +45,7 @@ impl SubcommandExportDataset {
 
         let dir_containing_programs: PathBuf = self.config.loda_programs_oeis_dir();
         let mut paths: Vec<PathBuf> = find_asm_files_recursively(&dir_containing_programs);
-        paths.truncate(100);
+        paths.truncate(1000);
         let number_of_paths = paths.len();
         if number_of_paths <= 0 {
             return Err(anyhow::anyhow!("Expected 1 or more programs, but there are no programs to analyze"));
@@ -63,8 +68,13 @@ impl SubcommandExportDataset {
             HumanDuration(start.elapsed())
         );
 
-        println!("number_of_program_files_ignored: {}", self.number_of_program_files_ignored);
+        if self.number_of_program_files_ignored > 0 {
+            println!("number_of_program_files_ignored: {}", self.number_of_program_files_ignored);
+        }
 
+        self.save()?;
+
+        println!("Ok");
         Ok(())
     }
 
@@ -85,7 +95,49 @@ impl SubcommandExportDataset {
                 return Err(anyhow::anyhow!("loading program_id: {:?}, something went wrong reading the file: {:?}", program_id, error));
             }
         };
+        let parsed_program: ParsedProgram = match ParsedProgram::parse_program(&contents) {
+            Ok(value) => value,
+            Err(error) => {
+                return Err(anyhow::anyhow!("parsing program_id: {:?}, something went wrong parsing the file: {:?}", program_id, error));
+            }
+        };
+        let instructions: Vec<String> = parsed_program.instruction_vec.iter().map(|instruction| {
+            instruction.to_string()
+        }).collect();
+        let instructions_joined: String = instructions.join("\\n");
+        // println!("program: {}\n{}", program_id, instructions_joined);
+
+        let record = Record {
+            program_id: program_id,
+            terms: "".to_string(),
+            program: instructions_joined
+        };
+        self.records.push(record);
 
         Ok(())
     }
+
+    /// Save as a CSV file
+    fn save(&self) -> anyhow::Result<()> {
+        let mut records: Vec<Record> = self.records.clone();
+        records.sort_unstable_by_key(|item| (item.program_id));
+
+        let output_path: PathBuf = self.config.analytics_dir().join("dataset.csv");
+        match create_csv_file(&self.records, &output_path) {
+            Ok(_) => {},
+            Err(error) => {
+                return Err(anyhow::anyhow!("Unable to save csv file at {:?}, error: {:?}", output_path, error));
+            }
+        }
+        Ok(())
+    }
+}
+
+#[derive(Clone, Serialize)]
+struct Record {
+    #[serde(rename = "oeis")]
+    program_id: u32,
+    terms: String,
+    #[serde(rename = "loda")]
+    program: String,
 }
