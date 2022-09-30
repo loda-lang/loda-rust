b356c40db3d4b1bf51e99b78462cc5afb33b6e55
Simon Strandgaard <neoneye@gmail.com>
2022-09-30 17:56:53 +0200

    The CSV file now contains both terms, and program, and oeis_id.

--- rust_project/loda-rust-cli/src/subcommand_export_dataset.rs
+++ rust_project/loda-rust-cli/src/subcommand_export_dataset.rs
@@ -2,23 +2,31 @@
 use crate::config::Config;
 use crate::common::{find_asm_files_recursively, load_program_ids_csv_file, oeis_id_from_path};
 use crate::common::create_csv_file;
-use crate::postmine::{ParentDirAndChildFile, path_for_oeis_program};
+use crate::oeis::{ProcessStrippedFile, StrippedRow};
+use loda_rust_core::util::BigIntVecToString;
+use loda_rust_core::oeis::OeisIdHashSet;
 use loda_rust_core::oeis::OeisId;
 use loda_rust_core::parser::{ParsedProgram};
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::fs;
+use std::fs::File;
+use std::io::BufReader;
 use std::path::{Path, PathBuf};
 use std::time::Instant;
-use std::time::Duration;
-use anyhow::Context;
 use console::Style;
 use indicatif::{HumanDuration, ProgressBar};
 use serde::Serialize;
+use num_bigint::BigInt;
+use num_traits::Zero;
+
+pub type OeisIdToTermsSet = HashMap::<OeisId, String>;
 
 pub struct SubcommandExportDataset {
     config: Config,
-    number_of_program_files_ignored: usize,
+    count_ignored: usize,
+    count_insufficient_number_of_terms: usize,
+    oeis_id_to_terms_set: OeisIdToTermsSet,
     records: Vec<Record>,
 }
 
@@ -26,14 +34,58 @@ impl SubcommandExportDataset {
     pub fn export_dataset() -> Result<(), Box<dyn Error>> {
         let mut instance = Self {
             config: Config::load(),
-            number_of_program_files_ignored: 0,
+            count_ignored: 0,
+            count_insufficient_number_of_terms: 0,
             records: vec!(),
+            oeis_id_to_terms_set: OeisIdToTermsSet::new(),
         };
         instance.run()?;
         Ok(())
     }
 
     fn run(&mut self) -> anyhow::Result<()> {
+        self.load_stripped_file()?;
+        self.process_program_files()?;
+        self.save()?;
+        println!("Ok");
+        Ok(())
+    }
+
+    fn load_stripped_file(&mut self) -> anyhow::Result<()> {
+        let mut oeis_id_to_terms_set = OeisIdToTermsSet::new();
+
+        let callback = |row: &StrippedRow, _| {
+            let value: String = row.terms().to_compact_comma_string();
+            let key: OeisId = row.oeis_id();
+            oeis_id_to_terms_set.insert(key, value);
+        };
+
+        let oeis_stripped_file = self.config.oeis_stripped_file();
+        let file = File::open(oeis_stripped_file)?;
+        let mut oeis_stripped_file_reader = BufReader::new(file);
+
+        let minimum_number_of_required_terms = 20;
+        let term_count = 20;
+        let padding_value = BigInt::zero();
+    
+        let mut processor = ProcessStrippedFile::new();
+        let oeis_ids_to_ignore = OeisIdHashSet::new();
+        processor.execute(
+            &mut oeis_stripped_file_reader,
+            minimum_number_of_required_terms,
+            term_count,
+            &oeis_ids_to_ignore,
+            &padding_value, 
+            false,
+            callback
+        );
+
+        self.oeis_id_to_terms_set = oeis_id_to_terms_set;
+
+        Ok(())
+    }
+
+    fn process_program_files(&mut self) -> anyhow::Result<()> {
         let programs_invalid_file = self.config.analytics_dir_programs_invalid_file();
         let invalid_program_ids: Vec<u32> = match load_program_ids_csv_file(&programs_invalid_file) {
             Ok(value) => value,
@@ -45,7 +97,7 @@ impl SubcommandExportDataset {
 
         let dir_containing_programs: PathBuf = self.config.loda_programs_oeis_dir();
         let mut paths: Vec<PathBuf> = find_asm_files_recursively(&dir_containing_programs);
-        paths.truncate(1000);
+        // paths.truncate(1000);
         let number_of_paths = paths.len();
         if number_of_paths <= 0 {
             return Err(anyhow::anyhow!("Expected 1 or more programs, but there are no programs to analyze"));
@@ -68,13 +120,12 @@ impl SubcommandExportDataset {
             HumanDuration(start.elapsed())
         );
 
-        if self.number_of_program_files_ignored > 0 {
-            println!("number_of_program_files_ignored: {}", self.number_of_program_files_ignored);
+        if self.count_ignored > 0 {
+            println!("count_ignored: {}", self.count_ignored);
         }
-
-        self.save()?;
-
-        println!("Ok");
+        if self.count_insufficient_number_of_terms > 0 {
+            println!("count_insufficient_number_of_terms: {}", self.count_insufficient_number_of_terms);
+        }
         Ok(())
     }
 
@@ -86,9 +137,19 @@ impl SubcommandExportDataset {
             }
         };
         if ignore_program_ids.contains(&program_id) {
-            self.number_of_program_files_ignored += 1;
+            self.count_ignored += 1;
             return Ok(());
         }
+        let oeis_id = OeisId::from(program_id);
+        let terms: String = match self.oeis_id_to_terms_set.get(&oeis_id) {
+            Some(value) => {
+                value.clone()
+            },
+            None => {
+                self.count_insufficient_number_of_terms += 1;
+                return Ok(());
+            }
+        };
         let contents: String = match fs::read_to_string(&path_to_program) {
             Ok(value) => value,
             Err(error) => {
@@ -105,11 +166,10 @@ impl SubcommandExportDataset {
             instruction.to_string()
         }).collect();
         let instructions_joined: String = instructions.join("\\n");
-        // println!("program: {}\n{}", program_id, instructions_joined);
 
         let record = Record {
             program_id: program_id,
-            terms: "".to_string(),
+            terms: terms,
             program: instructions_joined
         };
         self.records.push(record);
@@ -123,7 +183,7 @@ impl SubcommandExportDataset {
         records.sort_unstable_by_key(|item| (item.program_id));
 
         let output_path: PathBuf = self.config.analytics_dir().join("dataset.csv");
-        match create_csv_file(&self.records, &output_path) {
+        match create_csv_file(&records, &output_path) {
             Ok(_) => {},
             Err(error) => {
                 return Err(anyhow::anyhow!("Unable to save csv file at {:?}, error: {:?}", output_path, error));
