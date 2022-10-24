fadd25aebd171510a5eae25bfd2b18e361ef3e5f
Simon Strandgaard <neoneye@gmail.com>
2022-10-24 22:23:56 +0200

    Replaced hardcoded path to executable with path specified in file: ~/.loda-rust/config.toml

--- script/task_analyze_mined_programs.rb
+++ script/task_analyze_mined_programs.rb
@@ -1,11 +1,15 @@
 #!/usr/bin/env ruby
 
 =begin
-Look up terms in oeis and gather the OEIS ids.
-
-This script traverses all the programs inside the "mine-event" dir.
-It looks for all the LODA assembly programs there are.
-
+This traverses all the candidate programs inside the "mine-event" dir.
+It decides whether to keep or reject the programs.
 =end
 
-exec("data/loda-rust postmine")
+require_relative 'config'
+
+LODA_RUST_EXECUTABLE = Config.instance.loda_rust_executable
+unless File.executable?(LODA_RUST_EXECUTABLE)
+    raise "No such file #{LODA_RUST_EXECUTABLE}, cannot run script"
+end
+
+exec("#{LODA_RUST_EXECUTABLE} postmine")
