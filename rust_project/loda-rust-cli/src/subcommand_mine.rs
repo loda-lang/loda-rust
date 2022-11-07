097057072b726294229d8ad8b9c37b48f640da22
Simon Strandgaard <neoneye@gmail.com>
2022-11-07 15:43:22 +0100

    Extracted miner_worker.

--- rust_project/loda-rust-cli/src/subcommand_mine.rs
+++ rust_project/loda-rust-cli/src/subcommand_mine.rs
@@ -1,22 +1,19 @@
 //! The `loda-rust mine` subcommand, runs the miner daemon process.
 use crate::config::{Config, NumberOfWorkers};
 use crate::common::PendingProgramsWithPriority;
-use crate::mine::{ExecuteBatchResult, FunnelConfig, MineEventDirectoryState, MetricsWorker, RunMinerLoop, MetricEvent};
-use crate::mine::{create_funnel, Funnel};
+use crate::mine::{MineEventDirectoryState, MetricsWorker};
+use crate::mine::{create_funnel, Funnel, FunnelConfig};
 use crate::mine::{create_genome_mutate_context, GenomeMutateContext};
 use crate::mine::{create_prevent_flooding, PreventFlooding};
+use crate::mine::{miner_worker};
+use crate::mine::{postmine_worker, SharedMinerWorkerState};
 use crate::mine::upload_worker;
-use crate::mine::{postmine_worker, PostmineWorkerMessage, SharedMinerWorkerState};
 use crate::oeis::{load_terms_to_program_id_set, TermsToProgramIdSet};
-use loda_rust_core::control::{DependencyManager, DependencyManagerFileSystemMode, ExecuteProfile};
 use bastion::prelude::*;
 use num_bigint::{BigInt, ToBigInt};
 use anyhow::Context;
-use std::thread;
-use std::time::Duration;
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
-use rand::{RngCore, thread_rng};
 
 #[derive(Debug)]
 pub enum SubcommandMineMetricsMode {
@@ -243,156 +240,5 @@ impl SubcommandMine {
         Bastion::start();
 
         Ok(())
-    }
-}
-
-#[derive(Debug, Clone)]
-enum MinerWorkerMessage {
-    #[allow(dead_code)]
-    Ping,
-}
-
-async fn miner_worker(
-    ctx: BastionContext,
-    terms_to_program_id: Arc<TermsToProgramIdSet>,
-    prevent_flooding: Arc<Mutex<PreventFlooding>>,
-    mine_event_dir_state: Arc<Mutex<MineEventDirectoryState>>,
-    shared_miner_worker_state: Arc<Mutex<SharedMinerWorkerState>>,
-    config: Config,
-    funnel: Funnel,
-    genome_mutate_context: GenomeMutateContext,    
-) -> Result<(), ()> {
-    debug!("miner_worker - started, {:?}", ctx.current().id());
-    let loda_programs_oeis_dir: PathBuf = config.loda_programs_oeis_dir();
-
-    let postmine_worker_distributor = Distributor::named("postmine_worker");
-    let metrics_worker_distributor = Distributor::named("metrics_worker");
-
-    let initial_random_seed: u64 = {
-        let mut rng = thread_rng();
-        rng.next_u64()
-    };
-
-    let mut rml = RunMinerLoop::new(
-        funnel,
-        &config,
-        prevent_flooding,
-        genome_mutate_context,
-        initial_random_seed,
-        terms_to_program_id,
-    );
-    let callback = move |metric_event: MetricEvent| {
-        let tell_result = metrics_worker_distributor.tell_everyone(metric_event.clone());
-        if let Err(error) = tell_result {
-            error!("miner_worker: Unable to send MetricEvent to metrics_worker_distributor. error: {:?}", error);
-        }
-    }; 
-    rml.set_metrics_callback(callback);
-
-    loop {
-        // try receive, if there is no pending message, then continue working
-        // this way the worker, is kept busy, until there is an incoming message.
-        let optional_message: Option<SignedMessage> = ctx.try_recv().await;
-        match optional_message {
-            Some(message) => {
-                MessageHandler::new(message)
-                    .on_tell(|miner_worker_message: MinerWorkerMessage, _| {
-                        println!(
-                            "miner_worker {}, received broadcast MinerWorkerMessage!:\n{:?}",
-                            ctx.current().id(),
-                            miner_worker_message
-                        );
-                        match miner_worker_message {
-                            MinerWorkerMessage::Ping => {
-                                println!("Ping");
-                            }
-                        }
-                    })
-                    .on_fallback(|unknown, _sender_addr| {
-                        error!(
-                            "miner_worker {}, received an unknown message!:\n{:?}",
-                            ctx.current().id(),
-                            unknown
-                        );
-                    });
-            },
-            None => {
-                let the_state: SharedMinerWorkerState = match shared_miner_worker_state.lock() {
-                    Ok(state) => *state,
-                    Err(error) => {
-                        error!("miner_worker. shared_miner_worker_state. Unable to lock mutex. {:?}", error);
-                        thread::sleep(Duration::from_millis(200));
-                        continue;
-                    }
-                };
-                if the_state == SharedMinerWorkerState::Paused {
-                    // Not mining, sleep for a while, and poll again
-                    thread::sleep(Duration::from_millis(200));
-                    continue;
-                }
-
-                // We are mining
-                // debug!("miner-worker {}: execute_batch", ctx.current().id());
-
-                let mut dependency_manager = DependencyManager::new(
-                    DependencyManagerFileSystemMode::System,
-                    loda_programs_oeis_dir.clone(),
-                );
-                dependency_manager.set_execute_profile(ExecuteProfile::SmallLimits);
-            
-                let result: ExecuteBatchResult = match rml.execute_batch(&mut dependency_manager) {
-                    Ok(value) => value,
-                    Err(error) => {
-                        error!(
-                            "miner_worker {}, execute_batch error: {:?}",
-                            ctx.current().id(),
-                            error
-                        );
-                        thread::sleep(Duration::from_millis(200));
-                        continue;
-                    }
-                };
-                // println!("execute_batch stats: {:?}", result);
-
-                let mut has_reached_mining_limit = false;
-                match mine_event_dir_state.lock() {
-                    Ok(mut state) => {
-                        state.accumulate_stats(&result);
-                        if state.has_reached_mining_limit() {
-                            // debug!("reached mining limit. {:?}", state);
-                            has_reached_mining_limit = true;
-                        }
-                    },
-                    Err(error) => {
-                        error!("miner_worker: mine_event_dir_state.lock() failed. {:?}", error);
-                    }
-                }
-
-                if has_reached_mining_limit {
-                    let mut trigger_start_postmine = false;
-                    match shared_miner_worker_state.lock() {
-                        Ok(mut state) => {
-                            if *state == SharedMinerWorkerState::Mining {
-                                trigger_start_postmine = true;
-                            }
-                            *state = SharedMinerWorkerState::Paused;
-                        },
-                        Err(error) => {
-                            error!("miner_worker: Unable to Pause all miner_workers. error: {:?}", error);
-                        }
-                    }
-
-                    if trigger_start_postmine {
-                        println!("trigger start postmine");
-                        thread::sleep(Duration::from_millis(1000));
-                        let tell_result = postmine_worker_distributor
-                            .tell_everyone(PostmineWorkerMessage::StartPostmineJob);
-                        if let Err(error) = tell_result {
-                            error!("miner_worker: Unable to send StartPostmineJob. error: {:?}", error);
-                        }
-                    }
-                }
-            }
-        }
     }
 }
