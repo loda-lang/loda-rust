8847063223d5e25eb1ee3489cfcb1ec99c5cddff
Simon Strandgaard <neoneye@gmail.com>
2022-11-24 17:59:34 +0100

    Replaced fragile state keeping with booleans and sleep(1 second), with a state machine.

--- rust_project/loda-rust-cli/src/mine/coordinator_worker.rs
+++ rust_project/loda-rust-cli/src/mine/coordinator_worker.rs
@@ -4,10 +4,10 @@ use super::MineEventDirectoryState;
 use super::MinerWorkerMessage;
 use super::PostmineWorkerMessage;
 use bastion::prelude::*;
-use std::time::Duration;
-use std::thread;
+use std::time::{Duration, Instant};
 
-const RECEIVE_TIMEOUT_SECONDS: u64 = 1 * 60; // 1 minute
+const RECEIVE_TIMEOUT_SECONDS: u64 = 1; // 1 second
+const STOP_MINING_SHUTDOWN_PERIOD_MILLIS: u128 = 1500; // 1.5 second
 
 #[derive(Clone, Debug)]
 pub enum CoordinatorWorkerMessage {
@@ -22,26 +22,24 @@ pub enum CoordinatorWorkerQuestion {
     MinerWorkerExecutedOneBatch { execute_batch_result: ExecuteBatchResult },
 }
 
-
 pub async fn coordinator_worker(
     ctx: BastionContext,
 ) -> Result<(), ()> {
     let timeout = Duration::from_secs(RECEIVE_TIMEOUT_SECONDS);
     let mut mineevent_dir_state = MineEventDirectoryState::new();
-    let mut is_postmine_running = false;
+    let mut state_machine = StateMachine::new();
     loop {
         let message: SignedMessage = match ctx.try_recv_timeout(timeout).await {
             Ok(message) => message,
             Err(error) => {
                 if let ReceiveError::Timeout(_duration) = error {
-                    error!("coordinator_worker: timeout happened. No activity for a while.");
+                    state_machine.timeout();
                     continue;
                 }
                 error!("coordinator_worker: Unknown error happened. error: {:?}", error);
                 continue;
             }
         };
-        let mut trigger_start_postmine_job: bool = false;
         MessageHandler::new(message)
             .on_tell(|message: CoordinatorWorkerMessage, _| {
                 println!(
@@ -51,19 +49,17 @@ pub async fn coordinator_worker(
                 );
                 match message {
                     CoordinatorWorkerMessage::RunLaunchProcedure => {
-                        run_launch_procedure();
+                        state_machine.run_launch_procedure();
                     },
                     CoordinatorWorkerMessage::CronjobTriggerSync => {
                         println!("!!!!!!!!! trigger sync")
                     },
                     CoordinatorWorkerMessage::SyncAndAnalyticsIsComplete => {
-                        start_mining();
+                        state_machine.sync_and_analytics_is_complete();
                     },
                     CoordinatorWorkerMessage::PostmineJobComplete => {
-                        is_postmine_running = false;
+                        state_machine.postmine_job_is_complete();
                         mineevent_dir_state.reset();
-                        println!("coordinator_worker: postmine job is complete. Resume mining again");
-                        start_mining();
                     }
                 }
             })
@@ -74,18 +70,12 @@ pub async fn coordinator_worker(
                 // );
                 match message {
                     CoordinatorWorkerQuestion::MinerWorkerExecutedOneBatch { execute_batch_result } => {
-                        debug!("coordinator_worker: executed one batch: {:?}", execute_batch_result);
-                        mineevent_dir_state.accumulate_stats(&execute_batch_result);
+                        state_machine.miner_worker_executed_one_batch(&execute_batch_result, &mut mineevent_dir_state);
                         let reply: String;
-                        if mineevent_dir_state.has_reached_mining_limit() {
-                            // debug!("reached mining limit. {:?}", state);
-                            // don't schedule another batch execute. Instead trigger the postmine job to run.
-                            trigger_start_postmine_job = true;
-                            reply = "stop".to_string();
-                        } else {
-                            // the discovery count is lower than the limit
-                            // tell the worker to execute another batch
+                        if state_machine.should_continue_mining() {
                             reply = "continue".to_string();
+                        } else {
+                            reply = "stop".to_string();
                         }
                         match sender.reply(reply) {
                             Ok(value) => {
@@ -105,26 +95,6 @@ pub async fn coordinator_worker(
                     unknown
                 );
             });
-        if trigger_start_postmine_job {
-            // ensure it only gets triggered once. 
-            // if postmine is already in progress, and it gets triggered over and over, then it's a mess.
-            if is_postmine_running {
-                // do nothing                
-            } else {
-                is_postmine_running = true;
-                // TODO: if the discovery count is greater than the limit, wait for all workers to finish, tell postmine to run
-                println!("coordinator_worker: trigger start postmine - pretend we are waiting for all workers to finish");
-                thread::sleep(Duration::from_millis(1000));
-    
-                println!("coordinator_worker: trigger start postmine - pretend all miner workers have finished");
-                let distributor = Distributor::named("postmine_worker");
-                let tell_result = distributor
-                    .tell_everyone(PostmineWorkerMessage::StartPostmineJob);
-                if let Err(error) = tell_result {
-                    error!("coordinator_worker: Unable to send StartPostmineJob. error: {:?}", error);
-                }
-            }
-        }
     }
 }
 
@@ -144,5 +114,294 @@ fn start_mining() {
     if let Err(error) = tell_result {
         Bastion::stop();
         panic!("coordinator_worker: Unable to send StartMining to miner_worker_distributor. error: {:?}", error);
+    }
+}
+
+trait State: Send {
+    fn run_launch_procedure(self: Box<Self>) -> Box<dyn State>;
+    fn sync_and_analytics_is_complete(self: Box<Self>) -> Box<dyn State>;
+    fn miner_worker_executed_one_batch(
+        self: Box<Self>, 
+        execute_batch_result: &ExecuteBatchResult,
+        mineevent_directory_state: &mut MineEventDirectoryState,
+    ) -> Box<dyn State>;
+
+    fn should_continue_mining(&self) -> bool;
+
+    fn postmine_job_is_complete(self: Box<Self>) -> Box<dyn State>;
+
+    fn timeout(self: Box<Self>) -> Box<dyn State>;
+}
+
+struct InitialState;
+
+impl State for InitialState {
+    fn run_launch_procedure(self: Box<Self>) -> Box<dyn State> {
+        run_launch_procedure();
+        Box::new(RunLaunchProcedureInProgressState {})
+    }
+
+    fn sync_and_analytics_is_complete(self: Box<Self>) -> Box<dyn State> {
+        error!("InitialState.sync_and_analytics_is_complete() called, but is never supposed to be invoked in this state");
+        self
+    }
+
+    fn miner_worker_executed_one_batch(
+        self: Box<Self>, 
+        _execute_batch_result: &ExecuteBatchResult,
+        _mineevent_directory_state: &mut MineEventDirectoryState,
+    ) -> Box<dyn State> {
+        error!("InitialState.miner_worker_executed_one_batch() called, but is never supposed to be invoked in this state");
+        self
+    }
+
+    fn should_continue_mining(&self) -> bool {
+        error!("InitialState.should_continue_mining() called, but is never supposed to be invoked in this state");
+        false
+    }
+
+    fn postmine_job_is_complete(self: Box<Self>) -> Box<dyn State> {
+        error!("InitialState.postmine_job_is_complete() called, but is never supposed to be invoked in this state");
+        self
+    }
+
+    fn timeout(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+struct RunLaunchProcedureInProgressState;
+
+impl State for RunLaunchProcedureInProgressState {
+    fn run_launch_procedure(self: Box<Self>) -> Box<dyn State> {
+        error!("RunLaunchProcedureInProgressState.run_launch_procedure() called, but is never supposed to be invoked in this state");
+        self
+    }
+
+    fn sync_and_analytics_is_complete(self: Box<Self>) -> Box<dyn State> {
+        start_mining();
+        Box::new(MiningInProgressState {})
+    }
+
+    fn miner_worker_executed_one_batch(
+        self: Box<Self>, 
+        _execute_batch_result: &ExecuteBatchResult,
+        _mineevent_directory_state: &mut MineEventDirectoryState,
+    ) -> Box<dyn State> {
+        error!("RunLaunchProcedureInProgressState.miner_worker_executed_one_batch() called, but is never supposed to be invoked in this state");
+        self
+    }
+
+    fn should_continue_mining(&self) -> bool {
+        error!("RunLaunchProcedureInProgressState.should_continue_mining() called, but is never supposed to be invoked in this state");
+        false
+    }
+
+    fn postmine_job_is_complete(self: Box<Self>) -> Box<dyn State> {
+        error!("RunLaunchProcedureInProgressState.postmine_job_is_complete() called, but is never supposed to be invoked in this state");
+        self
+    }
+
+    fn timeout(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+struct MiningInProgressState;
+
+impl State for MiningInProgressState {
+    fn run_launch_procedure(self: Box<Self>) -> Box<dyn State> {
+        error!("MiningInProgressState.run_launch_procedure() called, but is never supposed to be invoked in this state");
+        self
+    }
+
+    fn sync_and_analytics_is_complete(self: Box<Self>) -> Box<dyn State> {
+        error!("MiningInProgressState.sync_and_analytics_is_complete() called, but is never supposed to be invoked in this state");
+        self
+    }
+
+    fn miner_worker_executed_one_batch(
+        self: Box<Self>, 
+        execute_batch_result: &ExecuteBatchResult,
+        mineevent_directory_state: &mut MineEventDirectoryState,
+    ) -> Box<dyn State> {
+        debug!("MiningInProgressState: executed one batch: {:?}", execute_batch_result);
+        mineevent_directory_state.accumulate_stats(&execute_batch_result);
+
+        if !mineevent_directory_state.has_reached_mining_limit() {
+            // Stay in this state, and accumulate candidate programs
+            return self;
+        }
+        println!("MiningInProgressState. the number of accumulated candiate programs has reached the limit");
+        return Box::new(MiningIsStoppingState::new());
+    }
+
+    fn should_continue_mining(&self) -> bool {
+        // Keep the miner_workers busy with mining
+        true
+    }
+
+    fn postmine_job_is_complete(self: Box<Self>) -> Box<dyn State> {
+        error!("MiningInProgressState.postmine_job_is_complete() called, but is never supposed to be invoked in this state");
+        self
+    }
+
+    fn timeout(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+/// Wait for all miner_worker instances to complete their mining job
+struct MiningIsStoppingState {
+    start_time: Instant,
+}
+
+impl MiningIsStoppingState {
+    fn new() -> Self {
+        let start_time = Instant::now();
+        Self { start_time }
+    }
+}
+
+impl State for MiningIsStoppingState {
+    fn run_launch_procedure(self: Box<Self>) -> Box<dyn State> {
+        error!("MiningIsStoppingState.run_launch_procedure() called, but is never supposed to be invoked in this state");
+        self
+    }
+
+    fn sync_and_analytics_is_complete(self: Box<Self>) -> Box<dyn State> {
+        error!("MiningIsStoppingState.sync_and_analytics_is_complete() called, but is never supposed to be invoked in this state");
+        self
+    }
+
+    fn miner_worker_executed_one_batch(
+        self: Box<Self>, 
+        execute_batch_result: &ExecuteBatchResult,
+        mineevent_directory_state: &mut MineEventDirectoryState,
+    ) -> Box<dyn State> {
+        debug!("MiningIsStoppingState: executed one batch: {:?}", execute_batch_result);
+        mineevent_directory_state.accumulate_stats(&execute_batch_result);
+        return self;
+    }
+
+    fn should_continue_mining(&self) -> bool {
+        // Tell miner_worker to stop mining
+        false
+    }
+
+    fn postmine_job_is_complete(self: Box<Self>) -> Box<dyn State> {
+        error!("MiningIsStoppingState.postmine_job_is_complete() called, but is never supposed to be invoked in this state");
+        self
+    }
+
+    fn timeout(self: Box<Self>) -> Box<dyn State> {
+        let elapsed: u128 = self.start_time.elapsed().as_millis();
+        if elapsed < STOP_MINING_SHUTDOWN_PERIOD_MILLIS {
+            // Stay in this state while waiting for all the miner_worker instances 
+            // to complete their mining jobs
+            return self;
+        }
+
+        println!("MiningIsStoppingState: trigger start postmine");
+        let distributor = Distributor::named("postmine_worker");
+        let tell_result = distributor
+            .tell_everyone(PostmineWorkerMessage::StartPostmineJob);
+        if let Err(error) = tell_result {
+            error!("MiningIsStoppingState: Unable to send StartPostmineJob. error: {:?}", error);
+        }
+        
+        // Transition to the "postmine" state.
+        return Box::new(PostmineInProgressState {});
+    }
+}
+
+struct PostmineInProgressState;
+
+impl State for PostmineInProgressState {
+    fn run_launch_procedure(self: Box<Self>) -> Box<dyn State> {
+        error!("PostmineInProgressState.run_launch_procedure() called, but is never supposed to be invoked in this state");
+        self
+    }
+
+    fn sync_and_analytics_is_complete(self: Box<Self>) -> Box<dyn State> {
+        error!("PostmineInProgressState.sync_and_analytics_is_complete() called, but is never supposed to be invoked in this state");
+        self
+    }
+
+    fn miner_worker_executed_one_batch(
+        self: Box<Self>, 
+        _execute_batch_result: &ExecuteBatchResult,
+        _mineevent_directory_state: &mut MineEventDirectoryState,
+    ) -> Box<dyn State> {
+        error!("PostmineInProgressState.miner_worker_executed_one_batch() called, but is never supposed to be invoked in this state");
+        self
+    }
+
+    fn should_continue_mining(&self) -> bool {
+        error!("PostmineInProgressState.should_continue_mining() called, but is never supposed to be invoked in this state");
+        false
+    }
+
+    fn postmine_job_is_complete(self: Box<Self>) -> Box<dyn State> {
+        println!("PostmineInProgressState: postmine job is complete. Resume mining again");
+        start_mining();
+        Box::new(MiningInProgressState {})
+    }
+
+    fn timeout(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+struct StateMachine {
+    state: Option<Box<dyn State>>,
+}
+
+impl StateMachine {
+    fn new() -> Self {
+        Self {
+            state: Some(Box::new(InitialState {})),
+        }
+    }
+
+    fn run_launch_procedure(&mut self) {
+        if let Some(state) = self.state.take() {
+            self.state = Some(state.run_launch_procedure());
+        }
+    }
+
+    fn sync_and_analytics_is_complete(&mut self) {
+        if let Some(state) = self.state.take() {
+            self.state = Some(state.sync_and_analytics_is_complete());
+        }
+    }
+
+    fn miner_worker_executed_one_batch(&mut self, execute_batch_result: &ExecuteBatchResult, mineevent_directory_state: &mut MineEventDirectoryState) {
+        if let Some(state) = self.state.take() {
+            let new_state = state.miner_worker_executed_one_batch(
+                execute_batch_result, 
+                mineevent_directory_state
+            );
+            self.state = Some(new_state);
+        }
+    }
+
+    fn should_continue_mining(&mut self) -> bool {
+        if let Some(state) = &self.state {
+            return state.should_continue_mining();
+        }
+        false
+    }
+
+    fn postmine_job_is_complete(&mut self) {
+        if let Some(state) = self.state.take() {
+            self.state = Some(state.postmine_job_is_complete());
+        }
+    }
+
+    fn timeout(&mut self) {
+        if let Some(state) = self.state.take() {
+            self.state = Some(state.timeout());
+        }
     }
 }
