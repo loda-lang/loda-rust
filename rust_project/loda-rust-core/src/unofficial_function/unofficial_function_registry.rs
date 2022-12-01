6888ed189430bccabc017c782bdf0aef741de5c9
Simon Strandgaard <neoneye@gmail.com>
2022-12-01 17:15:14 +0100

    Moved tests to core

--- rust_project/loda-rust-core/src/unofficial_function/unofficial_function_registry.rs
+++ rust_project/loda-rust-core/src/unofficial_function/unofficial_function_registry.rs
@@ -45,3 +45,56 @@ impl UnofficialFunctionRegistry {
         None
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::unofficial_function::SumFunction;
+    use num_bigint::{BigInt, ToBigInt};
+    use std::sync::Arc;
+
+    #[test]
+    fn test_10000_lookup() {
+        let registry = UnofficialFunctionRegistry::new();
+        let plugin = SumFunction::new(1234, 2);
+        registry.register(Arc::new(Box::new(plugin)));
+        let key = UnofficialFunctionId::InputOutput { 
+            id: 1234, 
+            inputs: 2, 
+            outputs: 1 
+        };
+
+        // Act
+        let unofficial_function: Arc<Box<dyn UnofficialFunction>> = registry.lookup(key).expect("unofficial_function");
+
+        // Assert
+        let input_vec: Vec<BigInt> = vec![1000.to_bigint().unwrap(), 1.to_bigint().unwrap()];
+        let output_vec: Vec<BigInt> = unofficial_function.run(input_vec).expect("output");
+        let expected_output_vec: Vec<BigInt> = vec![1001.to_bigint().unwrap()];
+        assert_eq!(output_vec, expected_output_vec);
+    }
+
+    #[test]
+    fn test_20000_clone() {
+        // Arrange
+        let registry_original = UnofficialFunctionRegistry::new();
+        let registry: UnofficialFunctionRegistry = registry_original.clone();
+        let plugin = SumFunction::new(1234, 3);
+        registry.register(Arc::new(Box::new(plugin)));
+        let key = UnofficialFunctionId::InputOutput { 
+            id: 1234, 
+            inputs: 3, 
+            outputs: 1 
+        };
+        
+        // Act
+        // Check that the original registry contains the item
+        let unofficial_function: Arc<Box<dyn UnofficialFunction>> = registry_original.lookup(key).expect("unofficial_function");
+
+        // Assert
+        let input_vec: Vec<BigInt> = vec![100.to_bigint().unwrap(), 1.to_bigint().unwrap(), 10.to_bigint().unwrap()];
+        let output_vec: Vec<BigInt> = unofficial_function.run(input_vec).expect("output");
+        let expected_output_vec: Vec<BigInt> = vec![111.to_bigint().unwrap()];
+        assert_eq!(output_vec, expected_output_vec);
+    }
+}
