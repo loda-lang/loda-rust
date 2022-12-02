225222ac846109dd05cfce74022fd2b6b3efcaa2
Simon Strandgaard <neoneye@gmail.com>
2022-12-02 13:34:26 +0100

    ImageReplaceColorFunction added

--- rust_project/loda-rust-cli/src/arc/register_arc_functions.rs
+++ rust_project/loda-rust-cli/src/arc/register_arc_functions.rs
@@ -1,4 +1,4 @@
-use super::{Image, ImageToNumber, NumberToImage, ImageOffset, ImageTrim, ImageRemoveDuplicates, ImageRotate};
+use super::{Image, ImageToNumber, NumberToImage, ImageOffset, ImageTrim, ImageRemoveDuplicates, ImageRotate, ImageReplaceColor};
 use loda_rust_core::unofficial_function::{UnofficialFunction, UnofficialFunctionId, UnofficialFunctionRegistry};
 use num_bigint::{BigInt, BigUint, ToBigInt};
 use num_traits::{Signed, ToPrimitive};
@@ -175,9 +175,56 @@ impl UnofficialFunction for ImageRemoveDuplicatesFunction {
 }
 
 
+pub struct ImageReplaceColorFunction {
+    id: u32,
+}
+
+impl ImageReplaceColorFunction {
+    pub fn new(id: u32) -> Self {
+        Self {
+            id,
+        }
+    }
+}
+
+impl UnofficialFunction for ImageReplaceColorFunction {
+    fn id(&self) -> UnofficialFunctionId {
+        UnofficialFunctionId::InputOutput { id: self.id, inputs: 3, outputs: 1 }
+    }
+
+    fn name(&self) -> String {
+        "Image: replace color x with color y".to_string()
+    }
+
+    fn run(&self, input: Vec<BigInt>) -> anyhow::Result<Vec<BigInt>> {
+        if input.len() != 3 {
+            return Err(anyhow::anyhow!("Wrong number of inputs"));
+        }
+
+        // input0 is image
+        if input[0].is_negative() {
+            return Err(anyhow::anyhow!("Input[0] must be non-negative"));
+        }
+        let input0_uint: BigUint = input[0].to_biguint().context("BigInt to BigUint")?;
+        let input_image: Image = input0_uint.to_image()?;
+
+        // input1 is color x
+        let from_color: u8 = input[1].to_u8().context("u8 from_color")?;
+
+        // input2 is color y
+        let to_color: u8 = input[2].to_u8().context("u8 to_color")?;
+
+        let output_image: Image = input_image.replace_color(from_color, to_color)?;
+        let output_uint: BigUint = output_image.to_number()?;
+        let output: BigInt = output_uint.to_bigint().context("BigUint to BigInt")?;
+        Ok(vec![output])
+    }
+}
+
 pub fn register_arc_functions(registry: &UnofficialFunctionRegistry) {
     registry.register(Arc::new(Box::new(ImageOffsetFunction::new(100001))));
     registry.register(Arc::new(Box::new(ImageRotateFunction::new(100002))));
     registry.register(Arc::new(Box::new(ImageTrimFunction::new(100003))));
     registry.register(Arc::new(Box::new(ImageRemoveDuplicatesFunction::new(100004))));
+    registry.register(Arc::new(Box::new(ImageReplaceColorFunction::new(100005))));
 }
