d5f37b7783056b94ec8dcf1c1324cb8fb698d8d5
Simon Strandgaard <neoneye@gmail.com>
2022-12-02 13:57:41 +0100

    ImageSetPixelFunction added

--- rust_project/loda-rust-cli/src/arc/register_arc_functions.rs
+++ rust_project/loda-rust-cli/src/arc/register_arc_functions.rs
@@ -263,6 +263,59 @@ impl UnofficialFunction for ImageWithColorFunction {
     }
 }
 
+pub struct ImageSetPixelFunction {
+    id: u32,
+}
+
+impl ImageSetPixelFunction {
+    pub fn new(id: u32) -> Self {
+        Self {
+            id,
+        }
+    }
+}
+
+impl UnofficialFunction for ImageSetPixelFunction {
+    fn id(&self) -> UnofficialFunctionId {
+        UnofficialFunctionId::InputOutput { id: self.id, inputs: 4, outputs: 1 }
+    }
+
+    fn name(&self) -> String {
+        "Image: set pixel at (x, y) with color z".to_string()
+    }
+
+    fn run(&self, input: Vec<BigInt>) -> anyhow::Result<Vec<BigInt>> {
+        if input.len() != 4 {
+            return Err(anyhow::anyhow!("Wrong number of inputs"));
+        }
+
+        // input0 is image
+        if input[0].is_negative() {
+            return Err(anyhow::anyhow!("Input[0] must be non-negative"));
+        }
+        let input0_uint: BigUint = input[0].to_biguint().context("BigInt to BigUint")?;
+        let mut image: Image = input0_uint.to_image()?;
+
+        // input1 is position_x
+        let position_x: u8 = input[1].to_u8().context("u8 position_x")?;
+
+        // input2 is position_y
+        let position_y: u8 = input[2].to_u8().context("u8 position_y")?;
+
+        // input3 is pixel_color 
+        let pixel_color: u8 = input[3].to_u8().context("u8 pixel_color")?;
+
+        image.set(
+            position_x as i32, 
+            position_y as i32, 
+            pixel_color
+        ).context("set pixel")?;
+        let output_uint: BigUint = image.to_number()?;
+        let output: BigInt = output_uint.to_bigint().context("BigUint to BigInt")?;
+        Ok(vec![output])
+    }
+}
+
 pub fn register_arc_functions(registry: &UnofficialFunctionRegistry) {
     registry.register(Arc::new(Box::new(ImageOffsetFunction::new(100001))));
     registry.register(Arc::new(Box::new(ImageRotateFunction::new(100002))));
@@ -270,4 +323,5 @@ pub fn register_arc_functions(registry: &UnofficialFunctionRegistry) {
     registry.register(Arc::new(Box::new(ImageRemoveDuplicatesFunction::new(100004))));
     registry.register(Arc::new(Box::new(ImageReplaceColorFunction::new(100005))));
     registry.register(Arc::new(Box::new(ImageWithColorFunction::new(100006))));
+    registry.register(Arc::new(Box::new(ImageSetPixelFunction::new(100007))));
 }
