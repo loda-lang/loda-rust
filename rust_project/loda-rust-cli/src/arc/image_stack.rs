36d240ddc097c9a92d94b5ad7d762cf128b1c41f
Simon Strandgaard <neoneye@gmail.com>
2022-12-07 19:55:18 +0100

    ImageStack: hstack() and vstack() now allows for combining empty images with non-empty images.

--- rust_project/loda-rust-cli/src/arc/image_stack.rs
+++ rust_project/loda-rust-cli/src/arc/image_stack.rs
@@ -10,21 +10,28 @@ pub trait ImageStack {
 
 impl ImageStack for Image {
     fn hstack(images: Vec<Image>) -> anyhow::Result<Image> {
-        let first_image: &Image = match images.first() {
-            Some(image) => image,
-            None => {
-                return Ok(Image::empty());
-            }
-        };
-        let height = first_image.height();
-
-        // Compute width of all the images combined
-        // Verify that the height is the same of all the images
-        let mut width_usize: usize = 0;
+        // Determine the `height` of the image. Ignore empty images.
+        let mut height: u8 = 0;
         for image in &images {
+            if image.is_empty() {
+                continue;
+            }
+            if height == 0 {
+                height = image.height();
+                continue;
+            }
+            // Verify that `height` is the same of all the non-empty images
             if image.height() != height {
                 return Err(anyhow::anyhow!("hstack: Cannot horizontal stack images with different heights"));
             }
+        }
+        if height == 0 {
+            return Ok(Image::empty());
+        }
+
+        // Compute the `width` of all the images combined
+        let mut width_usize: usize = 0;
+        for image in &images {
             width_usize += image.width() as usize;
         }
         if width_usize > 255 {
@@ -39,6 +46,9 @@ impl ImageStack for Image {
         let mut result_image = Image::zero(width, height);
         let mut current_x: i32 = 0;
         for image in images {
+            if image.is_empty() {
+                continue;
+            }
             for y in 0..image.height() {
                 for x in 0..image.width() {
                     let pixel_value: u8 = image.get(x as i32, y as i32).unwrap_or(255);
@@ -58,21 +68,28 @@ impl ImageStack for Image {
     }
 
     fn vstack(images: Vec<Image>) -> anyhow::Result<Image> {
-        let first_image: &Image = match images.first() {
-            Some(image) => image,
-            None => {
-                return Ok(Image::empty());
-            }
-        };
-        let width = first_image.width();
-
-        // Compute height of all the images combined
-        // Verify that the width is the same of all the images
-        let mut height_usize: usize = 0;
+        // Determine the `width` of the image. Ignore empty images.
+        let mut width: u8 = 0;
         for image in &images {
+            if image.is_empty() {
+                continue;
+            }
+            if width == 0 {
+                width = image.width();
+                continue;
+            }
+            // Verify that `width` is the same of all the non-empty images
             if image.width() != width {
                 return Err(anyhow::anyhow!("vstack: Cannot vertical stack images with different widths"));
             }
+        }
+        if width == 0 {
+            return Ok(Image::empty());
+        }
+
+        // Compute the `height` of all the images combined
+        let mut height_usize: usize = 0;
+        for image in &images {
             height_usize += image.height() as usize;
         }
         if height_usize > 255 {
@@ -87,6 +104,9 @@ impl ImageStack for Image {
         let mut result_image = Image::zero(width, height);
         let mut current_y: i32 = 0;
         for image in images {
+            if image.is_empty() {
+                continue;
+            }
             for y in 0..image.height() {
                 for x in 0..image.width() {
                     let pixel_value: u8 = image.get(x as i32, y as i32).unwrap_or(255);
@@ -145,6 +165,40 @@ mod tests {
     }
 
     #[test]
+    fn test_10002_hstack_mixed_empty() {
+        // Arrange
+        let image0: Image = Image::empty();
+        let image1: Image = Image::color(2, 2, 0);
+        let image2: Image = Image::empty();
+        let image3: Image = Image::color(3, 2, 1);
+        let image4: Image = Image::empty();
+
+        // Act
+        let actual: Image = Image::hstack(vec![image0, image1, image2, image3, image4]).expect("image");
+
+        // Assert
+        let expected_pixels: Vec<u8> = vec![
+            0, 0, 1, 1, 1,
+            0, 0, 1, 1, 1,
+        ];
+        let expected: Image = Image::try_create(5, 2, expected_pixels).expect("image");
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_10003_hstack_entirely_empty() {
+        // Arrange
+        let image0: Image = Image::empty();
+        let image1: Image = Image::empty();
+
+        // Act
+        let actual: Image = Image::hstack(vec![image0, image1]).expect("image");
+
+        // Assert
+        assert_eq!(actual, Image::empty());
+    }
+
+    #[test]
     fn test_20000_vstack_small() {
         // Arrange
         let image0: Image = Image::color(1, 1, 0);
@@ -178,5 +232,42 @@ mod tests {
         ];
         let expected: Image = Image::try_create(2, 5, expected_pixels).expect("image");
         assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_20002_vstack_mixed_empty() {
+        // Arrange
+        let image0: Image = Image::empty();
+        let image1: Image = Image::color(2, 2, 0);
+        let image2: Image = Image::empty();
+        let image3: Image = Image::color(2, 3, 1);
+        let image4: Image = Image::empty();
+
+        // Act
+        let actual: Image = Image::vstack(vec![image0, image1, image2, image3, image4]).expect("image");
+
+        // Assert
+        let expected_pixels: Vec<u8> = vec![
+            0, 0,
+            0, 0,
+            1, 1,
+            1, 1,
+            1, 1,
+        ];
+        let expected: Image = Image::try_create(2, 5, expected_pixels).expect("image");
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_20003_vstack_entirely_empty() {
+        // Arrange
+        let image0: Image = Image::empty();
+        let image1: Image = Image::empty();
+
+        // Act
+        let actual: Image = Image::vstack(vec![image0, image1]).expect("image");
+
+        // Assert
+        assert_eq!(actual, Image::empty());
     }
 }
