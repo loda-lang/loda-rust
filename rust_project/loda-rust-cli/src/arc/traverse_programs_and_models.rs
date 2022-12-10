35698403b43056cc4efd0ffdbead91e20b9602e1
Simon Strandgaard <neoneye@gmail.com>
2022-12-10 23:12:11 +0100

    convert json files to Model instances

--- rust_project/loda-rust-cli/src/arc/traverse_programs_and_models.rs
+++ rust_project/loda-rust-cli/src/arc/traverse_programs_and_models.rs
@@ -1,31 +1,68 @@
+use super::{Model, Image, GridToImage, ImagePair};
 use crate::config::Config;
 use crate::common::find_json_files_recursively;
 use crate::common::find_asm_files_recursively;
 use std::path::PathBuf;
 
-pub struct TraverseProgramsAndModels {}
+pub struct TraverseProgramsAndModels {
+    config: Config,
+    model_item_vec: Vec<ModelItem>,
+}
 
 impl TraverseProgramsAndModels {
     pub async fn run() -> anyhow::Result<()> {
-        let mut instance = Self {};
+        let config = Config::load();
+        let mut instance = Self { 
+            config,
+            model_item_vec: vec!(),
+        };
+        instance.load_arc_models()?;
+        instance.load_programs()?;
         instance.run_inner()?;
         Ok(())
     }
 
-    fn run_inner(&mut self) -> anyhow::Result<()> {
+    fn load_arc_models(&mut self) -> anyhow::Result<()> {
+        let path: PathBuf = self.config.arc_repository_data_training();
+        let paths: Vec<PathBuf> = find_json_files_recursively(&path);
+        println!("arc_repository_data_training. number of json files: {}", paths.len());
 
-        let config = Config::load();
-        {
-            let path: PathBuf = config.arc_repository_data_training();
-            let paths: Vec<PathBuf> = find_json_files_recursively(&path);
-            println!("number of json files: {}", paths.len());
+        let mut model_item_vec = Vec::<ModelItem>::new();
+        for path in &paths {
+            let model = Model::load_with_json_file(path).expect("model");
+            let item = ModelItem {
+                id: ModelItemId::Path { path: path.clone() },
+                model,
+            };
+            model_item_vec.push(item);
         }
-        {
-            let path: PathBuf = config.loda_arc_challenge_repository_programs();
-            let paths: Vec<PathBuf> = find_asm_files_recursively(&path);
-            println!("number of asm files: {}", paths.len());
-        }
-
+        println!("model_item_vec.len(): {}", model_item_vec.len());
+        self.model_item_vec = model_item_vec;
         Ok(())
     }
+
+    fn load_programs(&mut self) -> anyhow::Result<()> {
+        let path: PathBuf = self.config.loda_arc_challenge_repository_programs();
+        let paths: Vec<PathBuf> = find_asm_files_recursively(&path);
+        println!("loda_arc_challenge_repository_programs. number of asm files: {}", paths.len());
+        Ok(())
+    }
+
+    fn run_inner(&mut self) -> anyhow::Result<()> {
+        Ok(())
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Clone, Debug)]
+enum ModelItemId {
+    None,
+    Path { path: PathBuf },
+}
+
+#[allow(dead_code)]
+#[derive(Clone, Debug)]
+struct ModelItem {
+    id: ModelItemId,
+    model: Model,
 }
