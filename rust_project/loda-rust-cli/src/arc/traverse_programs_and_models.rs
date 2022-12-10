1b29a9e499c6bf72e4505fcddcd897c9b31ebbf4
Simon Strandgaard <neoneye@gmail.com>
2022-12-10 23:47:26 +0100

    Loading paths into ProgramItem's

--- rust_project/loda-rust-cli/src/arc/traverse_programs_and_models.rs
+++ rust_project/loda-rust-cli/src/arc/traverse_programs_and_models.rs
@@ -2,11 +2,13 @@ use super::{Model, Image, GridToImage, ImagePair};
 use crate::config::Config;
 use crate::common::find_json_files_recursively;
 use crate::common::find_asm_files_recursively;
+use std::fs;
 use std::path::PathBuf;
 
 pub struct TraverseProgramsAndModels {
     config: Config,
     model_item_vec: Vec<ModelItem>,
+    program_item_vec: Vec<ProgramItem>,
 }
 
 impl TraverseProgramsAndModels {
@@ -15,6 +17,7 @@ impl TraverseProgramsAndModels {
         let mut instance = Self { 
             config,
             model_item_vec: vec!(),
+            program_item_vec: vec!(),
         };
         instance.load_arc_models()?;
         instance.load_programs()?;
@@ -36,7 +39,9 @@ impl TraverseProgramsAndModels {
             };
             model_item_vec.push(item);
         }
-        println!("model_item_vec.len(): {}", model_item_vec.len());
+        if model_item_vec.len() != paths.len() {
+            error!("Skipped some models. paths.len()={}, but model_item_vec.len()={}", paths.len(), model_item_vec.len());
+        }
         self.model_item_vec = model_item_vec;
         Ok(())
     }
@@ -45,6 +50,49 @@ impl TraverseProgramsAndModels {
         let path: PathBuf = self.config.loda_arc_challenge_repository_programs();
         let paths: Vec<PathBuf> = find_asm_files_recursively(&path);
         println!("loda_arc_challenge_repository_programs. number of asm files: {}", paths.len());
+
+        let mut program_item_vec: Vec<ProgramItem> = vec!();
+        for path in &paths {
+
+            let program_string: String = match fs::read_to_string(path) {
+                Ok(value) => value,
+                Err(error) => {
+                    error!("cannot read the file: {:?} error: {:?}", path, error);
+                    continue;
+                }
+            };
+
+            let is_simple: bool = program_string.contains("Program Type: simple");
+            let is_advanced: bool = program_string.contains("Program Type: advanced");
+            let program_type: ProgramType;
+            match (is_simple, is_advanced) {
+                (false, false) => {
+                    error!("Cannot find 'Program Type: simple' nor 'Program Type: advanced'. Skipping program. path: {:?}", path);
+                    continue;
+                },
+                (false, true) => {
+                    program_type = ProgramType::Advance;
+                },
+                (true, false) => {
+                    program_type = ProgramType::Simple;
+                },
+                (true, true) => {
+                    error!("Ambiguous use of 'Program Type'. Should be either 'Program Type: simple' or 'Program Type: advanced'. Skipping program. path: {:?}", path);
+                    continue;
+                }
+            }
+
+            let item = ProgramItem {
+                id: ProgramItemId::Path { path: path.clone() },
+                program_string,
+                program_type,
+            };
+            program_item_vec.push(item);
+        }
+        if program_item_vec.len() != paths.len() {
+            error!("Skipped some programs. paths.len()={}, but program_item_vec.len()={}", paths.len(), program_item_vec.len());
+        }
+        self.program_item_vec = program_item_vec;
         Ok(())
     }
 
@@ -53,16 +101,33 @@ impl TraverseProgramsAndModels {
     }
 }
 
-#[allow(dead_code)]
 #[derive(Clone, Debug)]
 enum ModelItemId {
     None,
     Path { path: PathBuf },
 }
 
-#[allow(dead_code)]
 #[derive(Clone, Debug)]
 struct ModelItem {
     id: ModelItemId,
     model: Model,
+}
+
+#[derive(Clone, Debug)]
+enum ProgramType {
+    Simple,
+    Advance,
+}
+
+#[derive(Clone, Debug)]
+enum ProgramItemId {
+    None,
+    Path { path: PathBuf },
+}
+
+#[derive(Clone, Debug)]
+struct ProgramItem {
+    id: ProgramItemId,
+    program_string: String,
+    program_type: ProgramType,
 }
