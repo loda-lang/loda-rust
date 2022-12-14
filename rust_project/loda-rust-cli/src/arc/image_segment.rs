051ae1060ecc62e89edb0595ae7548667ed9b4d6
Simon Strandgaard <neoneye@gmail.com>
2022-12-14 13:04:36 +0100

    flood_fill_visit added

--- rust_project/loda-rust-cli/src/arc/image_segment.rs
+++ rust_project/loda-rust-cli/src/arc/image_segment.rs
@@ -1,7 +1,11 @@
 use super::Image;
 
 pub trait ImageSegment {
+    /// Replace color with another color
     fn flood_fill(&mut self, x: i32, y: i32, from_color: u8, to_color: u8);
+
+    /// Build a mask of connected pixels that has the same color
+    fn flood_fill_visit(&mut self, image: &Image, x: i32, y: i32, color: u8);
 }
 
 impl ImageSegment for Image {
@@ -18,6 +22,28 @@ impl ImageSegment for Image {
         self.flood_fill(x+1, y, from_color, to_color);
         self.flood_fill(x, y-1, from_color, to_color);
         self.flood_fill(x, y+1, from_color, to_color);
+    }
+
+    fn flood_fill_visit(&mut self, image: &Image, x: i32, y: i32, color: u8) {
+        assert!(self.width() == image.width());
+        assert!(self.height() == image.height());
+        if x < 0 || y < 0 || x >= (self.width() as i32) || y >= (self.height() as i32) {
+            return;
+        }
+        let mask_value: u8 = self.get(x, y).unwrap_or(255);
+        if mask_value > 0 {
+            // already visited
+            return;
+        }
+        let value: u8 = image.get(x, y).unwrap_or(255);
+        if value != color {
+            return;
+        }
+        let _ = self.set(x, y, 1); // flag as visited
+        self.flood_fill_visit(image, x-1, y, color);
+        self.flood_fill_visit(image, x+1, y, color);
+        self.flood_fill_visit(image, x, y-1, color);
+        self.flood_fill_visit(image, x, y+1, color);
     }
 }
 
@@ -99,6 +125,87 @@ mod tests {
             5, 8, 8, 5, 1,
             5, 8, 5, 5, 1,
             5, 5, 5, 5, 1,
+        ];
+        let expected = Image::create_raw(5, 4, expected_pixels);
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_20000_flood_fill_visit() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            5, 5, 5, 5, 5,
+            5, 8, 8, 5, 8,
+            5, 8, 5, 5, 8,
+            5, 5, 5, 5, 8,
+        ];
+        let input: Image = Image::try_create(5, 4, pixels).expect("image");
+        let mut output = Image::zero(5, 4);
+        let color: u8 = input.get(0, 0).unwrap_or(255);
+
+        // Act
+        output.flood_fill_visit(&input, 0, 0, color);
+
+        // Assert
+        let expected_pixels: Vec<u8> = vec![
+            1, 1, 1, 1, 1,
+            1, 0, 0, 1, 0,
+            1, 0, 1, 1, 0,
+            1, 1, 1, 1, 0,
+        ];
+        let expected = Image::create_raw(5, 4, expected_pixels);
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_20001_flood_fill_visit() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            5, 5, 5, 5, 5,
+            5, 8, 8, 5, 8,
+            5, 8, 5, 5, 8,
+            5, 5, 5, 5, 8,
+        ];
+        let input: Image = Image::try_create(5, 4, pixels).expect("image");
+        let mut output = Image::zero(5, 4);
+        let color: u8 = input.get(1, 1).unwrap_or(255);
+
+        // Act
+        output.flood_fill_visit(&input, 1, 1, color);
+
+        // Assert
+        let expected_pixels: Vec<u8> = vec![
+            0, 0, 0, 0, 0,
+            0, 1, 1, 0, 0,
+            0, 1, 0, 0, 0,
+            0, 0, 0, 0, 0,
+        ];
+        let expected = Image::create_raw(5, 4, expected_pixels);
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_20002_flood_fill_visit() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            5, 5, 5, 5, 5,
+            5, 8, 8, 5, 8,
+            5, 8, 5, 5, 8,
+            5, 5, 5, 5, 8,
+        ];
+        let input: Image = Image::try_create(5, 4, pixels).expect("image");
+        let mut output = Image::zero(5, 4);
+        let color: u8 = input.get(4, 1).unwrap_or(255);
+
+        // Act
+        output.flood_fill_visit(&input, 4, 1, color);
+
+        // Assert
+        let expected_pixels: Vec<u8> = vec![
+            0, 0, 0, 0, 0,
+            0, 0, 0, 0, 1,
+            0, 0, 0, 0, 1,
+            0, 0, 0, 0, 1,
         ];
         let expected = Image::create_raw(5, 4, expected_pixels);
         assert_eq!(output, expected);
