c393979c46c9f5a6a46dff0d8da7d6d92ca06821
Simon Strandgaard <neoneye@gmail.com>
2022-12-14 13:27:21 +0100

    find_object_masks added

--- rust_project/loda-rust-cli/src/arc/image_segment.rs
+++ rust_project/loda-rust-cli/src/arc/image_segment.rs
@@ -6,6 +6,9 @@ pub trait ImageSegment {
 
     /// Build a mask of connected pixels that has the same color
     fn flood_fill_visit(&mut self, image: &Image, x: i32, y: i32, color: u8);
+
+    /// Identify clusters of connected pixels
+    fn find_object_masks(&self) -> anyhow::Result<Vec<Image>>;
 }
 
 impl ImageSegment for Image {
@@ -45,12 +48,41 @@ impl ImageSegment for Image {
         self.flood_fill_visit(image, x, y-1, color);
         self.flood_fill_visit(image, x, y+1, color);
     }
+
+    fn find_object_masks(&self) -> anyhow::Result<Vec<Image>> {
+        let mut object_mask_vec = Vec::<Image>::new();
+        let mut accumulated_mask = Image::zero(self.width(), self.height());
+        for y in 0..(self.height() as i32) {
+            for x in 0..(self.width() as i32) {
+                let mask_value: u8 = accumulated_mask.get(x, y).unwrap_or(255);
+                if mask_value > 0 {
+                    continue;
+                }
+                let color: u8 = self.get(x, y).unwrap_or(255);
+                let mut object_mask = Image::zero(self.width(), self.height());
+                object_mask.flood_fill_visit(&self, x, y, color);
+
+                // copy the mask into the accumulated mask, so that the pixel doesn't get visited again
+                for yy in 0..(self.height() as i32) {
+                    for xx in 0..(self.width() as i32) {
+                        let mask_value: u8 = object_mask.get(xx, yy).unwrap_or(255);
+                        if mask_value > 0 {
+                            let _ = accumulated_mask.set(xx, yy, 1);
+                        }
+                    }
+                }
+                object_mask_vec.push(object_mask);
+            }
+        }
+        Ok(object_mask_vec)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::arc::ImageTryCreate;
+    use crate::arc::ImageStack;
 
     #[test]
     fn test_10000_flood_fill() {
@@ -208,6 +240,84 @@ mod tests {
             0, 0, 0, 0, 1,
         ];
         let expected = Image::create_raw(5, 4, expected_pixels);
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_30000_find_object_masks() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            5, 5, 5, 5, 5,
+            5, 8, 8, 5, 8,
+            5, 8, 5, 5, 8,
+            5, 5, 5, 5, 8,
+        ];
+        let input: Image = Image::try_create(5, 4, pixels).expect("image");
+
+        // Act
+        let mask_vec: Vec<Image> = input.find_object_masks().expect("image");
+
+        // Assert
+        assert_eq!(mask_vec.len(), 3);
+        let output: Image = Image::vstack(mask_vec).expect("image");
+        let expected_pixels: Vec<u8> = vec![
+            1, 1, 1, 1, 1,
+            1, 0, 0, 1, 0,
+            1, 0, 1, 1, 0,
+            1, 1, 1, 1, 0,
+
+            0, 0, 0, 0, 0,
+            0, 1, 1, 0, 0,
+            0, 1, 0, 0, 0,
+            0, 0, 0, 0, 0,
+
+            0, 0, 0, 0, 0,
+            0, 0, 0, 0, 1,
+            0, 0, 0, 0, 1,
+            0, 0, 0, 0, 1,
+        ];
+        let expected = Image::create_raw(5, 4*3, expected_pixels);
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_30001_find_object_masks() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            5, 5, 5, 5, 5,
+            5, 6, 6, 6, 5,
+            5, 6, 5, 6, 5,
+            5, 6, 6, 6, 5,
+            5, 5, 5, 5, 5,
+        ];
+        let input: Image = Image::try_create(5, 5, pixels).expect("image");
+
+        // Act
+        let mask_vec: Vec<Image> = input.find_object_masks().expect("image");
+
+        // Assert
+        assert_eq!(mask_vec.len(), 3);
+        let output: Image = Image::vstack(mask_vec).expect("image");
+        let expected_pixels: Vec<u8> = vec![
+            1, 1, 1, 1, 1,
+            1, 0, 0, 0, 1,
+            1, 0, 0, 0, 1,
+            1, 0, 0, 0, 1,
+            1, 1, 1, 1, 1,
+
+            0, 0, 0, 0, 0,
+            0, 1, 1, 1, 0,
+            0, 1, 0, 1, 0,
+            0, 1, 1, 1, 0,
+            0, 0, 0, 0, 0,
+            
+            0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0,
+            0, 0, 1, 0, 0,
+            0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0,
+        ];
+        let expected = Image::create_raw(5, 5*3, expected_pixels);
         assert_eq!(output, expected);
     }
 }
