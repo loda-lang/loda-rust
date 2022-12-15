06d2d4e056196fd93aac36f3ae381a10ee3b5637
Simon Strandgaard <neoneye@gmail.com>
2022-12-15 19:18:23 +0100

    ImageMask.bounding_box added

--- rust_project/loda-rust-cli/src/arc/image_mask.rs
+++ rust_project/loda-rust-cli/src/arc/image_mask.rs
@@ -15,6 +15,9 @@ pub trait ImageMask {
     
     /// Pick pixels from two images. When the mask is 0 then pick `image_a`. When the mask is [1..255] then pick from `image_b`.
     fn select_from_images(&self, image_a: &Image, image_b: &Image) -> anyhow::Result<Image>;
+
+    /// The smallest box that can contain the mask.
+    fn bounding_box(&self) -> Option<(u8,u8,u8,u8)>;
 }
 
 impl ImageMask for Image {
@@ -125,6 +128,65 @@ impl ImageMask for Image {
             }
         }
         return Ok(result_image);
+    }
+
+    fn bounding_box(&self) -> Option<(u8,u8,u8,u8)> {
+        if self.is_empty() {
+            return None;
+        }
+
+        let x_max: i32 = (self.width() as i32) - 1;
+        let y_max: i32 = (self.height() as i32) - 1;
+        let mut found_x0: i32 = x_max;
+        let mut found_x1: i32 = 0;
+        let mut found_y0: i32 = y_max;
+        let mut found_y1: i32 = 0;
+        for y in 0..=y_max {
+            for x in 0..=x_max {
+                let pixel_value: u8 = self.get(x, y).unwrap_or(255);
+                if pixel_value == 0 {
+                    continue;
+                }
+
+                // grow the bounding box
+                found_x0 = i32::min(found_x0, x);
+                found_x1 = i32::max(found_x1, x);
+                found_y0 = i32::min(found_y0, y);
+                found_y1 = i32::max(found_y1, y);
+            }
+        }
+
+        if found_x0 > found_x1 || found_y0 > found_y1 {
+            return None;
+        }
+
+        // X position
+        if found_x0 < 0 || found_x0 > (u8::MAX as i32) {
+            return None;
+        }
+        let new_x = found_x0 as u8;
+
+        // Y position
+        if found_y0 < 0 || found_y0 > (u8::MAX as i32) {
+            return None;
+        }
+        let new_y = found_y0 as u8;
+
+        // Width of the object
+        let new_width_i32: i32 = found_x1 - found_x0 + 1;
+        if new_width_i32 < 1 || new_width_i32 > (u8::MAX as i32) {
+            return None;
+        }
+        let new_width: u8 = new_width_i32 as u8;
+
+        // Height of the object
+        let new_height_i32: i32 = found_y1 - found_y0 + 1;
+        if new_height_i32 < 1 || new_height_i32 > (u8::MAX as i32) {
+            return None;
+        }
+        let new_height: u8 = new_height_i32 as u8;
+
+        return Some((new_x, new_y, new_width, new_height));
     }
 }
 
@@ -283,5 +345,75 @@ mod tests {
         ];
         let expected: Image = Image::try_create(4, 4, expected_pixels).expect("image");
         assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_60000_bounding_box() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            0, 0, 1,
+            0, 0, 0,
+            0, 0, 0,
+            0, 0, 0,
+            1, 0, 0,
+        ];
+        let input: Image = Image::try_create(3, 5, pixels).expect("image");
+
+        // Act
+        let actual: (u8,u8,u8,u8) = input.bounding_box().expect("bounding box");
+
+        // Assert
+        assert_eq!(actual, (0, 0, 3, 5));
+    }
+
+    #[test]
+    fn test_60001_bounding_box() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            0, 0, 0,
+            0, 0, 1,
+            0, 1, 1,
+            0, 1, 0,
+            0, 0, 0,
+        ];
+        let input: Image = Image::try_create(3, 5, pixels).expect("image");
+
+        // Act
+        let actual: (u8,u8,u8,u8) = input.bounding_box().expect("bounding box");
+
+        // Assert
+        assert_eq!(actual, (1, 1, 2, 3));
+    }
+
+    #[test]
+    fn test_60002_bounding_box() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            0, 0, 0,
+            0, 0, 1,
+        ];
+        let input: Image = Image::try_create(3, 2, pixels).expect("image");
+
+        // Act
+        let actual: (u8,u8,u8,u8) = input.bounding_box().expect("bounding box");
+
+        // Assert
+        assert_eq!(actual, (2, 1, 1, 1));
+    }
+
+    #[test]
+    fn test_60003_bounding_box() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            0, 0,
+            0, 0,
+        ];
+        let input: Image = Image::try_create(2, 2, pixels).expect("image");
+
+        // Act
+        let actual: Option<(u8,u8,u8,u8)> = input.bounding_box();
+
+        // Assert
+        assert_eq!(actual, None);
     }
 }
