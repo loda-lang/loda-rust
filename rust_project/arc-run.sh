b7cbe541dfa0b8b80265c128d9ba0482e5eb51cb
Simon Strandgaard <neoneye@gmail.com>
2022-12-31 19:29:51 +0100

    run the docker image

--- rust_project/arc-run.sh
+++ rust_project/arc-run.sh
@@ -1,2 +1,3 @@
-docker run neoneye/loda-rust-cli:latest
-#docker run --mount type=bind,source="$(pwd)"/secret_data,target=/data docker-loda-rust-cli-image
+#docker run neoneye/loda-rust-cli:latest
+docker run --mount type=bind,source="$(pwd)"/secret_data,target=/data neoneye/loda-rust-cli:latest
+#docker run --mount type=bind,source="$(pwd)"/secret_data,target=/data -it neoneye/loda-rust-cli:latest /bin/bash
