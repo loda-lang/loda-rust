5e07b11b6e05c517d7442657dc09056ce78d69e6
Simon Strandgaard <neoneye@gmail.com>
2023-01-03 13:14:52 +0100

    removed dead code

--- rust_project/arc-run.sh
+++ rust_project/arc-run.sh
@@ -1,3 +1,1 @@
-#docker run neoneye/loda-rust-cli:latest
 docker run --mount type=bind,source="$(pwd)"/secret_data,target=/data neoneye/loda-rust-cli:latest
-#docker run --mount type=bind,source="$(pwd)"/secret_data,target=/data -it neoneye/loda-rust-cli:latest /bin/bash
