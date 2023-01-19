0b7bb0fc1c4a73d9e6de703b6d44dfa4ca5e0dd1
Simon Strandgaard <neoneye@gmail.com>
2023-01-19 21:21:02 +0100

    ImageNeighbour can now traverse diagonals

--- rust_project/loda-rust-cli/src/arc/image_neighbour.rs
+++ rust_project/loda-rust-cli/src/arc/image_neighbour.rs
@@ -7,6 +7,124 @@ pub enum ImageNeighbourDirection {
     Down,
     Left,
     Right,
+    UpLeft,
+    UpRight,
+    DownLeft,
+    DownRight,
+}
+
+impl ImageNeighbourDirection {
+    fn traversal_positions(&self, width: u8, height: u8) -> Vec<Vec<(i32,i32)>> {
+        let width: i32 = width as i32;
+        let height: i32 = height as i32;
+
+        let mut outer_positions = Vec::<Vec::<(i32,i32)>>::new();
+        match self {
+            ImageNeighbourDirection::Left => {
+                for y in 0..height {
+                    let mut row = Vec::<(i32,i32)>::new();
+                    for x in 0..width {
+                        row.push((x, y));
+                    }
+                    outer_positions.push(row);
+                }
+            },
+            ImageNeighbourDirection::Right => {
+                let x1: i32 = width - 1;
+                for y in 0..height {
+                    let mut row = Vec::<(i32,i32)>::new();
+                    for x in 0..width {
+                        row.push((x1 - x, y));
+                    }
+                    outer_positions.push(row);
+                }
+            },
+            ImageNeighbourDirection::Up => {
+                for x in 0..width {
+                    let mut column = Vec::<(i32,i32)>::new();
+                    for y in 0..height {
+                        column.push((x, y));
+                    }
+                    outer_positions.push(column);
+                }
+            },
+            ImageNeighbourDirection::Down => {
+                let y1: i32 = height - 1;
+                for x in 0..width {
+                    let mut column = Vec::<(i32,i32)>::new();
+                    for y in 0..height {
+                        column.push((x, y1 - y));
+                    }
+                    outer_positions.push(column);
+                }
+            },
+            ImageNeighbourDirection::UpLeft => {
+                let xrange: i32 = i32::max(width, height);
+                for x in -xrange..xrange {
+                    let mut diagonal_positions = Vec::<(i32,i32)>::new();
+                    for y in 0..height {
+                        let set_x: i32 = x + y;
+                        if set_x < 0 || set_x >= width {
+                            continue;
+                        }
+                        diagonal_positions.push((set_x, y));
+                    }
+                    if !diagonal_positions.is_empty() {
+                        outer_positions.push(diagonal_positions);
+                    }
+                }
+            },
+            ImageNeighbourDirection::UpRight => {
+                let xrange: i32 = i32::max(width, height);
+                for x in -xrange..xrange {
+                    let mut diagonal_positions = Vec::<(i32,i32)>::new();
+                    for y in 0..height {
+                        let set_x: i32 = width - 1 - x - y;
+                        if set_x < 0 || set_x >= width {
+                            continue;
+                        }
+                        diagonal_positions.push((set_x, y));
+                    }
+                    if !diagonal_positions.is_empty() {
+                        outer_positions.push(diagonal_positions);
+                    }
+                }
+            },
+            ImageNeighbourDirection::DownLeft => {
+                let xrange: i32 = i32::max(width, height);
+                for x in -xrange..xrange {
+                    let mut diagonal_positions = Vec::<(i32,i32)>::new();
+                    for y in 0..height {
+                        let set_x: i32 = x + y;
+                        if set_x < 0 || set_x >= width {
+                            continue;
+                        }
+                        diagonal_positions.push((set_x, height - 1 - y));
+                    }
+                    if !diagonal_positions.is_empty() {
+                        outer_positions.push(diagonal_positions);
+                    }
+                }
+            },
+            ImageNeighbourDirection::DownRight => {
+                let xrange: i32 = i32::max(width, height);
+                for x in -xrange..xrange {
+                    let mut diagonal_positions = Vec::<(i32,i32)>::new();
+                    for y in 0..height {
+                        let set_x: i32 = height - 1 - x - y;
+                        if set_x < 0 || set_x >= width {
+                            continue;
+                        }
+                        diagonal_positions.push((set_x, height - 1 - y));
+                    }
+                    if !diagonal_positions.is_empty() {
+                        outer_positions.push(diagonal_positions);
+                    }
+                }
+            }
+        }
+        outer_positions
+    }
 }
 
 pub trait ImageNeighbour {
@@ -23,47 +141,7 @@ impl ImageNeighbour for Image {
         }
 
         // Plan the traversal of pixels
-        let mut outer_positions = Vec::<Vec::<(i32,i32)>>::new();
-        match direction {
-            ImageNeighbourDirection::Left => {
-                for y in 0..(self.height() as i32) {
-                    let mut row = Vec::<(i32,i32)>::new();
-                    for x in 0..(self.width() as i32) {
-                        row.push((x, y));
-                    }
-                    outer_positions.push(row);
-                }
-            },
-            ImageNeighbourDirection::Right => {
-                let x1: i32 = (self.width() as i32) - 1;
-                for y in 0..(self.height() as i32) {
-                    let mut row = Vec::<(i32,i32)>::new();
-                    for x in 0..(self.width() as i32) {
-                        row.push((x1 - x, y));
-                    }
-                    outer_positions.push(row);
-                }
-            },
-            ImageNeighbourDirection::Up => {
-                for x in 0..(self.width() as i32) {
-                    let mut column = Vec::<(i32,i32)>::new();
-                    for y in 0..(self.height() as i32) {
-                        column.push((x, y));
-                    }
-                    outer_positions.push(column);
-                }
-            },
-            ImageNeighbourDirection::Down => {
-                let y1: i32 = (self.height() as i32) - 1;
-                for x in 0..(self.width() as i32) {
-                    let mut column = Vec::<(i32,i32)>::new();
-                    for y in 0..(self.height() as i32) {
-                        column.push((x, y1 - y));
-                    }
-                    outer_positions.push(column);
-                }
-            }
-        }
+        let outer_positions: Vec<Vec<(i32,i32)>> = direction.traversal_positions(self.width(), self.height());
     
         let mut result_image = Image::color(self.width(), self.height(), color_when_there_is_no_neighbour);
         // Perform traversal of the pixels
@@ -96,7 +174,305 @@ mod tests {
     use crate::arc::ImageMask;
 
     #[test]
-    fn test_10000_color_of_neighbour_left() {
+    fn test_10000_traversal_positions_left() {
+        // Arrange
+        let direction = ImageNeighbourDirection::Left;
+
+        // Act
+        let positions: Vec<Vec<(i32, i32)>> = direction.traversal_positions(3, 2);
+
+        // Assert
+        let expected: Vec<Vec<(i32, i32)>> = vec![
+            vec![(0, 0), (1, 0), (2, 0)],
+            vec![(0, 1), (1, 1), (2, 1)],
+        ];
+        assert_eq!(positions, expected);
+    }
+
+    #[test]
+    fn test_10001_traversal_positions_right() {
+        // Arrange
+        let direction = ImageNeighbourDirection::Right;
+
+        // Act
+        let positions: Vec<Vec<(i32, i32)>> = direction.traversal_positions(3, 2);
+
+        // Assert
+        let expected: Vec<Vec<(i32, i32)>> = vec![
+            vec![(2, 0), (1, 0), (0, 0)],
+            vec![(2, 1), (1, 1), (0, 1)],
+        ];
+        assert_eq!(positions, expected);
+    }
+
+    #[test]
+    fn test_10002_traversal_positions_up() {
+        // Arrange
+        let direction = ImageNeighbourDirection::Up;
+
+        // Act
+        let positions: Vec<Vec<(i32, i32)>> = direction.traversal_positions(3, 2);
+
+        // Assert
+        let expected: Vec<Vec<(i32, i32)>> = vec![
+            vec![(0, 0), (0, 1)],
+            vec![(1, 0), (1, 1)],
+            vec![(2, 0), (2, 1)],
+        ];
+        assert_eq!(positions, expected);
+    }
+
+    #[test]
+    fn test_10003_traversal_positions_down() {
+        // Arrange
+        let direction = ImageNeighbourDirection::Down;
+
+        // Act
+        let positions: Vec<Vec<(i32, i32)>> = direction.traversal_positions(3, 2);
+
+        // Assert
+        let expected: Vec<Vec<(i32, i32)>> = vec![
+            vec![(0, 1), (0, 0)],
+            vec![(1, 1), (1, 0)],
+            vec![(2, 1), (2, 0)],
+        ];
+        assert_eq!(positions, expected);
+    }
+
+    #[test]
+    fn test_10004_traversal_positions_upleft_4x3() {
+        // Arrange
+        let direction = ImageNeighbourDirection::UpLeft;
+
+        // Act
+        let positions: Vec<Vec<(i32, i32)>> = direction.traversal_positions(4, 3);
+
+        // Assert
+        let expected: Vec<Vec<(i32, i32)>> = vec![
+            vec![(0, 2)],
+            vec![(0, 1), (1, 2)],
+            vec![(0, 0), (1, 1), (2, 2)],
+            vec![(1, 0), (2, 1), (3, 2)],
+            vec![(2, 0), (3, 1)],
+            vec![(3, 0)],
+        ];
+        assert_eq!(positions, expected);
+    }
+
+    #[test]
+    fn test_10004_traversal_positions_upleft_4x2() {
+        // Arrange
+        let direction = ImageNeighbourDirection::UpLeft;
+
+        // Act
+        let positions: Vec<Vec<(i32, i32)>> = direction.traversal_positions(4, 2);
+
+        // Assert
+        let expected: Vec<Vec<(i32, i32)>> = vec![
+            vec![(0, 1)],
+            vec![(0, 0), (1, 1)],
+            vec![(1, 0), (2, 1)],
+            vec![(2, 0), (3, 1)],
+            vec![(3, 0)],
+        ];
+        assert_eq!(positions, expected);
+    }
+
+    #[test]
+    fn test_10004_traversal_positions_upleft_2x4() {
+        // Arrange
+        let direction = ImageNeighbourDirection::UpLeft;
+
+        // Act
+        let positions: Vec<Vec<(i32, i32)>> = direction.traversal_positions(2, 4);
+
+        // Assert
+        let expected: Vec<Vec<(i32, i32)>> = vec![
+            vec![(0, 3)],
+            vec![(0, 2), (1, 3)],
+            vec![(0, 1), (1, 2)],
+            vec![(0, 0), (1, 1)],
+            vec![(1, 0)],
+        ];
+        assert_eq!(positions, expected);
+    }
+
+    #[test]
+    fn test_10005_traversal_positions_upright_4x3() {
+        // Arrange
+        let direction = ImageNeighbourDirection::UpRight;
+
+        // Act
+        let positions: Vec<Vec<(i32, i32)>> = direction.traversal_positions(4, 3);
+
+        // Assert
+        let expected: Vec<Vec<(i32, i32)>> = vec![
+            vec![(3, 2)],
+            vec![(3, 1), (2, 2)],
+            vec![(3, 0), (2, 1), (1, 2)],
+            vec![(2, 0), (1, 1), (0, 2)],
+            vec![(1, 0), (0, 1)],
+            vec![(0, 0)],
+        ];
+        assert_eq!(positions, expected);
+    }
+
+    #[test]
+    fn test_10005_traversal_positions_upright_4x2() {
+        // Arrange
+        let direction = ImageNeighbourDirection::UpRight;
+
+        // Act
+        let positions: Vec<Vec<(i32, i32)>> = direction.traversal_positions(4, 2);
+
+        // Assert
+        let expected: Vec<Vec<(i32, i32)>> = vec![
+            vec![(3, 1)],
+            vec![(3, 0), (2, 1)],
+            vec![(2, 0), (1, 1)],
+            vec![(1, 0), (0, 1)],
+            vec![(0, 0)],
+        ];
+        assert_eq!(positions, expected);
+    }
+
+    #[test]
+    fn test_10005_traversal_positions_upright_2x4() {
+        // Arrange
+        let direction = ImageNeighbourDirection::UpRight;
+
+        // Act
+        let positions: Vec<Vec<(i32, i32)>> = direction.traversal_positions(2, 4);
+
+        // Assert
+        let expected: Vec<Vec<(i32, i32)>> = vec![
+            vec![(1, 3)],
+            vec![(1, 2), (0, 3)],
+            vec![(1, 1), (0, 2)],
+            vec![(1, 0), (0, 1)],
+            vec![(0, 0)],
+        ];
+        assert_eq!(positions, expected);
+    }
+
+    #[test]
+    fn test_10006_traversal_positions_downleft_4x3() {
+        // Arrange
+        let direction = ImageNeighbourDirection::DownLeft;
+
+        // Act
+        let positions: Vec<Vec<(i32, i32)>> = direction.traversal_positions(4, 3);
+
+        // Assert
+        let expected: Vec<Vec<(i32, i32)>> = vec![
+            vec![(0, 0)],
+            vec![(0, 1), (1, 0)],
+            vec![(0, 2), (1, 1), (2, 0)],
+            vec![(1, 2), (2, 1), (3, 0)],
+            vec![(2, 2), (3, 1)],
+            vec![(3, 2)],
+        ];
+        assert_eq!(positions, expected);
+    }
+
+    #[test]
+    fn test_10006_traversal_positions_downleft_4x2() {
+        // Arrange
+        let direction = ImageNeighbourDirection::DownLeft;
+
+        // Act
+        let positions: Vec<Vec<(i32, i32)>> = direction.traversal_positions(4, 2);
+
+        // Assert
+        let expected: Vec<Vec<(i32, i32)>> = vec![
+            vec![(0, 0)],
+            vec![(0, 1), (1, 0)],
+            vec![(1, 1), (2, 0)],
+            vec![(2, 1), (3, 0)],
+            vec![(3, 1)],
+        ];
+        assert_eq!(positions, expected);
+    }
+
+    #[test]
+    fn test_10006_traversal_positions_downleft_2x4() {
+        // Arrange
+        let direction = ImageNeighbourDirection::DownLeft;
+
+        // Act
+        let positions: Vec<Vec<(i32, i32)>> = direction.traversal_positions(2, 4);
+
+        // Assert
+        let expected: Vec<Vec<(i32, i32)>> = vec![
+            vec![(0, 0)],
+            vec![(0, 1), (1, 0)],
+            vec![(0, 2), (1, 1)],
+            vec![(0, 3), (1, 2)],
+            vec![(1, 3)],
+        ];
+        assert_eq!(positions, expected);
+    }
+
+    #[test]
+    fn test_10007_traversal_positions_downright_4x3() {
+        // Arrange
+        let direction = ImageNeighbourDirection::DownRight;
+
+        // Act
+        let positions: Vec<Vec<(i32, i32)>> = direction.traversal_positions(4, 3);
+
+        // Assert
+        let expected: Vec<Vec<(i32, i32)>> = vec![
+            vec![(3, 0)],
+            vec![(3, 1), (2, 0)], 
+            vec![(3, 2), (2, 1), (1, 0)], 
+            vec![(2, 2), (1, 1), (0, 0)], 
+            vec![(1, 2), (0, 1)], 
+            vec![(0, 2)]
+        ];
+        assert_eq!(positions, expected);
+    }
+
+    #[test]
+    fn test_10007_traversal_positions_downright_4x2() {
+        // Arrange
+        let direction = ImageNeighbourDirection::DownRight;
+
+        // Act
+        let positions: Vec<Vec<(i32, i32)>> = direction.traversal_positions(4, 2);
+
+        // Assert
+        let expected: Vec<Vec<(i32, i32)>> = vec![
+            vec![(3, 0)],
+            vec![(3, 1), (2, 0)], 
+            vec![(2, 1), (1, 0)], 
+            vec![(1, 1), (0, 0)], 
+            vec![(0, 1)]
+        ];
+        assert_eq!(positions, expected);
+    }
+
+    #[test]
+    fn test_10007_traversal_positions_downright_2x4() {
+        // Arrange
+        let direction = ImageNeighbourDirection::DownRight;
+
+        // Act
+        let positions: Vec<Vec<(i32, i32)>> = direction.traversal_positions(2, 4);
+
+        // Assert
+        let expected: Vec<Vec<(i32, i32)>> = vec![
+            vec![(1, 0)],
+            vec![(1, 1), (0, 0)], 
+            vec![(1, 2), (0, 1)], 
+            vec![(1, 3), (0, 2)], 
+            vec![(0, 3)]
+        ];
+        assert_eq!(positions, expected);
+    }
+
+    #[test]
+    fn test_20000_color_of_neighbour_left() {
         // Arrange
         let pixels: Vec<u8> = vec![
             0, 0, 9,
@@ -122,7 +498,7 @@ mod tests {
     }
 
     #[test]
-    fn test_10001_color_of_neighbour_right() {
+    fn test_20001_color_of_neighbour_right() {
         // Arrange
         let pixels: Vec<u8> = vec![
             0, 0, 9,
@@ -148,7 +524,7 @@ mod tests {
     }
 
     #[test]
-    fn test_10002_color_of_neighbour_up() {
+    fn test_20002_color_of_neighbour_up() {
         // Arrange
         let pixels: Vec<u8> = vec![
             0, 0, 9, 0,
@@ -172,7 +548,7 @@ mod tests {
     }
 
     #[test]
-    fn test_10003_color_of_neighbour_down() {
+    fn test_20003_color_of_neighbour_down() {
         // Arrange
         let pixels: Vec<u8> = vec![
             0, 0, 9, 0,
@@ -192,6 +568,110 @@ mod tests {
             3, 3, 3, 3,
         ];
         let expected = Image::create_raw(4, 3, expected_pixels);
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_20004_color_of_neighbour_upleft() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            0, 0, 9, 0,
+            0, 9, 0, 0,
+            9, 0, 0, 0,
+            0, 0, 0, 0,
+        ];
+        let input: Image = Image::try_create(4, 4, pixels).expect("image");
+        let ignore_mask = input.to_mask_where_color_is(0);
+
+        // Act
+        let output: Image = input.color_of_neighbour(&ignore_mask, ImageNeighbourDirection::UpLeft, 3).expect("image");
+
+        // Assert
+        let expected_pixels: Vec<u8> = vec![
+            3, 3, 3, 3,
+            3, 3, 3, 9,
+            3, 3, 9, 3,
+            3, 9, 3, 9,
+        ];
+        let expected = Image::create_raw(4, 4, expected_pixels);
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_20005_color_of_neighbour_upright() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            0, 9, 0, 0,
+            0, 0, 9, 0,
+            0, 0, 0, 9,
+            0, 0, 0, 0,
+        ];
+        let input: Image = Image::try_create(4, 4, pixels).expect("image");
+        let ignore_mask = input.to_mask_where_color_is(0);
+
+        // Act
+        let output: Image = input.color_of_neighbour(&ignore_mask, ImageNeighbourDirection::UpRight, 3).expect("image");
+
+        // Assert
+        let expected_pixels: Vec<u8> = vec![
+            3, 3, 3, 3,
+            9, 3, 3, 3,
+            3, 9, 3, 3,
+            9, 3, 9, 3,
+        ];
+        let expected = Image::create_raw(4, 4, expected_pixels);
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_20006_color_of_neighbour_downleft() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            0, 0, 0, 0,
+            9, 0, 0, 0,
+            0, 9, 0, 0,
+            0, 0, 9, 0,
+        ];
+        let input: Image = Image::try_create(4, 4, pixels).expect("image");
+        let ignore_mask = input.to_mask_where_color_is(0);
+
+        // Act
+        let output: Image = input.color_of_neighbour(&ignore_mask, ImageNeighbourDirection::DownLeft, 3).expect("image");
+
+        // Assert
+        let expected_pixels: Vec<u8> = vec![
+            3, 9, 3, 9,
+            3, 3, 9, 3,
+            3, 3, 3, 9,
+            3, 3, 3, 3,
+        ];
+        let expected = Image::create_raw(4, 4, expected_pixels);
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_20007_color_of_neighbour_downright() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            0, 0, 0, 0,
+            0, 0, 0, 9,
+            0, 0, 9, 0,
+            0, 9, 0, 0,
+        ];
+        let input: Image = Image::try_create(4, 4, pixels).expect("image");
+        let ignore_mask = input.to_mask_where_color_is(0);
+
+        // Act
+        let output: Image = input.color_of_neighbour(&ignore_mask, ImageNeighbourDirection::DownRight, 3).expect("image");
+
+        // Assert
+        let expected_pixels: Vec<u8> = vec![
+            9, 3, 9, 3,
+            3, 9, 3, 3,
+            9, 3, 3, 3,
+            3, 3, 3, 3,
+        ];
+        let expected = Image::create_raw(4, 4, expected_pixels);
         assert_eq!(output, expected);
     }
 }
