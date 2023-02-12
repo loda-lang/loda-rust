0e34a7a78384fbf6fb3d094c2275c3e6945789a3
Simon Strandgaard <neoneye@gmail.com>
2023-02-12 22:12:44 +0100

    Solution to ARC:a68b268e

--- rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
+++ rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
@@ -1996,4 +1996,109 @@ mod tests {
         assert_eq!(result.count_train_correct(), 3);
         assert_eq!(result.count_test_correct(), 1);
     }
+
+    #[test]
+    fn test_350000_puzzle_a68b268e() {
+        let solution: SolutionSimple = |data| {
+            let input: Image = data.image;
+            let histogram: Histogram = input.histogram_all();
+            let background_color: u8 = histogram.most_popular_color().expect("color");
+
+            let top: Image = input.top_rows(4).expect("image");
+            let top_left: Image = top.left_columns(4).expect("image");
+            let top_right: Image = top.right_columns(4).expect("image");
+            let bottom: Image = input.bottom_rows(4).expect("image");
+            let bottom_left: Image = bottom.left_columns(4).expect("image");
+            let bottom_right: Image = bottom.right_columns(4).expect("image");
+
+            let mut output: Image = bottom_right;
+            output = output.overlay_with_mask_color(&bottom_left, background_color).expect("image");
+            output = output.overlay_with_mask_color(&top_right, background_color).expect("image");
+            output = output.overlay_with_mask_color(&top_left, background_color).expect("image");
+            Ok(output)
+        };
+        let model: Model = Model::load_testdata("a68b268e").expect("model");
+        let instance = RunWithProgram::new(model, true).expect("RunWithProgram");
+        let result: RunWithProgramResult = instance.run_solution(solution).expect("result");
+        assert_eq!(result.messages(), "");
+        assert_eq!(result.count_train_correct(), 6);
+        assert_eq!(result.count_test_correct(), 1);
+    }
+
+    const PROGRAM_A68B268E: &'static str = "
+    mov $1,$0
+    f11 $1,101060 ; most popular color
+
+    ; W = compute (width-1) / 2
+    mov $2,$0
+    f11 $2,101000 ; Get width of image
+    sub $2,1
+    div $2,2
+
+    ; H = compute (height-1) / 2
+    mov $3,$0
+    f11 $3,101001 ; Get height of image
+    sub $3,1
+    div $3,2
+
+    ; top left corner of size WxH
+    mov $10,$0
+    mov $11,$3
+    f21 $10,101220 ; get N top rows
+    mov $11,$2
+    f21 $10,101222 ; get N left columns
+  
+    ; top right corner of size WxH
+    mov $15,$0
+    mov $16,$3
+    f21 $15,101220 ; get N top rows
+    mov $16,$2
+    f21 $15,101223 ; get N right columns
+  
+    ; bottom left corner of size WxH
+    mov $20,$0
+    mov $21,$3
+    f21 $20,101221 ; get N bottom rows
+    mov $21,$2
+    f21 $20,101222 ; get N left columns
+
+    ; bottom right corner of size WxH
+    mov $25,$0
+    mov $26,$3
+    f21 $25,101221 ; get N bottom rows
+    mov $26,$2
+    f21 $25,101223 ; get N right columns
+
+    ; zstack where the images are placed on top of each other
+    ; zindex 0 - the bottom
+    mov $30,$25 ; bottom right
+
+    ; zindex 1
+    mov $31,$20 ; bottom left
+    mov $32,$1 ; most popular color
+    f31 $30,101150 ; overlay image
+
+    ; zindex 2
+    mov $31,$15 ; top right
+    mov $32,$1 ; most popular color
+    f31 $30,101150 ; overlay image
+
+    ; zindex 3 - the top
+    mov $31,$10 ; top left
+    mov $32,$1 ; most popular color
+    f31 $30,101150 ; overlay image
+
+    mov $0,$30
+    ";
+
+    #[test]
+    fn test_350001_puzzle_a68b268e_loda() {
+        let model: Model = Model::load_testdata("a68b268e").expect("model");
+        let program = PROGRAM_A68B268E;
+        let instance = RunWithProgram::new(model, true).expect("RunWithProgram");
+        let result: RunWithProgramResult = instance.run_simple(program).expect("result");
+        assert_eq!(result.messages(), "");
+        assert_eq!(result.count_train_correct(), 6);
+        assert_eq!(result.count_test_correct(), 1);
+    }
 }
