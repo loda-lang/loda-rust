41585f532cf1521c0d2db5912f6e60bbffb4baf8
Simon Strandgaard <neoneye@gmail.com>
2023-02-25 23:47:09 +0100

    Solution 27f8ce4f is now faster than the previous nested loop that did the same.

--- rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
+++ rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
@@ -2244,4 +2244,45 @@ mod tests {
         assert_eq!(result.count_train_correct(), 6);
         assert_eq!(result.count_test_correct(), 1);
     }
+
+    const PROGRAM_27F8CE4F: &'static str = "
+    mov $1,$0
+    f11 $1,101060 ; most popular color
+
+    ; tile_width
+    mov $2,$0
+    f11 $2,101000 ; Get width of image
+
+    ; tile_height
+    mov $3,$0
+    f11 $3,101001 ; Get height of image
+
+    ; tile
+    mov $7,0 ; color
+    mov $6,$3 ; height
+    mov $5,$2 ; width
+    f31 $5,101010 ; Create new image with size (x, y) and filled with color z
+
+    ; mask
+    mov $10,$0 ; image
+    mov $11,$1 ; color
+    f21 $10,101251 ; Convert to a mask image by converting `color` to 0 and converting anything else to to 1.
+
+    mov $11,$0 ; tile0
+    mov $12,$5 ; tile1
+    f31 $10,102110 ; Create a big composition of tiles.
+
+    mov $0,$10
+    ";
+
+    #[test]
+    fn test_410000_puzzle_27f8ce4f_loda() {
+        let model: Model = Model::load_testdata("27f8ce4f").expect("model");
+        let program = PROGRAM_27F8CE4F;
+        let instance = RunWithProgram::new(model, true).expect("RunWithProgram");
+        let result: RunWithProgramResult = instance.run_simple(program).expect("result");
+        assert_eq!(result.messages(), "");
+        assert_eq!(result.count_train_correct(), 4);
+        assert_eq!(result.count_test_correct(), 1);
+    }
 }
