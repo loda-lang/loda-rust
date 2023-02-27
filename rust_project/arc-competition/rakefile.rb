bce628308439a07a1a1f1b1d2a5749eb19357692
Simon Strandgaard <neoneye@gmail.com>
2023-02-27 21:33:09 +0100

    "docker run" now provides a "linux/amd64" parameter.

--- rust_project/arc-competition/rakefile.rb
+++ rust_project/arc-competition/rakefile.rb
@@ -31,14 +31,14 @@ desc "Runs the LODA-RUST process in an isolated container, in an enviroment simi
 task "run" do
     pwd = Dir.pwd
     image = DOCKER_IMAGE
-    system("docker run --mount type=bind,source=#{pwd}/secret_data,target=/data #{image}")
+    system("docker run --platform linux/amd64 --mount type=bind,source=#{pwd}/secret_data,target=/data #{image}")
 end
 
 desc "Runs an interactive bash shell inside the docker image, so it's possible to troubleshoot."
 task "shell" do
     pwd = Dir.pwd
     image = DOCKER_IMAGE
-    system("docker run -it --mount type=bind,source=#{pwd}/secret_data,target=/data #{image} /bin/bash")
+    system("docker run --platform linux/amd64 -it --mount type=bind,source=#{pwd}/secret_data,target=/data #{image} /bin/bash")
 end
 
 desc "Export docker image to tar file"
