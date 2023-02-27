694168b63e57e31786229981edb2657be48dd091
Simon Strandgaard <neoneye@gmail.com>
2023-02-27 22:38:11 +0100

    Snapshot of what I submitted for ARCathon 2023 in iteration 3.

---
+++ rust_project/arc-competition/payload/loda-arc-challenge/programs/4258a5f9-1.asm
@@ -0,0 +1,43 @@
+; ARC:4258a5f9
+; Submitted by Simon Strandgaard
+; Program Type: advanced
+
+mov $40,0 ; outline color
+
+; process "train" vector
+mov $80,$97 ; set iteration counter = length of "train" vector
+mov $81,100 ; address of first training data train[0].input
+mov $82,101 ; address of first training data train[0].output
+lps $80
+  mov $0,$$81 ; load train[x].input image
+  mov $1,$$82 ; load train[x].output image
+
+  ; analyze the output images
+  f12 $1,101070 ; least popular colors
+  mov $40,$2 ; get the outline color
+
+  ; next iteration
+  add $81,10 ; jump to address of next training input image
+  add $82,10 ; jump to address of next training output image
+lpe
+
+; process "train"+"test" vectors
+mov $80,$99 ; set iteration counter = length of "train"+"test" vectors
+mov $81,100 ; address of vector[0].input
+mov $82,102 ; address of vector[0].computed_output
+lps $80
+  mov $0,$$81 ; load vector[x].input image
+
+  mov $5,$0
+  f11 $5,101060 ; most popular color
+
+  mov $1,$40 ; outline color
+  mov $2,$5 ; background color
+  f31 $0,101080 ; draw outline
+
+  mov $$82,$0 ; save vector[x].computed_output image
+
+  ; next iteration
+  add $81,10 ; jump to address of next input image
+  add $82,10 ; jump to address of next computed_output image
+lpe
---
+++ rust_project/arc-competition/payload/loda-arc-challenge/programs/5b6cbef5-1.asm
@@ -0,0 +1,27 @@
+; Submitted by Simon Strandgaard
+; Program Type: simple
+
+; tile_width
+mov $2,$0
+f11 $2,101000 ; Get width of image
+
+; tile_height
+mov $3,$0
+f11 $3,101001 ; Get height of image
+
+; tile
+mov $7,0 ; color
+mov $6,$3 ; height
+mov $5,$2 ; width
+f31 $5,101010 ; Create new image with size (x, y) and filled with color z
+
+; mask
+mov $10,$0 ; image
+mov $11,$1 ; color
+f21 $10,101251 ; Convert to a mask image by converting `color` to 0 and converting anything else to to 1.
+
+mov $11,$5 ; tile0
+mov $12,$0 ; tile1
+f31 $10,102110 ; Create a big composition of tiles.
+
+mov $0,$10
