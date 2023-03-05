cc27fa76eabb4854019a77079fdf903f825fb80a
Simon Strandgaard <neoneye@gmail.com>
2023-03-05 17:05:57 +0100

    Snapshot of what I submitted for ARCathon 2023 in iteration 5.

---
+++ rust_project/arc-competition/payload/loda-arc-challenge/programs/2dee498d-x.asm
@@ -0,0 +1,38 @@
+; Submitted by Simon Strandgaard
+; Program Type: advanced
+
+mov $80,$99
+mov $81,100
+mov $82,102
+lps $80
+  mov $0,$$81
+  mov $4,$0
+  mov $5,$0
+  f11 $4,101000 ; Get width of image
+  f11 $5,101001 ; Get height of image
+  div $4,3
+  div $5,$4
+  mov $1,$4
+  f21 $0,101220 ; get N top rows
+  mov $1,$4
+  f21 $0,101222 ; get N left columns
+  mov $$82,$0
+  add $81,10
+  add $82,10
+lpe
+
+; template: "d10ecb37-x.asm"
+; mutate: CallRecentProgram, no change
+; mutate: CallRecentProgram, no change
+; mutate: InsertLineWithHistogram, no change
+; mutate: CallRecentProgram, no change
+; mutate: CallRecentProgram, no change
+; mutate: SetSourceToDirect
+; mutate: CallRecentProgram, no change
+; mutate: CallRecentProgram, no change
+; mutate: CallRecentProgram, no change
+; mutate: CallRecentProgram, no change
+; mutate: InsertLineWithHistogram, no change
+; mutate: ReplaceLineWithHistogram
+; mutate: InsertLineWithHistogram, no change
+; mutate: ToggleEnabled
---
+++ rust_project/arc-competition/payload/loda-arc-challenge/programs/67a3c6ac-1.asm
@@ -0,0 +1,5 @@
+; ARC:67a3c6ac
+; Submitted by Simon Strandgaard
+; Program Type: simple
+
+f11 $0,101190 ; flip x
