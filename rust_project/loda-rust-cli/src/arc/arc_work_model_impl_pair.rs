658c344dd2510c603aa6606f2c2ea723f06a336a
Simon Strandgaard <neoneye@gmail.com>
2023-03-21 13:51:19 +0100

    analyze_object_why_is_the_output_present_once_in_input() added.

--- rust_project/loda-rust-cli/src/arc/arc_work_model_impl_pair.rs
+++ rust_project/loda-rust-cli/src/arc/arc_work_model_impl_pair.rs
@@ -1,4 +1,6 @@
-use super::arc_work_model;
+use crate::arc::{HtmlLog, ImageToHTML};
+
+use super::{arc_work_model, ImageSegment, Image, ImageMask, ImageCrop};
 use super::{ActionLabel, PropertyOutput};
 use super::{ImageFind, ImageSymmetry};
 
@@ -68,5 +70,95 @@ impl arc_work_model::Pair {
         if self.input.histogram == self.output.histogram {
             self.action_label_set.insert(ActionLabel::OutputImageHistogramEqualToInputImageHistogram);
         }
+
+        // _ = self.analyze_object_why_is_the_output_present_once_in_input();
+    }
+
+    fn analyze_object_why_is_the_output_present_once_in_input(&self) -> anyhow::Result<()> {
+        if !self.action_label_set.contains(&ActionLabel::OutputImageIsPresentExactlyOnceInsideInputImage) {
+            return Ok(());
+        }
+
+        let objects: Vec<Image> = self.input.image.find_objects(super::ImageSegmentAlgorithm::Neighbors)?;
+        let mut found_count: usize = 0;
+        let mut object_and_meta_vec: Vec<ObjectAndMeta> = vec!();
+        for (index, object) in objects.iter().enumerate() {
+            let (x, y, width, height) = match object.bounding_box() {
+                Some(value) => value,
+                None => continue
+            };
+            let cropped_object_image: Image = self.input.image.crop(x, y, width, height)?;
+
+            let object_and_meta = ObjectAndMeta {
+                index: index,
+                mask: object.clone(),
+                cropped_object_image: cropped_object_image.clone(),
+            };
+            object_and_meta_vec.push(object_and_meta);
+            if cropped_object_image == self.output.image {
+                found_count += 1;
+            }
+        }
+        if found_count != 1 {
+            return Ok(());
+        }
+        // println!("OutputImage is one of the objects in the input image");
+        HtmlLog::html(self.input.image.to_html());
+        HtmlLog::html(self.output.image.to_html());
+        HtmlLog::text("separator");
+
+        if object_and_meta_vec.len() == 1 {
+            println!("OutputImage is only object in the input image");
+        }
+
+        // Reset the sorting
+        object_and_meta_vec.sort_unstable_by_key(|obj| obj.index);
+
+        // Smallest objects first, biggest last
+        object_and_meta_vec.sort_unstable_by_key(|obj| obj.area());
+        for _ in 0..1 {
+            let object0: &ObjectAndMeta = match object_and_meta_vec.get(0) {
+                Some(obj) => obj,
+                None => break
+            };
+            let object1: &ObjectAndMeta = match object_and_meta_vec.get(1) {
+                Some(obj) => obj,
+                None => break
+            };
+            if object0.area() < object1.area() {
+                println!("OutputImage is object with the smallest area");
+            }            
+        }
+
+        // Biggest objects first, smallest last
+        object_and_meta_vec.reverse();
+        for _ in 0..1 {
+            let object0: &ObjectAndMeta = match object_and_meta_vec.get(0) {
+                Some(obj) => obj,
+                None => break
+            };
+            let object1: &ObjectAndMeta = match object_and_meta_vec.get(1) {
+                Some(obj) => obj,
+                None => break
+            };
+            if object0.area() > object1.area() {
+                println!("OutputImage is object with the biggest area");
+            }            
+        }
+
+        Ok(())
+    }
+}
+
+struct ObjectAndMeta {
+    index: usize,
+    mask: Image,
+    cropped_object_image: Image,
+}
+
+impl ObjectAndMeta {
+    fn area(&self) -> u16 {
+        let image = &self.cropped_object_image;
+        (image.width() as u16) * (image.height() as u16)
     }
 }
