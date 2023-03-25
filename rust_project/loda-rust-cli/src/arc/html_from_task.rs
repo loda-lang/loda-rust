b72f6fc513271d78d9a55085323d5173de461376
Simon Strandgaard <neoneye@gmail.com>
2023-03-25 13:16:42 +0100

    Moved the "analysis" column: so it's now the "train" pairs, "analysis" column, the "test" pairs. Previously the "analysis" column was the right most column.

--- rust_project/loda-rust-cli/src/arc/html_from_task.rs
+++ rust_project/loda-rust-cli/src/arc/html_from_task.rs
@@ -3,9 +3,29 @@ use super::{InputLabelSet, ActionLabelSet, PropertyInput};
 use super::{HtmlLog, ImageToHTML};
 use std::collections::HashMap;
 
-pub struct HtmlFromTask;
+pub struct HtmlFromTask {
+    row_title: String,
+    row_input_image: String,
+    row_input_properties: String,
+    row_input_labels: String,
+    row_output_image: String,
+    row_action_colors: String,
+    row_action_labels: String,
+}
 
 impl HtmlFromTask {
+    fn new() -> Self {
+        Self {
+            row_title: "<tr><td></td>".to_string(),
+            row_input_image: "<tr><td>Input image</td>".to_string(),
+            row_input_properties: "<tr><td>Input properties</td>".to_string(),
+            row_input_labels: "<tr><td>Input labels</td>".to_string(),
+            row_output_image: "<tr><td>Output image</td>".to_string(),
+            row_action_colors: "<tr><td>Action colors</td>".to_string(),
+            row_action_labels: "<tr><td>Action labels</td>".to_string(),
+        }
+    }
+
     fn labelset_to_html(label_set: &ActionLabelSet) -> String {
         let mut label_vec: Vec<String> = label_set.iter().map(|label| format!("{:?}", label)).collect();
         if label_vec.is_empty() {
@@ -36,160 +56,153 @@ impl HtmlFromTask {
         format!("<ul>{}</ul>", list_vec.join(""))
     }
 
-    pub fn inspect(task: &arc_work_model::Task) -> anyhow::Result<()> {
-        let mut row_title: String = "<tr><td></td>".to_string();
-        let mut row_input_image: String = "<tr><td>Input image</td>".to_string();
-        let mut row_input_properties: String = "<tr><td>Input properties</td>".to_string();
-        let mut row_input_labels: String = "<tr><td>Input labels</td>".to_string();
-        let mut row_output_image: String = "<tr><td>Output image</td>".to_string();
-        let mut row_action_colors: String = "<tr><td>Action colors</td>".to_string();
-        let mut row_action_labels: String = "<tr><td>Action labels</td>".to_string();
-
-        // The current ordering of columns is terrible. train pairs, test pairs, analysis.
-        // It's counter intuitive that the analysis comes last.
-        // The way the computation takes place is train pairs, analysis, test pairs.
-        // TODO: Reorder the columns like this: train pairs, analysis, test pairs.
-
-        for pair in &task.pairs {
-            {
-                row_title += "<td>";
-                let title: &str = match pair.pair_type {
-                    arc_work_model::PairType::Train => "Train",
-                    arc_work_model::PairType::Test => "Test",
-                };
-                row_title += title;
-                row_title += "</td>";
-            }
-            {
-                row_input_image += "<td>";
-                row_input_image += &pair.input.image.to_html();
-                row_input_image += "</td>";
-            }
-            {
-                row_input_properties += "<td>";
-                row_input_properties += &Self::input_properties_to_html(&pair.input.input_properties);
-                row_input_properties += "</td>";
-            }
-            {
-                row_input_labels += "<td>";
-                row_input_labels += &Self::input_label_set_to_html(&pair.input.input_label_set);
-                row_input_labels += "</td>";
-            }
-            {
-                row_output_image += "<td>";
-                row_output_image += &pair.output.image.to_html();
-                row_output_image += "</td>";
-            }
-            {
-                row_action_colors += "<td>Removal<br>";
-                match pair.removal_histogram.color_image() {
-                    Ok(image) => {
-                        row_action_colors += &image.to_html();
-                    },
-                    Err(_) => {
-                        row_action_colors += "N/A";
-                    }
-                }
-                row_action_colors += "<br>Insert<br>";
-                match pair.insert_histogram.color_image() {
-                    Ok(image) => {
-                        row_action_colors += &image.to_html();
-                    },
-                    Err(_) => {
-                        row_action_colors += "N/A";
-                    }
-                }
-                row_action_colors += "</td>";
-            }
-            {
-                row_action_labels += "<td>";
-                row_action_labels += &Self::labelset_to_html(&pair.action_label_set);
-                row_action_labels += "</td>";
-            }
+    fn push_pair(&mut self, pair: &arc_work_model::Pair) -> anyhow::Result<()> {
+        {
+            self.row_title += "<td>";
+            let title: &str = match pair.pair_type {
+                arc_work_model::PairType::Train => "Train",
+                arc_work_model::PairType::Test => "Test",
+            };
+            self.row_title += title;
+            self.row_title += "</td>";
         }
+        {
+            self.row_input_image += "<td>";
+            self.row_input_image += &pair.input.image.to_html();
+            self.row_input_image += "</td>";
+        }
+        {
+            self.row_input_properties += "<td>";
+            self.row_input_properties += &Self::input_properties_to_html(&pair.input.input_properties);
+            self.row_input_properties += "</td>";
+        }
+        {
+            self.row_input_labels += "<td>";
+            self.row_input_labels += &Self::input_label_set_to_html(&pair.input.input_label_set);
+            self.row_input_labels += "</td>";
+        }
+        {
+            self.row_output_image += "<td>";
+            self.row_output_image += &pair.output.image.to_html();
+            self.row_output_image += "</td>";
+        }
+        {
+            self.row_action_colors += "<td>Removal<br>";
+            match pair.removal_histogram.color_image() {
+                Ok(image) => {
+                    self.row_action_colors += &image.to_html();
+                },
+                Err(_) => {
+                    self.row_action_colors += "N/A";
+                }
+            }
+            self.row_action_colors += "<br>Insert<br>";
+            match pair.insert_histogram.color_image() {
+                Ok(image) => {
+                    self.row_action_colors += &image.to_html();
+                },
+                Err(_) => {
+                    self.row_action_colors += "N/A";
+                }
+            }
+            self.row_action_colors += "</td>";
+        }
+        {
+            self.row_action_labels += "<td>";
+            self.row_action_labels += &Self::labelset_to_html(&pair.action_label_set);
+            self.row_action_labels += "</td>";
+        }
+        Ok(())
+    }
 
-        row_title += "<td>Analysis</td>";
+    fn push_column_analysis(&mut self, task: &arc_work_model::Task) -> anyhow::Result<()> {
+        self.row_title += "<td>Analysis</td>";
 
-        row_input_image += "<td>Union<br>";
+        self.row_input_image += "<td>Union<br>";
         match task.input_histogram_union.color_image() {
             Ok(image) => {
-                row_input_image += &image.to_html();
+                self.row_input_image += &image.to_html();
             },
             Err(_) => {
-                row_input_image += "N/A";
+                self.row_input_image += "N/A";
             }
         }
-        row_input_image += "<br><br>Intersection<br>";
+        self.row_input_image += "<br><br>Intersection<br>";
         match task.input_histogram_intersection.color_image() {
             Ok(image) => {
-                row_input_image += &image.to_html();
+                self.row_input_image += &image.to_html();
             },
             Err(_) => {
-                row_input_image += "N/A";
+                self.row_input_image += "N/A";
             }
         }
-        row_input_image += "</td>";
+        self.row_input_image += "</td>";
 
-        row_input_properties += "<td>";
-        row_input_properties += &Self::input_properties_to_html(&task.input_properties_intersection);
-        row_input_properties += "</td>";
+        self.row_input_properties += "<td>";
+        self.row_input_properties += &Self::input_properties_to_html(&task.input_properties_intersection);
+        self.row_input_properties += "</td>";
 
-        row_input_labels += "<td>";
-        row_input_labels += &Self::input_label_set_to_html(&task.input_label_set_intersection);
-        row_input_labels += "</td>";
+        self.row_input_labels += "<td>";
+        self.row_input_labels += &Self::input_label_set_to_html(&task.input_label_set_intersection);
+        self.row_input_labels += "</td>";
 
-        row_output_image += "<td>Union<br>";
+        self.row_output_image += "<td>Union<br>";
         match task.output_histogram_union.color_image() {
             Ok(image) => {
-                row_output_image += &image.to_html();
+                self.row_output_image += &image.to_html();
             },
             Err(_) => {
-                row_output_image += "N/A";
+                self.row_output_image += "N/A";
             }
         }
-        row_output_image += "<br><br>Intersection<br>";
+        self.row_output_image += "<br><br>Intersection<br>";
         match task.output_histogram_intersection.color_image() {
             Ok(image) => {
-                row_output_image += &image.to_html();
+                self.row_output_image += &image.to_html();
             },
             Err(_) => {
-                row_output_image += "N/A";
+                self.row_output_image += "N/A";
             }
         }
-        row_output_image += "</td>";
+        self.row_output_image += "</td>";
 
-        row_action_colors += "<td>Removal<br>";
+        self.row_action_colors += "<td>Removal<br>";
         match task.removal_histogram_intersection.color_image() {
             Ok(image) => {
-                row_action_colors += &image.to_html();
+                self.row_action_colors += &image.to_html();
             },
             Err(_) => {
-                row_action_colors += "N/A";
+                self.row_action_colors += "N/A";
             }
         }
-        row_action_colors += "<br>Insert<br>";
+        self.row_action_colors += "<br>Insert<br>";
         match task.insert_histogram_intersection.color_image() {
             Ok(image) => {
-                row_action_colors += &image.to_html();
+                self.row_action_colors += &image.to_html();
             },
             Err(_) => {
-                row_action_colors += "N/A";
+                self.row_action_colors += "N/A";
             }
         }
-        row_action_colors += "</td>";
+        self.row_action_colors += "</td>";
 
-        row_action_labels += "<td>";
-        row_action_labels += &Self::labelset_to_html(&task.action_label_set_intersection);
-        row_action_labels += "</td>";
+        self.row_action_labels += "<td>";
+        self.row_action_labels += &Self::labelset_to_html(&task.action_label_set_intersection);
+        self.row_action_labels += "</td>";
+        Ok(())
+    }
 
-        row_title += "</tr>";
-        row_input_image += "</tr>";
-        row_input_properties += "</tr>";
-        row_input_labels += "</tr>";
-        row_output_image += "</tr>";
-        row_action_colors += "</tr>";
-        row_action_labels += "</tr>";
+    fn end_of_row(&mut self) {
+        self.row_title += "</tr>";
+        self.row_input_image += "</tr>";
+        self.row_input_properties += "</tr>";
+        self.row_input_labels += "</tr>";
+        self.row_output_image += "</tr>";
+        self.row_action_colors += "</tr>";
+        self.row_action_labels += "</tr>";
+    }
 
+    fn to_html(&self, task: &arc_work_model::Task) -> String {
         let solution_status: &str;
         if task.occur_in_solutions_csv {
             solution_status = "solved";
@@ -203,14 +216,44 @@ impl HtmlFromTask {
             "<h2>{}</h2><p>Output size: {}</p><table>{}{}{}{}{}{}{}</table>",
             title, 
             task.estimated_output_size(),
-            row_title,
-            row_input_image, 
-            row_input_properties, 
-            row_input_labels, 
-            row_output_image, 
-            row_action_colors,
-            row_action_labels
+            self.row_title,
+            self.row_input_image, 
+            self.row_input_properties, 
+            self.row_input_labels, 
+            self.row_output_image, 
+            self.row_action_colors,
+            self.row_action_labels
         );
+        html
+    }
+
+    pub fn inspect(task: &arc_work_model::Task) -> anyhow::Result<()> {
+        let mut instance = Self::new();
+
+        // The current ordering of columns is terrible. train pairs, test pairs, analysis.
+        // It's counter intuitive that the analysis comes last.
+        // The way the computation takes place is train pairs, analysis, test pairs.
+        // TODO: Reorder the columns like this: train pairs, analysis, test pairs.
+
+        for pair in &task.pairs {
+            if pair.pair_type != arc_work_model::PairType::Train {
+                continue;
+            }
+            instance.push_pair(pair)?;
+        }
+
+        instance.push_column_analysis(task)?;
+
+        for pair in &task.pairs {
+            if pair.pair_type != arc_work_model::PairType::Test {
+                continue;
+            }
+            instance.push_pair(pair)?;
+        }
+
+        instance.end_of_row();
+
+        let html: String = instance.to_html(task);
         HtmlLog::html(html);
         Ok(())
     }
