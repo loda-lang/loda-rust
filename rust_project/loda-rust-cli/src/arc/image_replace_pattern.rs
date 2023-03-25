1a609f923eceaaefff76594cdfea436f86776b52
Simon Strandgaard <neoneye@gmail.com>
2023-03-25 22:33:40 +0100

    replace_pattern() added

---
+++ rust_project/loda-rust-cli/src/arc/image_replace_pattern.rs
@@ -0,0 +1,35 @@
+use super::{Image,ImageFind,ImageOverlay};
+use std::collections::HashMap;
+
+pub trait ImageReplacePattern {
+    /// Do substitutions from the dictionary
+    fn replace_pattern(&mut self, replacements: &HashMap::<Image, Image>, max_iterations: usize) -> anyhow::Result<()>;
+}
+
+impl ImageReplacePattern for Image {
+    fn replace_pattern(&mut self, replacements: &HashMap::<Image, Image>, max_iterations: usize) -> anyhow::Result<()> {
+        if self.is_empty() {
+            return Ok(());
+        }
+        if replacements.is_empty() {
+            return Ok(());
+        }
+        let mut result_image: Image = self.clone();
+
+        for _ in 0..max_iterations {
+            let mut stop = true;
+            for (key, value) in replacements {
+                let position = result_image.find_exact(key)?;
+                if let Some((x, y)) = position {
+                    result_image = result_image.overlay_with_position(value, x as i32, y as i32)?;
+                    stop = false;
+                }
+            }
+            if stop {
+                break;
+            }
+        }
+        self.set_image(result_image);
+        Ok(())
+    }
+}
