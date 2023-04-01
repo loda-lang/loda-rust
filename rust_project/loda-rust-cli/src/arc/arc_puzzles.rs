1a0084e317ea6a367ba33b5cd2cf722e1dabfd7d
Simon Strandgaard <neoneye@gmail.com>
2023-04-01 17:06:40 +0200

    Testing that OutputImageIsInputImageWithChangesLimitedToPixelsWithColor gets transferred to the program

--- rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
+++ rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
@@ -2190,6 +2190,28 @@ mod tests {
         assert_eq!(result, "3 1");
     }
 
+    const ADVANCED_PROGRAM_EA959FEB: &'static str = r#"
+    mov $80,$99
+    mov $81,100
+    mov $82,102
+    mov $83,105 ; address of vector[0].OutputImageIsInputImageWithChangesLimitedToPixelsWithColor
+    lps $80
+      mov $0,$$81
+      mov $1,$$83
+      f21 $0,102151 ; Repair damaged pixels and recreate big repeating patterns such as mosaics.
+      mov $$82,$0
+      add $81,100
+      add $82,100
+      add $83,100
+    lpe
+    "#;
+
+    #[test]
+    fn test_470005_puzzle_ea959feb_loda() {
+        let result: String = run_advanced("ea959feb", ADVANCED_PROGRAM_EA959FEB).expect("String");
+        assert_eq!(result, "3 1");
+    }
+
     #[test]
     fn test_520000_puzzle_49d1d64f() {
         let solution: SolutionSimple = |data| {
