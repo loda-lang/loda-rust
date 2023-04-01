bd8263b5551ca3432fda36300321ef3c4de1fd70
Simon Strandgaard <neoneye@gmail.com>
2023-04-01 21:06:38 +0200

    Solved b6afb2da

--- rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
+++ rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
@@ -2826,6 +2826,39 @@ mod tests {
         assert_eq!(result, "2 1");
     }
 
+    const PROGRAM_B6AFB2DA: &'static str = "
+    mov $80,$99
+    mov $81,100
+    mov $82,102
+    mov $83,105 ; address of vector[0].OutputImageIsInputImageWithChangesLimitedToPixelsWithColor
+    lps $80
+        mov $0,$$81
+
+        mov $4,$0
+        f11 $4,102140 ; Traverse all pixels in the 3x3 convolution and count how many have the same color as the center.
+
+        mov $5,$0
+        mov $6,$$83
+        f21 $5,101251 ; Convert to a mask image by converting `color` to 0 and converting anything else to to 1.
+
+        mov $8,42
+        mov $7,$4
+        mov $6,$5
+        f31 $6,102131 ; Pick pixels from color and image. When the mask is 0 then pick the `default_color`. When the mask is [1..255] then pick from the image.
+
+        mov $$82,$6
+        add $81,100
+        add $82,100
+        add $83,100
+    lpe
+    ";
+
+    #[test]
+    fn test_630001_puzzle_b6afb2da_loda() {
+        let result: String = run_advanced("b6afb2da", PROGRAM_B6AFB2DA).expect("String");
+        assert_eq!(result, "2 1");
+    }
+
     #[test]
     fn test_640000_puzzle_c444b776() {
         let solution: SolutionSimple = |data| {
