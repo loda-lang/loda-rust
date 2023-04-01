93aac9b2ca2d7812ebd80bb2e0b1b1fe0f08924b
Simon Strandgaard <neoneye@gmail.com>
2023-04-01 20:03:54 +0200

    Solved 50cb2852 in loda

--- rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
+++ rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
@@ -2715,6 +2715,31 @@ mod tests {
         assert_eq!(result, "3 1");
     }
 
+    const PROGRAM_50CB2852: &'static str = "
+    mov $1,$0
+    f11 $1,102140 ; Traverse all pixels in the 3x3 convolution and count how many have the same color as the center.
+    mov $2,8
+    f21 $1,101253 ; Convert to a mask image by converting `pixel_color >= threshold_color` to 1 and converting anything else to to 0.
+
+    mov $2,$0
+    mov $3,0
+    f21 $2,101251 ; Convert to a mask image by converting `color` to 0 and converting anything else to to 1.
+
+    f21 $1,101255 ; AND between two masks
+
+    mov $3,42
+    mov $2,$0
+    f31 $1,102131 ; Pick pixels from image and color. When the mask is 0 then pick from the image. When the mask is [1..255] then use the `default_color`.
+
+    mov $0,$1
+    ";
+
+    #[test]
+    fn test_590001_puzzle_50cb2852_loda() {
+        let result: String = run_simple("50cb2852", PROGRAM_50CB2852).expect("String");
+        assert_eq!(result, "3 1");
+    }
+
     #[test]
     fn test_600000_puzzle_c1d99e64() {
         let solution: SolutionSimple = |data| {
