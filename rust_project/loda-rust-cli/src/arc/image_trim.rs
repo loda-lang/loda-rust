225b17f87e326ed48f29bd36702c06b036f62d9d
Simon Strandgaard <neoneye@gmail.com>
2023-04-04 13:11:52 +0200

    shrink_bounding_box() added

--- rust_project/loda-rust-cli/src/arc/image_trim.rs
+++ rust_project/loda-rust-cli/src/arc/image_trim.rs
@@ -7,8 +7,15 @@ pub trait ImageTrim {
     /// Remove border with the specified color.
     fn trim_color(&self, color_to_be_trimmed: u8) -> anyhow::Result<Image>;
 
+    /// Find the outer bounding box.
+    /// 
     /// Bounding box of what remains after trimming with a specific color.
     fn bounding_box_trim_color(&self, color_to_be_trimmed: u8) -> anyhow::Result<Rectangle>;
+
+    /// Find the inner bounding box.
+    /// 
+    /// Shrink the bounding box further until the majority of border pixels are no longer the trim color.
+    fn shrink_bounding_box(&self, color_to_be_trimmed: u8, rect: Rectangle) -> anyhow::Result<Rectangle>;
 
     /// Remove border with the specified color. Shrink further until the majority of border pixels are not the trim color.
     fn trim_shrink_color(&self, color_to_be_trimmed: u8) -> anyhow::Result<Image>;
@@ -105,36 +112,15 @@ impl ImageTrim for Image {
         Ok(Rectangle::new(x, y, width, height))
     }
 
-    fn trim_shrink_color(&self, color_to_be_trimmed: u8) -> anyhow::Result<Image> {
-        if self.is_empty() {
-            return Ok(Image::empty());
+    fn shrink_bounding_box(&self, color_to_be_trimmed: u8, rect: Rectangle) -> anyhow::Result<Rectangle> {
+        if rect.is_empty() {
+            return Ok(Rectangle::empty());
         }
 
-        // Find bounding box
-        let x_max: i32 = (self.width() as i32) - 1;
-        let y_max: i32 = (self.height() as i32) - 1;
-        let mut found_x0: i32 = x_max;
-        let mut found_x1: i32 = 0;
-        let mut found_y0: i32 = y_max;
-        let mut found_y1: i32 = 0;
-        for y in 0..=y_max {
-            for x in 0..=x_max {
-                let pixel_value: u8 = self.get(x, y).unwrap_or(255);
-                if pixel_value == color_to_be_trimmed {
-                    continue;
-                }
-
-                // grow the bounding box
-                found_x0 = i32::min(found_x0, x);
-                found_x1 = i32::max(found_x1, x);
-                found_y0 = i32::min(found_y0, y);
-                found_y1 = i32::max(found_y1, y);
-            }
-        }
-
-        if found_x0 > found_x1 || found_y0 > found_y1 {
-            return Ok(Image::empty());
-        }
+        let mut found_x0: i32 = rect.min_x();
+        let mut found_x1: i32 = rect.max_x();
+        let mut found_y0: i32 = rect.min_y();
+        let mut found_y1: i32 = rect.max_y();
 
         // TODO: shrink bounding box, to get rid of junk
         let limit: usize = 3;
@@ -199,39 +185,47 @@ impl ImageTrim for Image {
             }
         }
 
+        if found_x0 > found_x1 || found_y0 > found_y1 {
+            return Ok(Rectangle::empty());
+        }
 
-        // Width of the object
+        // Left position
+        if found_x0 < 0 || found_x0 > (u8::MAX as i32) {
+            return Err(anyhow::anyhow!("Integrity error. Bounding box coordinates are messed up. found_x0: {}", found_x0));
+        }
+        let x: u8 = found_x0 as u8;
+
+        // Top position
+        if found_y0 < 0 || found_y0 > (u8::MAX as i32) {
+            return Err(anyhow::anyhow!("Integrity error. Bounding box coordinates are messed up. found_y0: {}", found_y0));
+        }
+        let y: u8 = found_y0 as u8;
+
+        // Width
         let new_width_i32: i32 = found_x1 - found_x0 + 1;
         if new_width_i32 < 1 || new_width_i32 > (u8::MAX as i32) {
             return Err(anyhow::anyhow!("Integrity error. Bounding box coordinates are messed up. new_width_i32: {}", new_width_i32));
         }
-        let new_width: u8 = new_width_i32 as u8;
+        let width: u8 = new_width_i32 as u8;
 
-        // Height of the object
+        // Height
         let new_height_i32: i32 = found_y1 - found_y0 + 1;
         if new_height_i32 < 1 || new_height_i32 > (u8::MAX as i32) {
             return Err(anyhow::anyhow!("Integrity error. Bounding box coordinates are messed up. new_height_i32: {}", new_height_i32));
         }
-        let new_height: u8 = new_height_i32 as u8;
+        let height: u8 = new_height_i32 as u8;
 
-        // TODO: return the bounding box, and in a separate function do the crop
+        Ok(Rectangle::new(x, y, width, height))
+    }
 
-        // Copy pixels of the object
-        let mut bitmap: Image = Image::zero(new_width, new_height);
-        for y in found_y0..=found_y1 {
-            for x in found_x0..=found_x1 {
-                let pixel_value: u8 = self.get(x, y).unwrap_or(255);
-                let set_x: i32 = x - found_x0;
-                let set_y: i32 = y - found_y0;
-                match bitmap.set(set_x, set_y, pixel_value) {
-                    Some(()) => {},
-                    None => {
-                        return Err(anyhow::anyhow!("Integrity error. Unable to set pixel ({}, {}) inside the result bitmap", set_x, set_y));
-                    }
-                }
-            }
+    fn trim_shrink_color(&self, color_to_be_trimmed: u8) -> anyhow::Result<Image> {
+        let rect: Rectangle = self.bounding_box_trim_color(color_to_be_trimmed)?;
+        let rect2: Rectangle = self.shrink_bounding_box(color_to_be_trimmed, rect)?;
+        if rect2.is_empty() {
+            return Ok(Image::empty());
         }
-        Ok(bitmap)
+        let image: Image = self.crop(rect2)?;
+        Ok(image)
     }
 }
 
