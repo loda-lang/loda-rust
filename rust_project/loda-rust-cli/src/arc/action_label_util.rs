12934b857e903495d123d0c1fef9f2be4e70f370
Simon Strandgaard <neoneye@gmail.com>
2023-04-07 11:39:17 +0200

    is_output_size_same_as_input_size() added

---
+++ rust_project/loda-rust-cli/src/arc/action_label_util.rs
@@ -0,0 +1,81 @@
+use super::{ActionLabel, ActionLabelSet, PropertyInput, PropertyOutput};
+
+pub struct ActionLabelUtil;
+
+impl ActionLabelUtil {
+    pub fn is_output_size_same_as_input_size(action_label_set: &ActionLabelSet) -> bool {
+        let mut same_width = false;
+        let mut same_height = false;
+        for label in action_label_set {
+            match label {
+                ActionLabel::OutputPropertyIsEqualToInputProperty { output, input } => {
+                    if *output == PropertyOutput::OutputWidth && *input == PropertyInput::InputWidth {
+                        same_width = true;
+                    }
+                    if *output == PropertyOutput::OutputHeight && *input == PropertyInput::InputHeight {
+                        same_height = true;
+                    }
+                },
+                _ => {}
+            }
+        }
+        same_width && same_height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_10000_is_output_size_same_as_input_size_yes() {
+        // Arrange
+        let mut action_label_set = ActionLabelSet::new();
+        {
+            let label = ActionLabel::OutputPropertyIsEqualToInputProperty { 
+                output: PropertyOutput::OutputWidth, 
+                input: PropertyInput::InputWidth 
+            };
+            action_label_set.insert(label);
+        }
+        {
+            let label = ActionLabel::OutputPropertyIsEqualToInputProperty { 
+                output: PropertyOutput::OutputHeight,
+                input: PropertyInput::InputHeight
+            };
+            action_label_set.insert(label);
+        }
+
+        // Act
+        let actual: bool = ActionLabelUtil::is_output_size_same_as_input_size(&action_label_set);
+
+        // Assert
+        assert_eq!(actual, true);
+    }
+
+    #[test]
+    fn test_10001_is_output_size_same_as_input_size_no() {
+        // Arrange
+        let mut action_label_set = ActionLabelSet::new();
+        {
+            let label = ActionLabel::OutputPropertyIsEqualToInputProperty { 
+                output: PropertyOutput::OutputWidth, 
+                input: PropertyInput::InputHeight
+            };
+            action_label_set.insert(label);
+        }
+        {
+            let label = ActionLabel::OutputPropertyIsEqualToInputProperty { 
+                output: PropertyOutput::OutputHeight,
+                input: PropertyInput::InputWidth
+            };
+            action_label_set.insert(label);
+        }
+
+        // Act
+        let actual: bool = ActionLabelUtil::is_output_size_same_as_input_size(&action_label_set);
+
+        // Assert
+        assert_eq!(actual, false);
+    }
+}
