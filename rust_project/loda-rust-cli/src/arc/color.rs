7759d5bfe018d6b594e3fc4c1c854336fd58d9f3
Simon Strandgaard <neoneye@gmail.com>
2023-04-08 23:01:37 +0200

    Color enum added

---
+++ rust_project/loda-rust-cli/src/arc/color.rs
@@ -0,0 +1,15 @@
+#[allow(dead_code)]
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Red = 2,
+    Green = 3,
+    Yellow = 4,
+    Grey = 5,
+    Fuchsia = 6,
+    Orange = 7,
+    Teal = 8,
+    Brown = 9,
+
+    CannotCompute = 254,
+}
