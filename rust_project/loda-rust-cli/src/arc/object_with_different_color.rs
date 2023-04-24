0ad162be0c8df7581166daeed453d34c2a38c046
Simon Strandgaard <neoneye@gmail.com>
2023-04-24 16:53:48 +0200

    Edge case when there is no single unpopular color.

--- rust_project/loda-rust-cli/src/arc/object_with_different_color.rs
+++ rust_project/loda-rust-cli/src/arc/object_with_different_color.rs
@@ -6,7 +6,7 @@ pub struct ObjectWithDifferentColor;
 impl ObjectWithDifferentColor {
     /// Find the single object that has different colors than the other objects.
     /// 
-    /// Returns an error when there it's ambiguous what object to pick.
+    /// Returns an error when it's ambiguous what object to pick.
     /// 
     /// The `image` and the `enumerated_objects` must have same size. And the size must be 1x1 or bigger.
     /// 
@@ -22,6 +22,7 @@ impl ObjectWithDifferentColor {
 
         // Determine the most popular color of each cell
         let mut object_color_and_object_index = Vec::<(u8, u8)>::new();
+        // Skip object_index=0, because it's not considered an object.
         for object_index in 1..=255u8 {
             let mask: Image = enumerated_objects.to_mask_where_color_is(object_index);
             let mut histogram: Histogram = image.histogram_with_mask(&mask)?;
@@ -48,17 +49,22 @@ impl ObjectWithDifferentColor {
             object_histogram.increment(*color);
         }
 
-        // pick the object with the least popular color
-        let least_popular_object_color: u8 = match object_histogram.least_popular_color_disallow_ambiguous() {
+        // Pick the least popular color
+        let (least_popular_color, least_popular_count) = match object_histogram.least_popular_pair_disallow_ambiguous() {
             Some(value) => value,
             None => {
                 return Err(anyhow::anyhow!("Ambiguous what is the least popular color"));
             }
         };
         // println!("least_popular_object_color: {}", least_popular_object_color);
+        if least_popular_count != 1 {
+            return Err(anyhow::anyhow!("Expected 1 object. However zero objects or multiple objects have the least popular color. Ambiguous what object to pick"));
+        }
+
+        // Index for the object
         let mut found_object_index: Option<u8> = None;
         for (object_color, object_index) in &object_color_and_object_index {
-            if *object_color == least_popular_object_color {
+            if *object_color == least_popular_color {
                 found_object_index = Some(*object_index);
                 break;
             }
@@ -142,5 +148,32 @@ mod tests {
         ];
         let expected: Image = Image::try_create(5, 4, expected_pixels).expect("image");
         assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_10002_found_no_object() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            5, 5, 7, 5, 5,
+            5, 5, 7, 5, 5,
+            7, 7, 7, 7, 7,
+            5, 5, 7, 5, 5,
+        ];
+        let input: Image = Image::try_create(5, 4, pixels).expect("image");
+
+        let enumerated_object_pixels: Vec<u8> = vec![
+            1, 1, 0, 2, 2,
+            1, 1, 0, 2, 2,
+            0, 0, 0, 0, 0,
+            3, 3, 0, 4, 4,
+        ];
+        let enumerated_objects: Image = Image::try_create(5, 4, enumerated_object_pixels).expect("image");
+
+        // Act
+        let error = ObjectWithDifferentColor::run(&input, &enumerated_objects, None).expect_err("is supposed to fail");
+
+        // Assert
+        let s = format!("{:?}", error);
+        assert_eq!(s.contains("zero objects or multiple objects have the least popular color"), true);
     }
 }
