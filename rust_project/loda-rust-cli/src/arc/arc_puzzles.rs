df89dd3ff615cd4de11b692d7aed188bbc90e428
Simon Strandgaard <neoneye@gmail.com>
2023-04-24 22:41:27 +0200

    Solved 1c0d0a4b in loda!

--- rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
+++ rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
@@ -3445,6 +3445,36 @@ mod tests {
         assert_eq!(result, "3 1");
     }
 
+    const PROGRAM_1C0D0A4B: &'static str = "
+    mov $80,$99
+    mov $81,100
+    mov $82,102 ; address of vector[0].ComputedOutputImage
+    mov $83,108 ; address of vector[0].GridMask
+    mov $84,109 ; address of vector[0].GridColor
+    lps $80
+        mov $0,$$81
+
+        mov $1,$$84 ; grid color
+        f21 $0,101251 ; Convert to a mask image by converting `color` to 0 and converting anything else to to 1.
+
+        mov $1,$$83 ; grid mask
+        f21 $0,101254 ; xor
+
+        mov $$82,$0
+
+        add $81,100
+        add $82,100
+        add $83,100
+        add $84,100
+    lpe
+    ";
+
+    #[test]
+    fn test_750001_puzzle_1c0d0a4b_loda() {
+        let result: String = run_advanced("1c0d0a4b", PROGRAM_1C0D0A4B).expect("String");
+        assert_eq!(result, "3 1");
+    }
+
     mod solve_6773b310 {
         use super::*;
 
