2201ac6c8e5b49ba1e4ba39eab0be63920f8350c
Simon Strandgaard <neoneye@gmail.com>
2023-04-25 12:09:20 +0200

    Solved 0b148d64 in LODA!

--- rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
+++ rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
@@ -3353,6 +3353,40 @@ mod tests {
         assert_eq!(result, "3 1");
     }
 
+    const PROGRAM_0B148D64: &'static str = "
+    mov $80,$99
+    mov $81,100 ; address of vector[0].InputImage
+    mov $82,102 ; address of vector[0].ComputedOutputImage
+    mov $83,109 ; address of vector[0].GridColor
+    mov $84,110 ; address of vector[0].EnumeratedObjects
+    lps $80
+        mov $0,$$81
+        mov $1,$$84 ; enumerated objects
+        mov $2,$$83 ; grid color
+
+        f31 $0,104111 ; Find the single object that has different colors than the other objects. With 1 ignore color.
+
+        mov $1,255 ; color for the area to be trimmed
+        mov $2,$$81
+        f31 $0,102130 ; Pick pixels from color and image
+
+        ; $1 is the color to be trimmed
+        f21 $0,101161 ; trim with color
+
+        mov $$82,$0
+        add $81,100
+        add $82,100
+        add $83,100
+        add $84,100
+    lpe
+    ";
+
+    #[test]
+    fn test_730001_puzzle_0b148d64_loda() {
+        let result: String = run_advanced("0b148d64", PROGRAM_0B148D64).expect("String");
+        assert_eq!(result, "3 1");
+    }
+
     mod solve_c3202e5a {
         use super::*;
 
