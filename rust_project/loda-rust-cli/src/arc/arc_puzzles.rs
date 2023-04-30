33f6c2c142a5b269ddeaa969983f032b1ae29b74
Simon Strandgaard <neoneye@gmail.com>
2023-04-30 09:25:09 +0200

    Eliminated hardcoded background color in solution be94b721.

--- rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
+++ rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
@@ -3651,13 +3651,28 @@ mod tests {
     mov $82,102 ; address of vector[0].ComputedOutputImage
     mov $83,110 ; address of vector[0].EnumeratedObjects
     lps $80
-        mov $0,$$83 ; enumerated objects
+        ; extract the background image
+        mov $1,$$83 ; enumerated objects
+        mov $2,0 ; the 0th object is the background object
+        f21 $1,101250 ; where color is the mask of the 0th object
 
+        ; histogram of the background image
+        mov $0,$$81 ; input image
+        f21 $0,101231 ; histogram with mask
+
+        ; get pixel at x=0, y=0, this is the most popular color
+        mov $1,0
+        mov $2,1
+        f31 $0,101002  ; get pixel of the most popular color
+        mov $10,$0
+
+        ; extract object 1, the biggest object
+        mov $0,$$83 ; enumerated objects
         mov $1,1 ; the 1st object is the biggest object
         f21 $0,101250 ; where color is the mask of the 1st object
 
-        ; assumes that the color is 0, derive this from the non-object background
-        mov $1,0 ; color for the area to be trimmed
+        ; surround the object with the background-color
+        mov $1,$10 ; color for the area to be trimmed
         mov $2,$$81
         f31 $0,102130 ; Pick pixels from color and image
 
