768ef49afc1853015763fd5ab3a3b37b6a2acc1c
Simon Strandgaard <neoneye@gmail.com>
2023-05-16 13:16:13 +0200

    Solved 2c608aff

--- rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
+++ rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
@@ -4382,4 +4382,54 @@ mod tests {
         let result: String = run_analyze_and_solve("af902bf9", &mut instance).expect("String");
         assert_eq!(result, "3 1");
     }
+
+    mod solve_2c608aff {
+        use super::*;
+
+        pub struct MySolution;
+    
+        impl AnalyzeAndSolve for MySolution {
+            fn solve(&self, data: &SolutionSimpleData, task: &arc_work_model::Task) -> anyhow::Result<Image> {
+                let mut found = false;
+                for action_label in &task.action_label_set_intersection {
+                    match action_label {
+                        ActionLabel::OutputImageIsInputImageWithChangesLimitedToPixelsWithMostPopularColorOfTheInputImage => {
+                            found = true;
+                        },
+                        _ => {}
+                    }
+                }
+                if !found {
+                    return Err(anyhow::anyhow!("OutputImageIsInputImageWithChangesLimitedToPixelsWithMostPopularColorOfTheInputImage not found"));
+                }
+                let pair: &arc_work_model::Pair = &task.pairs[data.index];
+                let input: &Image = &pair.input.image;
+
+                let background_color: u8 = input.most_popular_color().expect("color");
+
+                let single_color_objects: &SingleColorObjects = pair.input.single_color_objects.as_ref().expect("some");
+
+                let noise_color: u8 = single_color_objects.single_pixel_noise_color().expect("color");
+
+                let mut histogram: Histogram = pair.input.histogram.clone();
+                histogram.set_counter_to_zero(background_color);
+                histogram.set_counter_to_zero(noise_color);
+                if histogram.number_of_counters_greater_than_zero() != 1 {
+                    return Err(anyhow::anyhow!("Expected exactly 1 color that is not background_color or noise_color"));
+                }
+                let sticky_color: u8 = histogram.most_popular_color_disallow_ambiguous().expect("color");
+
+                let mut result_image: Image = input.clone();
+                result_image.draw_line_connecting_two_colors(sticky_color, noise_color, noise_color)?;
+                Ok(result_image)
+            }
+        }
+    }
+
+    #[test]
+    fn test_890000_puzzle_2c608aff() {
+        let mut instance = solve_2c608aff::MySolution {};
+        let result: String = run_analyze_and_solve("2c608aff", &mut instance).expect("String");
+        assert_eq!(result, "4 1");
+    }
 }
