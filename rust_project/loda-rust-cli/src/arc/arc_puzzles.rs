ff39c467f34dffd067f4ebcf259f5226e3b400ec
Simon Strandgaard <neoneye@gmail.com>
2023-05-19 11:41:20 +0200

    Solved f5b8619d in loda

--- rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
+++ rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
@@ -4397,6 +4397,48 @@ mod tests {
         assert_eq!(result, "3 1");
     }
 
+    const PROGRAM_F5B8619D: &'static str = "
+    mov $80,$99
+    mov $81,100 ; address of vector[0].InputImage
+    mov $82,102 ; address of vector[0].ComputedOutputImage
+    mov $83,115 ; address of vector[0].InputSinglePixelNoiseColor
+    lps $80
+        mov $0,$$81 ; input image
+
+        mov $8,$0
+        mov $9,$$83 ; noise color
+        f21 $8,101250 ; Convert to a mask image by converting `color` to 1 and converting anything else to to 0.
+        ; $8 is now the mask
+
+        mov $10,$0 ; image
+        mov $11,$8 ; mask
+        mov $12,42 ; line color
+        f31 $10,102222 ; Draw a vertical line if the `mask` contains one or more non-zero pixels.
+        ; $10 is now the columns image
+
+        mov $13,$8 ; mask
+        mov $14,$10 ; the columns image
+        mov $15,$0 ; input image
+        f31 $13,102132 ; Pick pixels from two images. When the mask is 0 then pick `image_a`. When the mask is [1..255] then pick from `image_b`.
+
+        mov $0,$13
+        mov $1,2
+        mov $2,2
+        f31 $0,102120 ; repeat image
+
+        mov $$82,$0
+        add $81,100
+        add $82,100
+        add $83,100
+    lpe
+    ";
+
+    #[test]
+    fn test_870001_puzzle_f5b8619d_loda() {
+        let result: String = run_advanced("f5b8619d", PROGRAM_F5B8619D).expect("String");
+        assert_eq!(result, "3 1");
+    }
+
     mod solve_af902bf9 {
         use super::*;
 
