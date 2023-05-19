674b0b93dc8886b4b5d2a52731fb02aadb476d37
Simon Strandgaard <neoneye@gmail.com>
2023-05-19 14:35:47 +0200

    Solved 00d62c1b in loda

--- rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
+++ rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
@@ -1339,6 +1339,29 @@ mod tests {
         assert_eq!(result, "5 1");
     }
 
+    const PROGRAM_00D62C1B: &'static str = "
+    mov $80,$99
+    mov $81,100 ; address of vector[0].InputImage
+    mov $82,102 ; address of vector[0].ComputedOutputImage
+    mov $83,114 ; address of vector[0].InputMostPopularColor
+    lps $80
+        mov $0,$$81 ; input image
+        mov $1,$$83 ; from_color = most popular color
+        mov $2,255 ; to_color = auto assign color
+        f31 $0,102180 ; Flood fill at every pixel along the border, connectivity-4.
+        mov $$82,$0
+        add $81,100
+        add $82,100
+        add $83,100
+    lpe
+    ";
+
+    #[test]
+    fn test_290001_puzzle_00d62c1b_loda() {
+        let result: String = run_advanced("00d62c1b", PROGRAM_00D62C1B).expect("String");
+        assert_eq!(result, "5 1");
+    }
+
     #[test]
     fn test_300000_puzzle_ae3edfdc() {
         let solution: SolutionSimple = |data| {
