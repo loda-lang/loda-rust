688beb6546da9f58db1fedc81e9340814ff8b347
Simon Strandgaard <neoneye@gmail.com>
2023-05-22 15:02:08 +0200

    Solved 3906de3d

--- rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
+++ rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
@@ -5117,4 +5117,26 @@ mod tests {
         let result: String = run_analyze_and_solve("62ab2642", &mut instance).expect("String");
         assert_eq!(result, "3 1");
     }
+
+    const PROGRAM_3906DE3D: &'static str = "
+    mov $80,$99
+    mov $81,100 ; address of vector[0].InputImage
+    mov $82,102 ; address of vector[0].ComputedOutputImage
+    mov $83,114 ; address of vector[0].InputMostPopularColor
+    lps $80
+        mov $0,$$81 ; input image
+        mov $1,$$83 ; most popular color across inputs
+        f21 $0,102190 ; Gravity in the up direction
+        mov $$82,$0
+        add $81,100
+        add $82,100
+        add $83,100
+    lpe
+    ";
+
+    #[test]
+    fn test_990000_puzzle_3906de3d_loda() {
+        let result: String = run_advanced("3906de3d", PROGRAM_3906DE3D).expect("String");
+        assert_eq!(result, "3 1");
+    }
 }
