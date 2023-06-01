622a79092970108acc7bad3e818f775fff7912b3
Simon Strandgaard <neoneye@gmail.com>
2023-06-01 23:53:20 +0200

    Convert from petgraph to Image.

--- rust_project/loda-rust-cli/src/arc/experiment_with_petgraph.rs
+++ rust_project/loda-rust-cli/src/arc/experiment_with_petgraph.rs
@@ -1,18 +1,28 @@
 use super::{Image, ImageCompare, ImagePadding, ImageSize, ImageMaskCount};
-use petgraph::stable_graph::NodeIndex;
+use petgraph::{stable_graph::NodeIndex, visit::EdgeRef};
 
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
 enum NodeData {
-    Image,
+    Image { width: u8, height: u8 },
     Pixel,
     Color { color: u8 },
     PositionX { x: u8 },
     PositionY { y: u8 },
+    // Pair,
+    // PairTrain,
+    // PairTest,
+    // PairInputImage,
+    // PairOutputImage,
     // PositionReverseX { x: u8 },
     // PositionReverseY { y: u8 },
     // PixelColumn,
     // PixelRow,
     // Object,
+    // ObjectStaysLocked,
+    // ObjectMoves,
+    // GridLine,
+    // GridCell,
+    // GridCorner,
 }
 
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
@@ -27,6 +37,9 @@ enum PixelNeighborEdgeType {
 enum EdgeData {
     Link,
     PixelNeighbor { edge_type: PixelNeighborEdgeType },
+    // PixelNearbyWithSameColor { edge_type: PixelNeighborEdgeType },
+    // PixelNearbyWithDifferentColor { edge_type: PixelNeighborEdgeType },
+    // SymmetricPixel { edge_type: PixelNeighborEdgeType },
 }
 
 #[allow(dead_code)]
@@ -42,39 +55,40 @@ impl ExperimentWithPetgraph {
         }
     }
 
+    /// Returns the `NodeIndex` of the created image node.
     #[allow(dead_code)]
-    fn add_image(&mut self, image: &Image) -> anyhow::Result<()> {
-        let node_image = NodeData::Image;
-        let index_image: NodeIndex = self.graph.add_node(node_image);
+    fn add_image(&mut self, image: &Image) -> anyhow::Result<NodeIndex> {
+        let node_image = NodeData::Image { width: image.width(), height: image.height() };
+        let image_index: NodeIndex = self.graph.add_node(node_image);
 
         let mut indexes_pixels: Vec<NodeIndex> = Vec::new();
         for y in 0..image.height() {
             for x in 0..image.width() {
                 let color: u8 = image.get(x as i32, y as i32).unwrap_or(255);
-                let node_pixel = NodeData::Pixel;
-                let index_pixel: NodeIndex = self.graph.add_node(node_pixel);
-                self.graph.add_edge(index_image, index_pixel, EdgeData::Link);
+                let pixel_node = NodeData::Pixel;
+                let pixel_index: NodeIndex = self.graph.add_node(pixel_node);
+                self.graph.add_edge(image_index, pixel_index, EdgeData::Link);
                 {
                     let property = NodeData::Color { color };
                     let index: NodeIndex = self.graph.add_node(property);
-                    self.graph.add_edge(index_pixel, index, EdgeData::Link);
+                    self.graph.add_edge(pixel_index, index, EdgeData::Link);
                 }
                 {
                     let property = NodeData::PositionX { x };
                     let index: NodeIndex = self.graph.add_node(property);
-                    self.graph.add_edge(index_pixel, index, EdgeData::Link);
+                    self.graph.add_edge(pixel_index, index, EdgeData::Link);
                 }
                 {
                     let property = NodeData::PositionY { y };
                     let index: NodeIndex = self.graph.add_node(property);
-                    self.graph.add_edge(index_pixel, index, EdgeData::Link);
+                    self.graph.add_edge(pixel_index, index, EdgeData::Link);
                 }
 
-                indexes_pixels.push(index_pixel);
+                indexes_pixels.push(pixel_index);
             }
         }
 
-        // Establish horizontal edges between neighbor pixels.
+        // Establish `LeftRight` edges between neighbor pixels.
         for y in 0..image.height() {
             for x in 1..image.width() {
                 let x0: u8 = x - 1;
@@ -87,7 +101,7 @@ impl ExperimentWithPetgraph {
             }
         }
 
-        // Establish vertical edges between neighbor pixels.
+        // Establish `UpDown` edges between neighbor pixels.
         for x in 0..image.width() {
             for y in 1..image.height() {
                 let y0: u8 = y - 1;
@@ -100,9 +114,56 @@ impl ExperimentWithPetgraph {
             }
         }
 
-        println!("graph: {:?}", self.graph);
-        Ok(())
+        Ok(image_index)
     }
+
+    /// Generates an image from the graph for the given `NodeIndex`.
+    #[allow(dead_code)]
+    fn to_image(&self, image_index: NodeIndex) -> anyhow::Result<Image> {
+        let node: NodeData = self.graph[image_index];
+        let (width, height) = match node {
+            NodeData::Image { width, height } => { (width, height) },
+            _ => { 
+                return Err(anyhow::anyhow!("Expected NodeData::Image at index {:?}.", image_index)); 
+            }
+        };
+        let mut result_image = Image::color(width, height, 255);
+
+        for edge_image in self.graph.edges(image_index) {
+            let pixel_index: NodeIndex = edge_image.target();
+            let pixel_node: NodeData = self.graph[pixel_index];
+            match pixel_node {
+                NodeData::Pixel => {},
+                _ => continue
+            }
+
+            let mut found_x: Option<u8> = None;
+            let mut found_y: Option<u8> = None;
+            let mut found_color: Option<u8> = None;
+            for edge_pixel in self.graph.edges(pixel_index) {
+                let child_index: NodeIndex = edge_pixel.target();
+                let child_node: NodeData = self.graph[child_index];
+                match child_node {
+                    NodeData::PositionX { x } => { found_x = Some(x); },
+                    NodeData::PositionY { y } => { found_y = Some(y); },
+                    NodeData::Color { color } => { found_color = Some(color); },
+                    _ => {}
+                }
+            }
+            match (found_x, found_y, found_color) {
+                (Some(x), Some(y), Some(color)) => {
+                    result_image.set(x as i32, y as i32, color);
+                },
+                _ => {}
+            }
+        }
+
+        Ok(result_image)
+    }
+
+    // fn extend_image_with_metadata(&mut self, image_index: NodeIndex, image: &Image) -> anyhow::Result<()> {
+    //     Ok(())
+    // }
 }
 
 #[cfg(test)]
@@ -111,11 +172,11 @@ mod tests {
     use crate::arc::ImageTryCreate;
 
     #[test]
-    fn test_10000_analyze() {
+    fn test_10000_add_image_and_to_image() {
         // Arrange
         let pixels: Vec<u8> = vec![
-            1, 0, 0, 0,
-            0, 1, 0, 0,
+            1, 0, 5, 5,
+            0, 1, 3, 3,
             0, 0, 1, 0,
             0, 0, 0, 1,
         ];
@@ -123,6 +184,17 @@ mod tests {
         let mut instance = ExperimentWithPetgraph::new();
 
         // Act
-        instance.add_image(&input).expect("ok");
+        let image_index: NodeIndex = instance.add_image(&input).expect("NodeIndex");
+        let actual: Image = instance.to_image(image_index).expect("Image");
+
+        // Assert
+        let expected_pixels: Vec<u8> = vec![
+            1, 0, 5, 5,
+            0, 1, 3, 3,
+            0, 0, 1, 0,
+            0, 0, 0, 1,
+        ];
+        let expected: Image = Image::try_create(4, 4, expected_pixels).expect("image");
+        assert_eq!(actual, expected);
     }
 }
