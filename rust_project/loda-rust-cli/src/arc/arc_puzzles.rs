3960cea468e0fcdd52dec1820956872f41009130
Simon Strandgaard <neoneye@gmail.com>
2023-06-02 22:26:07 +0200

    Simplified solution cf98881b

--- rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
+++ rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
@@ -3405,42 +3405,23 @@ mod tests {
     mov $82,102 ; address of vector[0].ComputedOutputImage
     mov $83,114 ; address of vector[0].InputMostPopularColor
     lps $80
-        mov $0,$$81 ; input image
         mov $20,$$83 ; most popular color across inputs
 
-        mov $8,$0
-        f11 $8,101000 ; Get width of image
-        sub $8,2
-        div $8,3
-        ; $8 is 1/3 width
-      
-        mov $11,$0
-        mov $12,$8
-        f21 $11,101222 ; get N left columns
-        ; $11 is left 1/3 of the image
-
-        mov $12,$0
-        mov $13,$8
-        f21 $12,101223 ; get N right columns
-        ; $12 is right 1/3 of the image
-
-        mov $13,$0
-        mov $14,$8
-        add $14,1
-        f21 $13,101226 ; remove N left columns
-        f21 $13,101227 ; remove N right columns
-        ; $13 is middle 1/3 of the image
+        mov $10,$$81 ; input image
+        mov $11,1 ; 1 pixel spacing
+        f23 $10,102262 ; split into 3 columns
+        ; $10..$12 are the 3 columns
 
         ; layer 0
         mov $0,$12 ; right of image
 
         ; layer 1
-        mov $1,$13 ; middle of image
+        mov $1,$11 ; middle of image
         mov $2,$20 ; most popular color
         f31 $0,101150 ; Image: Overlay another image by using a color as mask
 
         ; layer 2
-        mov $1,$11 ; left of image
+        mov $1,$10 ; left of image
         mov $2,$20 ; most popular color
         f31 $0,101150 ; Image: Overlay another image by using a color as mask
         
