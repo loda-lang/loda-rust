7f662a37857f4d335e0b8fb208590383ff489c1d
Simon Strandgaard <neoneye@gmail.com>
2023-06-03 15:45:08 +0200

    Solved ea9794b1

--- rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
+++ rust_project/loda-rust-cli/src/arc/arc_puzzles.rs
@@ -5808,4 +5808,65 @@ mod tests {
         let result: String = run_advanced("d037b0a7", PROGRAM_D037B0A7).expect("String");
         assert_eq!(result, "3 1");
     }
+
+    const PROGRAM_EA9794B1: &'static str = "
+    mov $80,$99
+    mov $81,100 ; address of vector[0].InputImage
+    mov $82,102 ; address of vector[0].ComputedOutputImage
+    mov $83,114 ; address of vector[0].InputMostPopularColor
+    lps $80
+        mov $0,$$81 ; input image
+        mov $20,$$83 ; most popular color across inputs
+        mov $21,0 ; pixel spacing = 0
+
+        mov $10,$$81 ; input image
+        mov $11,$21 ; spacing
+        f22 $10,102261 ; split into 2 rows
+        ; $10..$11 are the 2 rows
+
+        mov $15,$10
+        mov $16,$21 ; spacing
+        f22 $15,102260 ; split into 2 columns
+        ; $15..$16 are the 2 columns
+
+        mov $17,$11
+        mov $18,$21 ; spacing
+        f22 $17,102260 ; split into 2 columns
+        ; $17..$18 are the 2 columns
+
+        ; $15 = cell top left
+        ; $16 = cell top right
+        ; $17 = cell bottom left
+        ; $18 = cell bottom right
+
+        ; layer 0
+        mov $0,$15
+
+        ; layer 1
+        mov $1,$18
+        mov $2,$20 ; most popular color
+        f31 $0,101150 ; Image: Overlay another image by using a color as mask
+
+        ; layer 2
+        mov $1,$17
+        mov $2,$20 ; most popular color
+        f31 $0,101150 ; Image: Overlay another image by using a color as mask
+
+        ; layer 3
+        mov $1,$16
+        mov $2,$20 ; most popular color
+        f31 $0,101150 ; Image: Overlay another image by using a color as mask
+
+        mov $$82,$0
+        add $81,100
+        add $82,100
+        add $83,100
+    lpe
+    ";
+
+    #[test]
+    fn test_1150000_puzzle_ea9794b1_loda() {
+        let result: String = run_advanced("ea9794b1", PROGRAM_EA9794B1).expect("String");
+        assert_eq!(result, "6 1");
+    }
 }
