5fdf7b2c3a62b7fb2210c6ea618ca481f912df58
Simon Strandgaard <neoneye@gmail.com>
2023-07-28 17:38:01 +0200

    Serialize to run length encoded format. I have tried different formats and checked if GPT4 can make sense of it. This seems to be the shortest format I can come up with, that GPT4 can make sense of.

--- rust_project/loda-rust-cli/src/arc/image_line_spans.rs
+++ rust_project/loda-rust-cli/src/arc/image_line_spans.rs
@@ -1,4 +1,4 @@
-use super::Image;
+use super::{Histogram, Image, ImageHistogram, ImageMask};
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 struct SpanItem {
@@ -42,6 +42,41 @@ impl LineSpan {
         };
         Ok(instance)
     }
+
+    /// Run-length encode an image.
+    /// https://en.wikipedia.org/wiki/Run-length_encoding
+    /// 
+    /// I have experimented with GPT4 and it's able to decode this RLE format.
+    /// I had to add a space character between the lines to make it work.
+    /// I had to use B (Black) and W (White), so it's the same as on wikipedia.
+    fn serialize_rle(image: &Image) -> anyhow::Result<String> {
+        let histogram: Histogram = image.histogram_all();
+        let mut s = String::new();
+        for (_count, color) in histogram.pairs_ordered_by_color() {
+            s += &format!("{};", color);
+            s += &Self::serialize_rle_color(image, color)?;
+            s += "\n";
+        }
+        Ok(s.trim().to_string())
+    }
+
+    fn serialize_rle_color(image: &Image, color: u8) -> anyhow::Result<String> {
+        let mask: Image = image.to_mask_where_color_is(color);
+        let line_span = LineSpan::scan(&mask)?;
+        let mut s = String::new();
+        for item in &line_span.items {
+            if item.x == 0 && item.y > 0 {
+                s += " ";
+            }
+            s += &format!("{}", item.length);
+            if item.color == 0 {
+                s += "W";
+            } else {
+                s += "B";
+            }
+        }
+        Ok(s)
+    }
 }
 
 #[cfg(test)]
@@ -83,5 +118,24 @@ mod tests {
         expected_items.push(SpanItem { color: 7, x: 3, y: 3, length: 1 });
         expected_items.push(SpanItem { color: 0, x: 4, y: 3, length: 1 });
         assert_eq!(actual.items, expected_items);
+    }
+
+    #[test]
+    fn test_20000_run_length_encoding() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            7, 0, 1, 1, 1,
+            0, 7, 0, 1, 1,
+            0, 0, 7, 0, 0,
+            0, 0, 0, 7, 0,
+        ];
+        let input: Image = Image::try_create(5, 4, pixels).expect("image");
+
+        // Act
+        let actual: String = LineSpan::serialize_rle(&input).expect("ok");
+
+        // Assert
+        let expected = "0;1W1B3W 1B1W1B2W 2B1W2B 3B1W1B\n1;2W3B 3W2B 5W 5W\n7;1B4W 1W1B3W 2W1B2W 3W1B1W";
+        assert_eq!(actual, expected);
     }
 }
