77c0d1867c56fff8a84875e74b66d851eb22afd3
Simon Strandgaard <neoneye@gmail.com>
2023-08-24 10:52:36 +0200

    I was dividing by 13, where I should be dividing by 12. Added test.

--- rust_project/loda-rust-cli/src/arc/image_export.rs
+++ rust_project/loda-rust-cli/src/arc/image_export.rs
@@ -2,6 +2,19 @@ use super::{Color, Image, ImageSize};
 use anyhow::Context;
 use std::path::Path;
 
+/// Converts from a color symbol `[0..12]` to a normalized color value in the range `[0..255]`.
+/// 
+/// * The smallest value 0 is mapped to 0.
+/// * The largest value 12 is mapped to 255.
+/// * Values beyond 12 are mapped to 255.
+fn normalize_color(color: u8) -> u8 {
+    let color_normalized: u16 = (color as u16) * 255 / 12;
+    if color_normalized > 255 {
+        return 255;
+    }
+    color_normalized as u8
+}
+
 pub trait ImageExport {
     /// Save the image to a file.
     /// 
@@ -62,8 +75,7 @@ impl ImageExport for Image {
         let mut output = image_crate::ImageBuffer::new(size.width as u32, size.height as u32);
         for (x, y, pixel) in output.enumerate_pixels_mut() {
             let color_symbol: u8 = self.get(x as i32, y as i32).unwrap_or(255);
-            let color_normalized: u16 = (color_symbol as u16) * 255 / 13;
-            let r: u8 = (color_normalized & 255) as u8;
+            let r: u8 = normalize_color(color_symbol);
             let g: u8 = 0;
             let b: u8 = 0;
             *pixel = image_crate::Rgb([r, g, b]);
@@ -81,10 +93,24 @@ mod tests {
     use crate::arc::ImageTryCreate;
 
     #[test]
-    fn test_10000_create_arc_image_file() -> anyhow::Result<()> {
+    fn test_10000_normalize_color() {
+        let colors = [
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+        ];
+        let mut normalized: Vec<u8> = Vec::new();
+        for color in colors {
+            normalized.push(normalize_color(color));
+        }
+        assert_eq!(normalized, vec![
+            0, 21, 42, 63, 85, 106, 127, 148, 170, 191, 212, 233, 255, 255, 255,
+        ]);
+    }
+
+    #[test]
+    fn test_20000_create_arc_image_file() -> anyhow::Result<()> {
         // Arrange
         let tempdir = tempfile::tempdir().unwrap();
-        let basedir = PathBuf::from(&tempdir.path()).join("test_10000_create_arc_image_file");
+        let basedir = PathBuf::from(&tempdir.path()).join("test_20000_create_arc_image_file");
         fs::create_dir(&basedir)?;
         let path: PathBuf = basedir.join("output.png");
 
