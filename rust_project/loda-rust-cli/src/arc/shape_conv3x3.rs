e8a0880dd62805945aaaa50d217413b396091ec1
Simon Strandgaard <neoneye@gmail.com>
2023-09-13 22:31:53 +0200

    Create a catalog of all the 3x3 shapes. It contains 48 different shapes, when allowing for rotate and flip.

--- rust_project/loda-rust-cli/src/arc/shape_conv3x3.rs
+++ rust_project/loda-rust-cli/src/arc/shape_conv3x3.rs
@@ -1,8 +1,14 @@
-use super::{Image, ImageMask, ImageTrim, ShapeTransformation, ImageSize};
-use std::collections::HashSet;
+use super::{Image, ImageMask, ImageTrim, ShapeTransformation, ImageSize, ImageTryCreate};
+use std::collections::{HashSet, HashMap};
 
 #[allow(dead_code)]
-struct FindShape;
+struct FindShape {
+    shape_set: HashSet<Image>,
+    shape_vec: Vec<Image>,
+    image_to_shapeid: HashMap<Image, u8>,
+    index_to_transformation: HashMap<u8, HashSet<ShapeTransformation>>,
+    index_to_shapeid: HashMap::<u8, u8>,
+}
 
 impl FindShape {
     #[allow(dead_code)]
@@ -20,6 +26,50 @@ impl FindShape {
 
         Ok((transformation_set, normalized_image))
     }
+
+    #[allow(dead_code)]
+    fn populate() -> anyhow::Result<Self> {
+        let mut shape_set = HashSet::<Image>::new();
+        let mut shape_vec = Vec::<Image>::new();
+        let mut image_to_shapeid = HashMap::<Image, u8>::new();
+        let mut index_to_transformation = HashMap::<u8, HashSet<ShapeTransformation>>::new();
+        let mut index_to_shapeid = HashMap::<u8, u8>::new();
+
+        // Traverse all possible 3x3 images, where the center pixel is 1.
+        // The center pixel is surrounded by 8 pixels. So there are 2^8 = 256 possible combinations.
+        for i in 0..=255u8 {
+            let values: [u8; 9] = [
+                i & 1, (i >> 1) & 1, (i >> 2) & 1,
+                (i >> 3) & 1, 1, (i >> 4) & 1,
+                (i >> 5) & 1, (i >> 6) & 1, (i >> 7) & 1,
+            ];
+            let image: Image = Image::try_create(3, 3, values.to_vec())?;
+            let (transformations, output) = FindShape::analyze(&image).expect("image");
+            if !shape_set.contains(&output) {
+                let shapeid: u8 = (shape_set.len() & 255) as u8;
+                image_to_shapeid.insert(output.clone(), shapeid);
+                shape_set.insert(output.clone());
+                shape_vec.push(output.clone());
+            }
+            if let Some(shapeid) = image_to_shapeid.get(&output) {
+                index_to_shapeid.insert(i, *shapeid);
+            }
+            index_to_transformation.insert(i, transformations);
+        }
+        // println!("shape_set: {}", shape_set.len());
+        // println!("shape_vec: {}", shape_vec.len());
+        // println!("image_to_shapeid: {}", image_to_shapeid.len());
+        // println!("index_to_transformation: {}", index_to_transformation.len());
+        // println!("index_to_shapeid: {}", index_to_shapeid.len());
+        let instance = Self {
+            shape_set,
+            shape_vec,
+            image_to_shapeid,
+            index_to_transformation,
+            index_to_shapeid,
+        };
+        Ok(instance)
+    }
 }
 
 #[cfg(test)]
@@ -28,7 +78,7 @@ mod tests {
     use crate::arc::ImageTryCreate;
 
     #[test]
-    fn test_10000_classify() {
+    fn test_10000_analyze() {
         // Arrange
         let pixels: Vec<u8> = vec![
             5, 5, 5,
@@ -49,5 +99,20 @@ mod tests {
         let expected: Image = Image::try_create(3, 3, expected_pixels).expect("image");
         assert_eq!(output, expected);
         assert_eq!(transformations, HashSet::<ShapeTransformation>::from([ShapeTransformation::RotateCw270, ShapeTransformation::FlipXRotateCw180]));
+    }
+
+    #[test]
+    fn test_20000_populate() {
+        // Arrange
+        
+        // Act
+        let instance: FindShape = FindShape::populate().expect("populate");
+
+        // Assert
+        assert_eq!(instance.shape_set.len(), 48);
+        assert_eq!(instance.shape_vec.len(), 48);
+        assert_eq!(instance.image_to_shapeid.len(), 48);
+        assert_eq!(instance.index_to_transformation.len(), 256);
+        assert_eq!(instance.index_to_shapeid.len(), 256);
     }
 }
