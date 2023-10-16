eeeee471ec76bd09afa011333e09d60ceffca17f
Simon Strandgaard <neoneye@gmail.com>
2023-10-16 23:48:46 +0200

    Number of bigrams trigrams does not improve the number of solved tasks.

--- rust_project/loda-rust-cli/src/arc/solve_logisticregression.rs
+++ rust_project/loda-rust-cli/src/arc/solve_logisticregression.rs
@@ -1889,6 +1889,63 @@ impl SolveLogisticRegression {
             //     random_image = image.crop_outside(0, 0, width, height, 255)?;
             // }
 
+            // let mut number_of_unique_bigrams_column_vec = Vec::<u8>::new();
+            // for x in 0..width {
+            //     let image: Image = input.crop_outside(x as i32, 0, 1, height, 255)?;
+            //     let bigrams: Vec<RecordBigram> = image.bigram_y()?;
+            //     let bigrams: Vec<RecordTrigram> = image.trigram_y()?;
+            //     number_of_unique_bigrams_column_vec.push(bigrams.len().min(255) as u8);
+            // }
+            // let mut number_of_unique_bigrams_row_vec = Vec::<u8>::new();
+            // for y in 0..height {
+            //     let image: Image = input.crop_outside(0, y as i32, width, 1, 255)?;
+            //     let bigrams: Vec<RecordBigram> = image.bigram_x()?;
+            //     let bigrams: Vec<RecordTrigram> = image.trigram_x()?;
+            //     number_of_unique_bigrams_row_vec.push(bigrams.len().min(255) as u8);
+            // }
+
+            // if let Some(ep_image) = earlier_prediction_image {
+            //     for x in 0..width {
+            //         let image: Image = ep_image.crop_outside(x as i32, 0, 1, height, 255)?;
+            //         let bigrams: Vec<RecordBigram> = image.bigram_y()?;
+            //         let bigrams: Vec<RecordTrigram> = image.trigram_y()?;
+            //         number_of_unique_bigrams_column_vec.push(bigrams.len().min(255) as u8);
+            //     }
+            //     for y in 0..height {
+            //         let image: Image = ep_image.crop_outside(0, y as i32, width, 1, 255)?;
+            //         let bigrams: Vec<RecordBigram> = image.bigram_x()?;
+            //         let bigrams: Vec<RecordTrigram> = image.trigram_x()?;
+            //         number_of_unique_bigrams_row_vec.push(bigrams.len().min(255) as u8);
+            //     }
+            // }
+
+            // let mut trigrams_column = HashMap::<u8, Vec::<RecordTrigram>>::new();
+            // let mut trigrams_row = HashMap::<u8, Vec::<RecordTrigram>>::new();
+            // {
+            //     for x in 0..width {
+            //         let image: Image = input.crop_outside(x as i32, 0, 1, height, 255)?;
+            //         let trigrams: Vec<RecordTrigram> = image.trigram_y()?;
+            //         trigrams_column.insert(x, trigrams);
+            //     }
+            //     for y in 0..height {
+            //         let image: Image = input.crop_outside(0, y as i32, width, 1, 255)?;
+            //         let trigrams: Vec<RecordTrigram> = image.trigram_x()?;
+            //         trigrams_row.insert(y, trigrams);
+            //     }
+            // }
+            // if let Some(ep_image) = earlier_prediction_image {
+            //     for x in 0..width {
+            //         let image: Image = ep_image.crop_outside(x as i32, 0, 1, height, 255)?;
+            //         let trigrams: Vec<RecordTrigram> = image.trigram_y()?;
+            //         trigrams_column.insert(x, trigrams);
+            //     }
+            //     for y in 0..height {
+            //         let image: Image = ep_image.crop_outside(0, y as i32, width, 1, 255)?;
+            //         let trigrams: Vec<RecordTrigram> = image.trigram_x()?;
+            //         trigrams_row.insert(y, trigrams);
+            //     }
+            // }
+
             for y in 0..height {
                 let yy: i32 = y as i32;
                 let y_reverse: u8 = ((height as i32) - 1 - yy).max(0) as u8;
@@ -1962,6 +2019,52 @@ impl SolveLogisticRegression {
                     let area3x3: Image = input.crop_outside(xx - 1, yy - 1, 3, 3, 255)?;
                     let area5x5: Image = input.crop_outside(xx - 2, yy - 2, 5, 5, 255)?;
                     let center: u8 = area5x5.get(2, 2).unwrap_or(255);
+
+                    // {
+                    //     let count: u8 = number_of_unique_bigrams_column_vec.get(x as usize).map(|n| *n).unwrap_or(0);
+                    //     record.serialize_u8(count);
+                    //     record.serialize_onehot_discard_overflow(count, 28);
+                    // }
+                    // {
+                    //     let count: u8 = number_of_unique_bigrams_row_vec.get(y as usize).map(|n| *n).unwrap_or(0);
+                    //     record.serialize_u8(count);
+                    //     record.serialize_onehot_discard_overflow(count, 28);
+                    // }
+
+                    // {
+                    //     let mut count: u8 = 0;
+                    //     if let Some(trigrams) = trigrams_column.get(&x) {
+                    //         for trigram in trigrams {
+                    //             if trigram.word0 == center {
+                    //                 count += 1;
+                    //             }
+                    //             if trigram.word1 == center {
+                    //                 count += 1;
+                    //             }
+                    //             if trigram.word2 == center {
+                    //                 count += 1;
+                    //             }
+                    //         }
+                    //     }
+                    //     record.serialize_u8(count);
+                    // }
+                    // {
+                    //     let mut count: u8 = 0;
+                    //     if let Some(trigrams) = trigrams_row.get(&y) {
+                    //         for trigram in trigrams {
+                    //             if trigram.word0 == center {
+                    //                 count += 1;
+                    //             }
+                    //             if trigram.word1 == center {
+                    //                 count += 1;
+                    //             }
+                    //             if trigram.word2 == center {
+                    //                 count += 1;
+                    //             }
+                    //         }
+                    //     }
+                    //     record.serialize_u8(count);
+                    // }
 
                     // let area_left: Image = if x > 2 {
                     //     input.left_columns(x - 1)?
