5b757643727acbbf0b99aa3cbc7001c2ed9e4b9e
Simon Strandgaard <neoneye@gmail.com>
2023-10-20 18:02:02 +0200

    Experiments with LargestInteriorRectangle

--- rust_project/loda-rust-cli/src/arc/largest_interior_rectangle.rs
+++ rust_project/loda-rust-cli/src/arc/largest_interior_rectangle.rs
@@ -1,34 +1,115 @@
-use crate::arc::ImageRotate90;
+use super::{Image, convolution2x2, HtmlLog, ImageMaskCount, Rectangle, ImageSymmetry};
 
-use super::{Image, convolution2x2, HtmlLog, ImageMaskCount};
-
-pub struct LargestInteriorRectangle;
+#[derive(Clone, Debug, PartialEq)]
+pub struct LargestInteriorRectangle {
+    rectangles: Vec<Rectangle>,
+    mass: u16,
+}
 
 impl LargestInteriorRectangle {
     fn analyze(image: &Image) -> anyhow::Result<Self> {
+        let verbose = false;
+        if verbose {
+            HtmlLog::image(&image);
+        }
+
+        let mut candidates = Vec::<Rectangle>::new();
+        let mut biggest_area: u16 = 0;
+        {
+            let slices: LongestHorizontalSlices = LongestHorizontalSlices::analyze(&image)?;
+            // println!("x slices: {:?}", slices);
+            let mass: u16 = slices.mass as u16;
+            if mass > biggest_area {
+                biggest_area = slices.mass as u16;
+                candidates.clear();
+            }
+            if mass == biggest_area {
+                for (x, y) in &slices.positions {
+                    let rect: Rectangle = Rectangle::new(*x, *y, slices.mass, 1);
+                    candidates.push(rect);
+                }
+            }
+        }
+
+        {
+            let layer90: Image = image.flip_diagonal_a()?;
+            let slices: LongestHorizontalSlices = LongestHorizontalSlices::analyze(&layer90)?;
+            // println!("y slices: {:?}", slices);
+            let mass: u16 = slices.mass as u16;
+            if mass > biggest_area {
+                biggest_area = slices.mass as u16;
+                candidates.clear();
+            }
+            if mass == biggest_area {
+                for (x, y) in &slices.positions {
+                    let rect: Rectangle = Rectangle::new(*y, *x, 1, slices.mass);
+                    candidates.push(rect);
+                }
+            }
+        }
         let mut current_layer: Image = image.clone();
+        let mut scale: u8 = 2;
         loop {
             let layer: Image = convolution2x2(&current_layer, conv2x2_is_full)?;
             let count: u16 = layer.mask_count_nonzero();
             if count == 0 {
                 break;
             }
-            HtmlLog::image(&layer);
+            if verbose {
+                HtmlLog::image(&layer);
+            }
 
-            let slices_x: LongestHorizontalSlices = LongestHorizontalSlices::analyze(&layer)?;
-            println!("slices_x: {:?}", slices_x);
+            {
+                let slices: LongestHorizontalSlices = LongestHorizontalSlices::analyze(&layer)?;
+                // println!("x slices: {:?}", slices);
+                let mass: u16 = ((slices.mass as u16) + ((scale - 1) as u16)) * (scale as u16);
+                if mass > biggest_area {
+                    biggest_area = mass;
+                    candidates.clear();
+                }
+                if mass == biggest_area {
+                    for (x, y) in &slices.positions {
+                        let rect: Rectangle = Rectangle::new(
+                            *x, 
+                            *y, 
+                            slices.mass + scale - 1, 
+                            scale,
+                        );
+                        candidates.push(rect);
+                    }
+                }
+            }
 
-            let layer90: Image = layer.rotate_cw()?;
-            let slices_y: LongestHorizontalSlices = LongestHorizontalSlices::analyze(&layer90)?;
-            println!("slices_y: {:?}", slices_y);
-
-            // if slices_x.mass == slices_y.mass {
-
-            // }
+            {
+                let layer90: Image = layer.flip_diagonal_a()?;
+                let slices: LongestHorizontalSlices = LongestHorizontalSlices::analyze(&layer90)?;
+                // println!("slices: {:?}", slices);
+                let mass: u16 = ((slices.mass as u16) + ((scale - 1) as u16)) * (scale as u16);
+                if mass > biggest_area {
+                    biggest_area = mass;
+                    candidates.clear();
+                }
+                if mass == biggest_area {
+                    for (x, y) in &slices.positions {
+                        let rect: Rectangle = Rectangle::new(
+                            *y, 
+                            *x, 
+                            scale,
+                            slices.mass + scale - 1, 
+                        );
+                        candidates.push(rect);
+                    }
+                }
+            }
 
             current_layer = layer;
+            scale += 1;
         }
-        Ok(Self)
+        let instance = Self {
+            rectangles: candidates,
+            mass: biggest_area,
+        };
+        Ok(instance)
     }
 }
 
@@ -92,14 +173,34 @@ fn conv2x2_is_full(image: &Image) -> anyhow::Result<u8> {
     Ok(1)
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::arc::ImageTryCreate;
 
     #[test]
-    fn test_10000_analyze() {
+    fn test_10000_longest_horizontal_slices() {
+        // Arrange
+        let input_pixels: Vec<u8> = vec![
+            1, 1, 1, 1, 0, 1,
+            1, 1, 0, 1, 1, 0,
+            1, 1, 1, 1, 1, 1,
+            0, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 0, 
+            1, 1, 0, 1, 1, 1,
+        ];
+        let input: Image = Image::try_create(6, 6, input_pixels).expect("image");
+
+        // Act
+        let actual: LongestHorizontalSlices = LongestHorizontalSlices::analyze(&input).expect("ok");
+
+        // Assert
+        assert_eq!(actual.mass, 6);
+        assert_eq!(actual.positions, vec![(0, 2)]);
+    }
+
+    #[test]
+    fn test_20000_largest_interior_rectangle() {
         // Arrange
         let input_pixels: Vec<u8> = vec![
             1, 1, 1, 1, 0, 1,
@@ -115,12 +216,12 @@ mod tests {
         let actual: LargestInteriorRectangle = LargestInteriorRectangle::analyze(&input).expect("ok");
 
         // Assert
-        // let expected_pixels: Vec<u8> = vec![
-        //     1, 2, 3, 4, 5,
-        //     6, 0, 0, 0, 10,
-        //     11, 12, 13, 14, 15,
-        // ];
-        // let expected: Image = Image::try_create(5, 3, expected_pixels).expect("image");
-        // assert_eq!(actual, expected);
+        let expected = LargestInteriorRectangle {
+            rectangles: vec![
+                Rectangle::new(1, 2, 4, 3),
+            ],
+            mass: 12,
+        };
+        assert_eq!(actual, expected);
     }
 }
