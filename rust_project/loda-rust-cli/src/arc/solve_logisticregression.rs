75c51c3b77622c2c61518bb5e7a1e8504aa5d028
Simon Strandgaard <neoneye@gmail.com>
2023-11-18 18:15:33 +0100

    enable_color_clustercount for variant1 and variant2, solves 2 new tasks.

--- rust_project/loda-rust-cli/src/arc/solve_logisticregression.rs
+++ rust_project/loda-rust-cli/src/arc/solve_logisticregression.rs
@@ -412,12 +412,13 @@ impl SolveLogisticRegression {
             "1c0d0a4b", "2072aba6", "21f83797", "2281f1f4", "23581191", "253bf280", "25d8a9c8", "2dee498d", "319f2597", "32597951",
             "332efdb3", "3618c87e", "37d3e8b2", "4258a5f9", "44d8ac46", "45737921", "4612dd53", "50cb2852", "5168d44c", "516b51b7",
             "5289ad53", "543a7ed5", "54d9e175", "5b526a93", "5b6cbef5", "639f5a19", "6455b5f5", "67385a82", "694f12f3", "69889d6e",
-            "6c434453", "6d75e8bb", "6ea4a07e", "6f8cd79b", "776ffc46", "810b9b61", "84f2aca1", "868de0fa", "8a371977", "90f3ed37",
-            "913fb3ed", "a2fd1cf0", "94f9d214", "95990924", "a5313dff", "a61f2674", "a65b410d", "a699fb00", "a8d7556c", "a934301b",
-            "a9f96cdd", "aa4ec2a5", "ae3edfdc", "ae58858e", "aedd82e4", "b0c4d837", "b1948b0a", "b230c067", "b2862040", "b60334d2",
-            "b6afb2da", "ba26e723", "bb43febb", "bbb1b8b6", "bbc9ae5d", "bdad9b1f", "c0f76784", "c8f0f002", "ce039d91", "ce22a75a",
-            "ce9e57f2", "d2abd087", "d364b489", "d37a1ef5", "d406998b", "d511f180", "d5d6de2d", "dbc1a6ce", "dc433765", "de1cd16c",
-            "ded97339", "e0fb7511", "e133d23d", "e7dd8335", "e8593010", "e872b94a", "e9c9d9a1", "ef135b50", "f45f5ca7",
+            "6c434453", "6d75e8bb", "6ea4a07e", "6f8cd79b", "72a961c9", "776ffc46", "810b9b61", "84db8fc4", "84f2aca1", "868de0fa",
+            "8a371977", "90f3ed37", "913fb3ed", "94f9d214", "95990924", "a2fd1cf0", "a5313dff", "a61f2674", "a65b410d", "a699fb00",
+            "a8d7556c", "a934301b", "a9f96cdd", "aa4ec2a5", "ae3edfdc", "ae58858e", "aedd82e4", "b0c4d837", "b1948b0a", "b230c067",
+            "b2862040", "b60334d2", "b6afb2da", "ba26e723", "bb43febb", "bbb1b8b6", "bbc9ae5d", "bdad9b1f", "c0f76784", "c8f0f002",
+            "ce039d91", "ce22a75a", "ce9e57f2", "d2abd087", "d364b489", "d37a1ef5", "d406998b", "d511f180", "d5d6de2d", "dbc1a6ce",
+            "dc433765", "de1cd16c", "ded97339", "e0fb7511", "e133d23d", "e7dd8335", "e8593010", "e872b94a", "e9c9d9a1", "ef135b50",
+            "f45f5ca7",
         ];
         let ignore_task_id: HashSet<String> = already_fully_solved_tasks_ids.iter().map(|s| s.to_string()).collect();
         
@@ -928,7 +929,7 @@ impl SolveLogisticRegression {
         let enable_serialize_color_complex: bool = [true, false, false][v];
         let enable_serialize_cluster_id_shakeup: bool = [false, true, true][v];
         let enable_total_clustercount: bool = false;
-        let enable_color_clustercount: bool = false;
+        let enable_color_clustercount: bool = [false, true, true][v];
         let enable_half_context_input_size: bool = [true, true, true][v];
         let enable_half_context_output_size: bool = [false, false, false][v];
         let enable_normalized_coordinates_context_input_size: bool = false;
