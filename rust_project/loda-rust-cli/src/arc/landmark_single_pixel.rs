35cbba4477b5cc8754603f73b5ced3d10a7509a9
Simon Strandgaard <neoneye@gmail.com>
2023-11-22 23:16:16 +0100

    Recognizing more shapes

--- rust_project/loda-rust-cli/src/arc/landmark_single_pixel.rs
+++ rust_project/loda-rust-cli/src/arc/landmark_single_pixel.rs
@@ -102,10 +102,13 @@ impl LandmarkSinglePixel {
             4, // L-shape 45 degree 
             6, // L-shape
             8, // bended line
-            11, // bended shape
             14, // T-shape
             19, // T-shape 45 degree
+            24, // 3 lines meeting in center
+            25, // 3 lines meeting in center
+            28, // one pixel attached on a 2x2 square
             37, // Plus shape
+            43, // Plus with diagonal corners
             45, // Plus shape 45 degree
         ]);
 
@@ -392,13 +395,36 @@ mod tests {
     }
 
     #[test]
-    fn test_190000_45degree_bending_line() {
+    fn test_190100_three_lines_meeting_in_center() {
         // Arrange
         let pixels: Vec<u8> = vec![
-            0, 0, 0, 0, 0,
-            3, 6, 9, 0, 0,
-            5, 5, 5, 3, 0,
-            5, 5, 5, 5, 1,
+            0, 0, 0, 0, 5,
+            0, 0, 0, 5, 0,
+            8, 8, 6, 0, 0,
+            0, 0, 0, 7, 0,
+        ];
+        let input: Image = Image::try_create(5, 4, pixels).expect("image");
+
+        // Act
+        let actual: LandmarkSinglePixel = LandmarkSinglePixel::analyze(&input, 0).expect("ok");
+
+        // Assert
+        let expected = LandmarkSinglePixel {
+            x: 2,
+            y: 2,
+            color: 6,
+        };
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_190200_three_lines_meeting_in_center() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            0, 0, 8, 0, 0,
+            8, 8, 6, 0, 0,
+            0, 0, 0, 5, 0,
+            0, 0, 0, 0, 5,
         ];
         let input: Image = Image::try_create(5, 4, pixels).expect("image");
 
@@ -409,7 +435,53 @@ mod tests {
         let expected = LandmarkSinglePixel {
             x: 2,
             y: 1,
-            color: 9,
+            color: 6,
+        };
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_190300_one_pixel_attached_on_a_2x2_square() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            0, 0, 0, 0, 7,
+            0, 0, 0, 8, 0,
+            0, 8, 1, 0, 0,
+            0, 9, 3, 0, 0,
+        ];
+        let input: Image = Image::try_create(5, 4, pixels).expect("image");
+
+        // Act
+        let actual: LandmarkSinglePixel = LandmarkSinglePixel::analyze(&input, 0).expect("ok");
+
+        // Assert
+        let expected = LandmarkSinglePixel {
+            x: 2,
+            y: 2,
+            color: 1,
+        };
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_190400_plus_with_diagonal_corners() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            0, 0, 9, 8, 0,
+            0, 8, 7, 9, 0,
+            0, 9, 3, 0, 0,
+            0, 0, 0, 0, 0,
+        ];
+        let input: Image = Image::try_create(5, 4, pixels).expect("image");
+
+        // Act
+        let actual: LandmarkSinglePixel = LandmarkSinglePixel::analyze(&input, 0).expect("ok");
+
+        // Assert
+        let expected = LandmarkSinglePixel {
+            x: 2,
+            y: 1,
+            color: 7,
         };
         assert_eq!(actual, expected);
     }
@@ -724,6 +796,25 @@ mod tests {
             0, 0, 4, 5, 0,
         ];
         let input: Image = Image::try_create(5, 2, pixels).expect("image");
+
+        // Act
+        let error = LandmarkSinglePixel::analyze(&input, 0).expect_err("should fail");
+
+        // Assert
+        let message = error.to_string();
+        assert_eq!(message, "zero landmarks found in the corner mask");
+    }
+
+    #[test]
+    fn test_280002_reject_45degree_bending_shape() {
+        // Arrange
+        let pixels: Vec<u8> = vec![
+            0, 0, 0, 0, 0,
+            3, 6, 9, 0, 0,
+            5, 5, 5, 3, 0,
+            5, 5, 5, 5, 1,
+        ];
+        let input: Image = Image::try_create(5, 4, pixels).expect("image");
 
         // Act
         let error = LandmarkSinglePixel::analyze(&input, 0).expect_err("should fail");
